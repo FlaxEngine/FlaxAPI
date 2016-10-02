e0b10caf4237eb53e0bd7139f43245fb8810bf94
mafiesto4 <mafiesto4.4@gmail.com>
2016-10-02 10:18:00 +0200

    project setup

---
+++ CelelejEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CelelejEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CelelejEngine")]
+[assembly: AssemblyCopyright("Copyright © 2016 Wojciech Figat")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("be9ef022-e4dd-439d-b197-3a3c6058f479")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0")]
+/*
+[assembly: InternalsVisibleTo("CelelejEditor")]
+[assembly: InternalsVisibleTo("CelelejEngine.Networking")]
+*/
