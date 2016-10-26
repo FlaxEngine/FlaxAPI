bc2a89ded80dea3f1fb8eff6f44f398ef55a6165
mafiesto4 <wojtek@figat.pl>
2016-10-27 00:40:36 +0200

    model and box brush interop

--- CelelejEngine/ActorTypes/ModelActor.cs
+++ CelelejEngine/ActorTypes/ModelActor.cs
@@ -1,14 +1,33 @@
 ﻿// Celelej Game Engine scripting API
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace CelelejEngine
 {
     public sealed class ModelActor : Actor
     {
+        // TODO: Model
+        // TODO: ModelInstanceBuffer
+        // TODO: easy materials changing wihout need to use InstanceBuffer manually
+
+        /// <summary>
+        /// Gets or sets model scale in lightmap parameter
+        /// </summary>
+        public float ScaleInLightmap
+        {
+            get { return Internal_GetScaleInLightmap(unmanagedPtr); }
+            set { Internal_SetScaleInLightmap(unmanagedPtr, value); }
+        }
+        
+        #region Internal Calls
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern float Internal_GetScaleInLightmap(IntPtr obj);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void Internal_SetScaleInLightmap(IntPtr obj, float value);
+        
+        #endregion
     }
 }
---
+++ CelelejEngine/Enums.cs
@@ -0,0 +1,20 @@
+﻿// Celelej Game Engine scripting API
+
+namespace CelelejEngine
+{
+    /// <summary>
+    /// CSG brush mode
+    /// </summary>
+    public enum BrushMode
+    {
+        /// <summary>
+        /// Brush adds
+        /// </summary>
+        Additive = 0,
+
+        /// <summary>
+        /// Brush substracts
+        /// </summary>
+        Substractive = 1,
+    }
+}
