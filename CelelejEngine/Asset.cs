17a267138c28dbb8858ab893439979b54c7d9e22
mafiesto4 <wojtek@figat.pl>
2016-11-02 11:11:10 +0100

    asset interop get name

--- CelelejEngine/Asset.cs
+++ CelelejEngine/Asset.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,25 @@ namespace CelelejEngine
     /// </summary>
     public abstract class Asset : Object
     {
+        /// <summary>
+        /// Gets asset name
+        /// </summary>
+        public string Name
+        {
+            get { return Internal_GetName(unmanagedPtr); }
+        }
+        
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Internal_GetName(unmanagedPtr), GetType().Name);
+        }
+
+        #region Internal Calls
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        internal static extern string Internal_GetName(IntPtr obj);
+        
+        #endregion
     }
 }
