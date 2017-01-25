1398ff8e39a86d8d19d3143e4d5a8119aca6979d
mafiesto4 <wojtek@figat.pl>
2017-01-25 12:13:40 +0100

    api copy script fix

--- CopyDotNetApi.bat
+++ CopyDotNetApi.bat
@@ -1,6 +1,6 @@
 @echo off
 
-xcopy /s /y "FlaxEditor\bin" "..\Source\Bin\Cache\bin"
-xcopy /s /y "FlaxEngine\bin" "..\Source\Bin\Cache\bin"
+xcopy /s /y "FlaxEditor\bin" "..\Source\Bin\Editor\Assemblies"
+xcopy /s /y "FlaxEngine\bin" "..\Source\Bin\Editor\Assemblies"
 
 echo Done!
