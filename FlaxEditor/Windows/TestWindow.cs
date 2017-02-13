c520bd56621cb655674005349f2a72b27d94a78b
mafiesto4 <wojtek@figat.pl>
2017-02-13 17:34:26 +0100

    test window added

---
+++ FlaxEditor/Windows/TestWindow.cs
@@ -0,0 +1,31 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.Windows
+{
+    public sealed class TestWindow : Window
+    {
+        protected override void Initialize()
+        {
+            BackgroundColor = Color.Red;
+        }
+
+        public override bool OnKeyDown(KeyCode key)
+        {
+            Debug.Log("Key down: " + key);
+
+            return base.OnKeyDown(key);
+        }
+
+        public override void OnKeyUp(KeyCode key)
+        {
+            Debug.Log("Key up: " + key);
+
+            base.OnKeyDown(key);
+        }
+    }
+}
