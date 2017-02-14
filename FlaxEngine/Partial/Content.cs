4799e10c183935a80ff0322156c8e78ada2b84e1
mafiesto4 <wojtek@figat.pl>
2017-02-14 15:39:13 +0100

    content pool interop added

---
+++ FlaxEngine/Partial/Content.cs
@@ -0,0 +1,18 @@
+﻿// Flax Engine scripting API
+
+using System;
+
+namespace FlaxEngine
+{
+    public static partial class Content
+    {
+        // TODO: expose import files methods (only in editor?)
+        // TODO: expose create assets methods (only in editor?)
+        // TODO: expose create temporary asset path method
+        // TODO: AssetsCount property
+        // TODO: assets list get
+        // TODO: GetAsset() methods
+        // TODO: Delete, Rename, Unload APIs
+        // TODO: CreateVirtualAsset method
+    }
+}
