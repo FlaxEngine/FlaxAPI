c443f26327449308692249721923ff969de4f3e1
mafiesto4 <wojtek@figat.pl>
2017-05-28 22:52:10 +0900

    [Engine] Added SceneManager interop

---
+++ FlaxEngine/API/Static/SceneManager.cs
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlaxEngine
+{
+	/// <summary>
+	/// The scene manager handles the scenes collection and spawns/deleted actors.
+	/// </summary>
+	public static partial class SceneManager
+	{
+		//TODO: This was auto-generated template made to keep file structure flow strict. Please add your custom implementations if needed.
+	}
+}
