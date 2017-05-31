f6edb144fe6131a0463dbb18df3e6bea17767c88
mafiesto4 <wojtek@figat.pl>
2017-05-31 18:37:20 +0900

    [Scene] Added scene actor API for save/unload actions

--- FlaxEngine/API/Actors/Scene.cs
+++ FlaxEngine/API/Actors/Scene.cs
@@ -12,5 +12,51 @@ namespace FlaxEngine
 {
 	public sealed partial class Scene
 	{
-	}
+	    /// <summary>
+	    /// Saves this scene to the asset.
+	    /// </summary>
+	    /// <returns>True if action fails, otherwise false.</returns>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+	    public bool SaveScene(Scene scene)
+	    {
+	        return SceneManager.SaveScene(this);
+        }
+
+	    /// <summary>
+	    /// Saves this scene to the asset. Done in the background.
+	    /// </summary>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+	    public void SaveSceneAsync(Scene scene)
+	    {
+	        SceneManager.SaveSceneAsync(this);
+	    }
+
+	    /// <summary>
+        /// Unloads this scene.
+        /// </summary>
+        /// <returns>True if action fails, otherwise false.</returns>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        public bool UnloadScene(Scene scene)
+	    {
+	        return SceneManager.UnloadScene(this);
+        }
+
+        /// <summary>
+        /// Unloads this scene. Done in the background.
+        /// </summary>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        public void UnloadSceneAsync(Scene scene)
+	    {
+	        SceneManager.UnloadSceneAsync(this);
+	    }
+
+    }
 }
