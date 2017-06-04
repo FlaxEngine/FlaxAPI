5080f775f0b214dac180580d190a41922a8f506b
mafiesto4 <wojtek@figat.pl>
2017-06-04 16:38:59 +0900

    [Engine.Tests] Added math tests

---
+++ FlaxEngine.Tests/TestFloatR11G11B10.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlaxEngine.Tests
+{
+    [TestClass]
+    public class TestFloatR11G11B10
+    {
+        [TestMethod]
+        public void TestConversion()
+        {
+            Assert.AreEqual(Vector3.Zero, new FloatR11G11B10(Vector3.Zero).ToVector3());
+            Assert.AreEqual(Vector3.One, new FloatR11G11B10(Vector3.One).ToVector3());
+            Assert.AreEqual(new Vector3(0.5f, 0.5f, 0.5f), new FloatR11G11B10(new Vector3(0.5f)).ToVector3());
+            Assert.AreEqual(new Vector3(1, 0, 0), new FloatR11G11B10(new Vector3(1, 0, 0)).ToVector3());
+            Assert.AreEqual(new Vector3(0, 1, 0), new FloatR11G11B10(new Vector3(0, 1, 0)).ToVector3());
+            Assert.AreEqual(new Vector3(0, 0, 1), new FloatR11G11B10(new Vector3(0, 0, 1)).ToVector3());
+            Assert.AreEqual(new Vector3(10, 11, 12), new FloatR11G11B10(new Vector3(10, 11, 12)).ToVector3());
+        }
+    }
+}
