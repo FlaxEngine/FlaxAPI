da3676d72579fd8b470ca19c19cb02319862687d
mafiesto4 <wojtek@figat.pl>
2017-06-08 21:39:43 +0900

    [Editor] Move editor code to diffrent file

--- FlaxEditor/API/Editor.cs
+++ FlaxEditor/API/Editor.cs
@@ -4,17 +4,181 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FlaxEditor.Modules;
+using FlaxEditor.States;
+using FlaxEngine;
 
 namespace FlaxEditor
 {
-	/// <summary>
-	/// The main managed editor class. Editor root object.
-	/// </summary>
-	public partial class Editor
-	{
-		//TODO: This was auto-generated template made to keep file structure flow strict. Please add your custom implementations if needed.
-	}
+    public sealed partial class Editor
+    {
+        private readonly List<EditorModule> _modules = new List<EditorModule>(16);
+        private bool _isAfterInit;
+
+        /// <summary>
+        /// The windows module.
+        /// </summary>
+        public readonly WindowsModule Windows;
+
+        /// <summary>
+        /// The UI module.
+        /// </summary>
+        public readonly UIModule UI;
+
+        /// <summary>
+        /// The thumbnails module.
+        /// </summary>
+        public readonly ThumbnailsModule Thumbnails;
+
+        /// <summary>
+        /// The simulation module.
+        /// </summary>
+        public readonly SimulationModule Simulation;
+
+        /// <summary>
+        /// The scripting module.
+        /// </summary>
+        public readonly ScriptingModule Scripting;
+
+        /// <summary>
+        /// The scene module.
+        /// </summary>
+        public readonly SceneModule Scene;
+
+        /// <summary>
+        /// The progress reporting module.
+        /// </summary>
+        public readonly ProgressReportingModule ProgressReporting;
+
+        /// <summary>
+        /// The content editing module.
+        /// </summary>
+        public readonly ContentEditingModule ContentEditing;
+
+        /// <summary>
+        /// The content database module.
+        /// </summary>
+        public readonly ContentDatabaseModule ContentDatabase;
+
+        /// <summary>
+        /// The editor state machine.
+        /// </summary>
+        public readonly EditorStateMachine StateMachine;
+
+        internal Editor()
+        {
+            Debug.Log("Setting up C# Editor...");
+
+            // Create common editor modules
+            RegisterModule(Windows = new WindowsModule(this));
+            RegisterModule(UI = new UIModule(this));
+            RegisterModule(Thumbnails = new ThumbnailsModule(this));
+            RegisterModule(Simulation = new SimulationModule(this));
+            RegisterModule(Scripting = new ScriptingModule(this));
+            RegisterModule(Scene = new SceneModule(this));
+            RegisterModule(ProgressReporting = new ProgressReportingModule(this));
+            RegisterModule(ContentEditing = new ContentEditingModule(this));
+            RegisterModule(ContentDatabase = new ContentDatabaseModule(this));
+
+            // Create state machine
+            StateMachine = new EditorStateMachine(this);
+        }
+
+        internal void RegisterModule(EditorModule module)
+        {
+            Debug.Log("Register Editor module " + module);
+
+            _modules.Add(module);
+            if (_isAfterInit)
+                _modules.Sort((a, b) => a.InitOrder - b.InitOrder);
+        }
+
+        internal void Init()
+        {
+            EnsureState<LoadingState>();
+            Debug.Log("Editor init");
+
+            // Note: we don't sort modules before Init (optimized)
+            _modules.Sort((a, b) => a.InitOrder - b.InitOrder);
+            _isAfterInit = true;
+
+            // Initialize modules (from front to back)
+            for (int i = 0; i < _modules.Count; i++)
+            {
+                _modules[i].OnInit();
+            }
+
+            // Start Editor initalization ending phrase (will wait for scripts compilation result)
+            StateMachine.LoadingState.StartInitEnding();
+        }
+
+        internal void EndInit()
+        {
+            EnsureState<LoadingState>();
+            Debug.Log("Editor end init");
+
+            // Change state
+            StateMachine.GoToState<EditingSceneState>();
+
+            // Initialize modules (from front to back)
+            for (int i = 0; i < _modules.Count; i++)
+            {
+                _modules[i].OnEndInit();
+            }
+        }
+
+        internal void Update()
+        {
+            try
+            {
+                // Update state machine
+                StateMachine.Update();
+
+                // Update modules
+                for (int i = 0; i < _modules.Count; i++)
+                {
+                    _modules[i].OnUpdate();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+
+        internal void Exit()
+        {
+            Debug.Log("Editor exit");
+
+            // Start exit
+            StateMachine.GoToState<ClosingState>();
+
+            // Release modules (from back to front)
+            for (int i = _modules.Count - 1; i >= 0; i--)
+            {
+                _modules[i].OnExit();
+            }
+        }
+
+        /// <summary>
+        /// Ensure that editor is in a given state, otherwise throws <see cref="InvalidStateException"/>.
+        /// </summary>
+        /// <param name="state">Valid state to check.</param>
+        /// <exception cref="InvalidStateException"></exception>
+        public void EnsureState(EditorState state)
+        {
+            if (StateMachine.CurrentState != state)
+                throw new InvalidStateException($"Operation cannot be performed in the current editor state. Current: {StateMachine.CurrentState}, Expected: {state}");
+        }
+
+        /// <summary>
+        /// Ensure that editor is in a state of given type, otherwise throws <see cref="InvalidStateException"/>.
+        /// </summary>
+        /// <typeparam name="TStateType">The type of the state type.</typeparam>
+        public void EnsureState<TStateType>()
+        {
+            var state = StateMachine.GetState<TStateType>() as EditorState;
+            EnsureState(state);
+        }
+    }
 }
