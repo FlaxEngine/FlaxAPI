5cf665715cbc4d15e44283387ec212ad251491c1
mafiesto4 <wojtek@figat.pl>
2017-06-08 22:16:52 +0900

    [Editor] Editor Windows scene events and Windows Module setup

--- FlaxEditor/Modules/WindowsModule.cs
+++ FlaxEditor/Modules/WindowsModule.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FlaxEditor.Windows;
+using FlaxEngine;
 
 namespace FlaxEditor.Modules
 {
@@ -15,6 +16,23 @@ namespace FlaxEditor.Modules
     /// <seealso cref="FlaxEditor.Modules.EditorModule" />
     public sealed class WindowsModule : EditorModule
     {
+        /// <summary>
+        /// The default workspace layout name.
+        /// </summary>
+        public static string DefaultLayoutName = "Default";
+
+        /// <summary>
+        /// The working workspace layout name.
+        /// </summary>
+        public static string WorkingLayoutName = "Current";
+
+        private DateTime _lastLayoutSaveTime;
+
+        /// <summary>
+        /// The main editor window.
+        /// </summary>
+        public FlaxEngine.Window MainWindow { get; private set; }
+
         /// <summary>
         /// List with all created editor windows.
         /// </summary>
@@ -26,5 +44,159 @@ namespace FlaxEditor.Modules
             // Init windows module first
             InitOrder = -100;
         }
+
+        /// <summary>
+        /// Flash main editor window to catch user attention
+        /// </summary>
+        public void FlashMainWindow()
+        {
+            MainWindow?.FlashWindow();
+        }
+
+        /// <summary>
+        /// Closes all windows that are using given element to view/edit
+        /// </summary>
+        /// TODO: finsih this
+        /*public void CloseAllEditors(ContentElement el);
+        {
+            for (int i = 0; i < Windows.Count; i++)
+            {
+                auto win = Windows[i];
+                if (win.IsEditingElement(el))
+                {
+                    win.Close();
+                }
+            }
+        }
+        */
+
+        /// <summary>
+        /// Saves the current workspace layout.
+        /// </summary>
+        public void SaveCurrentLayout()
+        {
+            _lastLayoutSaveTime = DateTime.UtcNow;
+            SaveLayout(WorkingLayoutName);
+        }
+
+        /// <summary>
+        /// Loads the default workspace layout.
+        /// </summary>
+        public void LoadDefaultLayout()
+        {
+            LoadLayout(DefaultLayoutName);
+        }
+
+        /// <summary>
+        /// Loads the layout.
+        /// </summary>
+        /// <param name="name">The layout name.</param>
+        public void LoadLayout(string name)
+        {
+            // TODO: finish this
+        }
+
+        /// <summary>
+        /// Saves the layout.
+        /// </summary>
+        /// <param name="name">The layout name.</param>
+        public void SaveLayout(string name)
+        {
+            // TODO: finish this
+        }
+
+        /// <inheritdoc />
+        public override void OnInit()
+        {
+            // TODO: create default editor windows here
+
+            // Bind events
+            SceneManager.OnSceneSaveError += OnSceneSaveError;
+            SceneManager.OnSceneLoaded += OnSceneLoaded;
+            SceneManager.OnSceneLoadError += OnSceneLoadError;
+            SceneManager.OnSceneLoading += OnSceneLoading;
+            SceneManager.OnSceneSaved += OnSceneSaved;
+            SceneManager.OnSceneSaving += OnSceneSaving;
+            SceneManager.OnSceneUnloaded += OnSceneUnloaded;
+            SceneManager.OnSceneUnloading += OnSceneUnloading;
+
+            // TODO: link for OnScriptsReloadStart/OnScriptsReloadEnd events and don't fire scene events on scripts reload?
+        }
+
+        /// <inheritdoc />
+        public override void OnEndInit()
+        {
+            // Load current workspace layout
+            LoadLayout(WorkingLayoutName);
+
+            // Clear timer flag
+            _lastLayoutSaveTime = DateTime.UtcNow;
+        }
+
+        /// <inheritdoc />
+        public override void OnExit()
+        {
+            // Unbind events
+            SceneManager.OnSceneSaveError -= OnSceneSaveError;
+            SceneManager.OnSceneLoaded -= OnSceneLoaded;
+            SceneManager.OnSceneLoadError -= OnSceneLoadError;
+            SceneManager.OnSceneLoading -= OnSceneLoading;
+            SceneManager.OnSceneSaved -= OnSceneSaved;
+            SceneManager.OnSceneSaving -= OnSceneSaving;
+            SceneManager.OnSceneUnloaded -= OnSceneUnloaded;
+            SceneManager.OnSceneUnloading -= OnSceneUnloading;
+        }
+
+        #region Window Events
+
+        private void OnSceneSaveError(Scene scene, Guid sceneId)
+        {
+            for (int i = 0; i < Windows.Count; i++)
+                Windows[i].OnSceneSaveError(scene, sceneId);
+        }
+
+        private void OnSceneLoaded(Scene scene, Guid sceneId)
+        {
+            for (int i = 0; i < Windows.Count; i++)
+                Windows[i].OnSceneLoaded(scene, sceneId);
+        }
+
+        private void OnSceneLoadError(Scene scene, Guid sceneId)
+        {
+            for (int i = 0; i < Windows.Count; i++)
+                Windows[i].OnSceneLoadError(scene, sceneId);
+        }
+
+        private void OnSceneLoading(Scene scene, Guid sceneId)
+        {
+            for (int i = 0; i < Windows.Count; i++)
+                Windows[i].OnSceneLoading(scene, sceneId);
+        }
+
+        private void OnSceneSaved(Scene scene, Guid sceneId)
+        {
+            for (int i = 0; i < Windows.Count; i++)
+                Windows[i].OnSceneSaved(scene, sceneId);
+        }
+
+        private void OnSceneSaving(Scene scene, Guid sceneId)
+        {
+            for (int i = 0; i < Windows.Count; i++)
+                Windows[i].OnSceneSaving(scene, sceneId);
+        }
+
+        private void OnSceneUnloaded(Scene scene, Guid sceneId)
+        {
+            for (int i = 0; i < Windows.Count; i++)
+                Windows[i].OnSceneUnloaded(scene, sceneId);
+        }
+
+        private void OnSceneUnloading(Scene scene, Guid sceneId)
+        {
+            for (int i = 0; i < Windows.Count; i++)
+                Windows[i].OnSceneUnloading(scene, sceneId);
+        }
+
+        #endregion
     }
 }
--- FlaxEditor/Windows/EditorWindow.cs
+++ FlaxEditor/Windows/EditorWindow.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlaxEngine;
 using FlaxEngine.GUI;
 using FlaxEngine.GUI.Docking;
 
@@ -14,7 +15,7 @@ namespace FlaxEditor.Windows
     ///  Base class for all windows in Editor.
     /// </summary>
     /// <seealso cref="FlaxEngine.GUI.Docking.DockWindow" />
-    public class EditorWindow : DockWindow
+    public abstract class EditorWindow : DockWindow
     {
         /// <summary>
         /// Gets the editor object.
@@ -35,6 +36,82 @@ namespace FlaxEditor.Windows
             // Register
             Editor.Windows.Windows.Add(this);
         }
+
+        #region Window Events
+
+        /// <summary>
+        /// Fired when scene starts saving
+        /// </summary>
+        /// <param name="scene">The scene object. It may be null!</param>
+        /// <param name="sceneId">The scene ID.</param>
+        public virtual void OnSceneSaving(Scene scene, Guid sceneId)
+        {
+        }
+
+        /// <summary>
+        /// Fired when scene gets saved
+        /// </summary>
+        /// <param name="scene">The scene object. It may be null!</param>
+        /// <param name="sceneId">The scene ID.</param>
+        public virtual void OnSceneSaved(Scene scene, Guid sceneId)
+        {
+        }
+
+        /// <summary>
+        /// Fired when scene gets saving error
+        /// </summary>
+        /// <param name="scene">The scene object. It may be null!</param>
+        /// <param name="sceneId">The scene ID.</param>
+        public virtual void OnSceneSaveError(Scene scene, Guid sceneId)
+        {
+        }
+
+        /// <summary>
+        /// Fired when scene starts loading
+        /// </summary>
+        /// <param name="scene">The scene object. It may be null!</param>
+        /// <param name="sceneId">The scene ID.</param>
+        public virtual void OnSceneLoading(Scene scene, Guid sceneId)
+        {
+        }
+
+        /// <summary>
+        /// Fired when scene gets loaded
+        /// </summary>
+        /// <param name="scene">The scene object. It may be null!</param>
+        /// <param name="sceneId">The scene ID.</param>
+        public virtual void OnSceneLoaded(Scene scene, Guid sceneId)
+        {
+        }
+
+        /// <summary>
+        /// Fired when scene cannot be loaded
+        /// </summary>
+        /// <param name="scene">The scene object. It may be null!</param>
+        /// <param name="sceneId">The scene ID.</param>
+        public virtual void OnSceneLoadError(Scene scene, Guid sceneId)
+        {
+        }
+
+        /// <summary>
+        /// Fired when scene gets unloading
+        /// </summary>
+        /// <param name="scene">The scene object. It may be null!</param>
+        /// <param name="sceneId">The scene ID.</param>
+        public virtual void OnSceneUnloading(Scene scene, Guid sceneId)
+        {
+        }
+
+        /// <summary>
+        /// Fired when scene gets unloaded
+        /// </summary>
+        /// <param name="scene">The scene object. It may be null!</param>
+        /// <param name="sceneId">The scene ID.</param>
+        public virtual void OnSceneUnloaded(Scene scene, Guid sceneId)
+        {
+        }
+
+        #endregion
 
         /// <inheritdoc />
         public override void OnDestroy()
