d36761bac7558c7f83e9adee89eaecadc32d7bf3
mafiesto4 <wojtek@figat.pl>
2017-06-12 01:37:58 +0900

    [Engine] Added Sprite Atlas GetSprite interop and Sprite structure type

---
+++ FlaxEngine/API/Assets/SpriteAtlas.cs
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlaxEngine
+{
+    /// <summary>
+    /// Sprite handle contains basic information about a sprite.
+    /// </summary>
+    public struct Sprite
+    {
+        /// <summary>
+        /// Invalid sprite handle.
+        /// </summary>
+        public static Sprite Invalid;
+
+        /// <summary>
+        /// The parent sprite atlas.
+        /// </summary>
+        public SpriteAtlas Atlas;
+
+        /// <summary>
+        /// The sprite index.
+        /// </summary>
+        public int Index;
+
+        /// <summary>
+        /// Returns true if sprite is valid.
+        /// </summary>
+        /// <returns>True if this sprite handle is valid, otherwise false.</returns>
+        public bool IsValid => Atlas != null && Index != -1;
+    }
+
+    public partial class SpriteAtlas
+	{
+		// TODO: This was auto-generated template made to keep file structure flow strict. Please add your custom implementations if needed.
+	}
+}
