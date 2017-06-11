27dca674983636c40095f8195a7065d6f812168a
mafiesto4 <wojtek@figat.pl>
2017-06-11 14:55:49 +0900

    [GUI] Added main menu control

---
+++ FlaxEditor/GUI/MainMenu.cs
@@ -0,0 +1,88 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.GUI
+{
+    /// <summary>
+    /// Menu strip with child buttons.
+    /// </summary>
+    /// <seealso cref="FlaxEngine.GUI.ContainerControl" />
+    public sealed class MainMenu : ContainerControl
+    {
+        private MainMenuButton _selected;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainMenu"/> class.
+        /// </summary>
+        public MainMenu()
+            : base(false, 0, 0, 0, 0)
+        {
+        }
+
+        /// <summary>
+        /// Adds the button.
+        /// </summary>
+        /// <param name="text">The button text.</param>
+        /// <returns>Created button control.</returns>
+        public MainMenuButton AddButton(string text)
+        {
+            var button = new MainMenuButton(text);
+            button.Parent = this;
+            return button;
+        }
+
+        /// <summary>
+        /// Selects the specified button. Used by <see cref="MainMenuButton"/>.
+        /// </summary>
+        /// <param name="button">The button.</param>
+        internal void Select(MainMenuButton button)
+        {
+            // Check if popup menu has been already hidden
+            if (_selected != null && !_selected.ContextMenu.Visible)
+                _selected = null;
+
+            if (_selected != button)
+            {
+                _selected = button;
+
+                if (_selected != null && _selected.ContextMenu.HasChildren)
+                {
+                    _selected.ContextMenu.Show(_selected, new Vector2(0, _selected.Height));
+                }
+            }
+            else if (_selected != null)
+            {
+                _selected.ContextMenu.Hide();
+                _selected = null;
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Draw()
+        {
+            BackgroundColor = Style.Current.LightBackground;
+            base.Draw();
+        }
+
+        /// <inheritdoc />
+        protected override void PerformLayoutSelf()
+        {
+            // Arrange controls
+            float y = 2;
+            float x = 1;
+            for (int i = 0; i < _children.Count; i++)
+            {
+                var c = _children[i];
+                if (c.Visible)
+                {
+                    c.Location = new Vector2(x, y);
+                    x += c.Width + 2;
+                }
+            }
+        }
+    }
+}
---
+++ FlaxEditor/GUI/MainMenuButton.cs
@@ -0,0 +1,73 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.GUI
+{
+    /// <summary>
+    /// Main menu button control.
+    /// </summary>
+    /// <seealso cref="FlaxEngine.GUI.Control" />
+    public class MainMenuButton : Control
+    {
+        /// <summary>
+        /// The button text.
+        /// </summary>
+        public string Text;
+
+        /// <summary>
+        /// The context menu.
+        /// </summary>
+        public readonly ContextMenu ContextMenu = new ContextMenu();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainMenuButton"/> class.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        public MainMenuButton(string text)
+            : base(true, 0, 0, 32, 16)
+        {
+            Text = text;
+        }
+
+        /// <inheritdoc />
+        public override void Draw()
+        {
+            // Cache data
+            var style = Style.Current;
+            var clientRect = new Rectangle(0, 0, Width, Height);
+            var hasChildItems = ContextMenu.HasChildren;
+            var isOpened = ContextMenu.IsOpened;
+
+            // Draw background
+            if (Enabled && hasChildItems && (isOpened || IsMouseOver))
+                Render2D.FillRectangle(clientRect, isOpened ? style.Background : style.BackgroundHighlighted);
+
+            // Draw text
+            Render2D.DrawText(style.FontMedium, Text, clientRect, Enabled && hasChildItems ? style.Foreground : style.ForegroundDisabled, TextAlignment.Center, TextAlignment.Center);
+        }
+
+        /// <inheritdoc />
+        public override bool OnMouseDown(MouseButtons buttons, Vector2 location)
+        {
+            Focus();
+            (Parent as MainMenu)?.Select(this);
+            return true;
+        }
+
+        /// <inheritdoc />
+        public override void PerformLayout()
+        {
+            var style = Style.Current;
+            float width = 12;
+
+            if (style.FontMedium)
+                width += style.FontMedium.MeasureText(Text).X;
+
+            Width = width;
+        }
+    }
+}
