42ac67fe24f7ffdefdc1757db23d6350b8ecc568
mafiesto4 <wojtek@figat.pl>
2017-06-13 02:39:08 +0900

    [GUI] Added Toolstrip control and related UI parts

---
+++ FlaxEditor/GUI/ToolStrip.cs
@@ -0,0 +1,176 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.GUI
+{
+    /// <summary>
+    /// Tool strip with child items.
+    /// </summary>
+    /// <seealso cref="FlaxEngine.GUI.ContainerControl" />
+    public class ToolStrip : ContainerControl
+    {
+        public const int DefaultMarginV = 1;
+        public const int DefaultMarginH = 2;
+        public const int DefaultHeight = 34;
+
+        /// <summary>
+        /// Event fired when button gets clicked.
+        /// </summary>
+        public Action<int> OnButtonClicked;
+
+        /// <summary>
+        /// Tries to get the last button.
+        /// </summary>
+        public ToolStripButton LastButton
+        {
+            get
+            {
+                for (int i = _children.Count - 1; i >= 0; i--)
+                {
+                    if (_children[i] is ToolStripButton button)
+                        return button;
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets amount of buttons that has been added
+        /// </summary>
+        /// <returns>Buttons count</returns>
+        public int ButtonsCount
+        {
+            get
+            {
+                int result = 0;
+                for (int i = 0; i < _children.Count; i++)
+                {
+                    if (_children[i] is ToolStripButton)
+                        result++;
+                }
+                return result;
+            }
+        }
+
+	    /// <summary>
+	    /// Gets the height for the items.
+	    /// </summary>
+	    public float ItemsHeight => Height - 2 * DefaultMarginV;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolStrip"/> class.
+        /// </summary>
+        /// <param name="height">The height.</param>
+        public ToolStrip(float height = DefaultHeight)
+            : base(false, 0, 0, 100, height)
+        {
+            DockStyle = DockStyle.Top;
+        }
+
+        /// <summary>
+        /// Tries to get button with given id.
+        /// </summary>
+        /// <param name="id">The button id.</param>
+        /// <returns>Found button or null.</returns>
+        public ToolStripButton GetButton(int id)
+        {
+            // TODO: we could build cache for buttons to access O(1) from lookup by id
+
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (_children[i] is ToolStripButton button && button.ID == id)
+                {
+                    return button;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Adds the button.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="sprite">The icon sprite.</param>
+        /// <returns>The button.</returns>
+        public ToolStripButton AddButton(int id, Sprite sprite)
+        {
+            return AddChild(new ToolStripButton(ItemsHeight, id, ref sprite));
+        }
+
+        /// <summary>
+        /// Adds the button.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="sprite">The icon sprite.</param>
+        /// <param name="text">The text.</param>
+        /// <returns>The button.</returns>
+        public ToolStripButton AddButton(int id, Sprite sprite, string text)
+        {
+            return AddChild(new ToolStripButton(ItemsHeight, id, ref sprite, ref text));
+        }
+
+        /// <summary>
+        /// Adds the button.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="text">The text.</param>
+        /// <returns>The button.</returns>
+        public ToolStripButton AddButton(int id, string text)
+        {
+            return AddChild(new ToolStripButton(ItemsHeight, id, ref Sprite.Invalid, ref text));
+        }
+
+        /// <summary>
+        /// Adds the separator.
+        /// </summary>
+        /// <returns>The separator.</returns>
+        public ToolStripSeparator AddSeparator()
+        {
+            return AddChild(new ToolStripSeparator(ItemsHeight));
+        }
+
+        internal void OnButtonClickedInternal(int id)
+        {
+            OnButtonClicked?.Invoke(id);
+        }
+
+        /// <inheritdoc />
+        public override void Draw()
+        {
+            BackgroundColor = Style.Current.LightBackground;
+            base.Draw();
+        }
+
+        /// <inheritdoc />
+        protected override void PerformLayoutSelf()
+        {
+            // Arrange controls
+            float x = DefaultMarginH;
+            float h = ItemsHeight;
+            for (int i = 0; i < _children.Count; i++)
+            {
+                var c = _children[i];
+                if (c.Visible)
+                {
+                    var w = c.Width;
+                    c.Bounds = new Rectangle(x, DefaultMarginV, w, h);
+                    x += w + DefaultMarginH;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public override void OnChildResized(Control control)
+        {
+            base.OnChildResized(control);
+
+            PerformLayout();
+        }
+    }
+}
---
+++ FlaxEditor/GUI/ToolStripButton.cs
@@ -0,0 +1,217 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.GUI
+{
+    /// <summary>
+    /// Tool strip button control.
+    /// </summary>
+    /// <seealso cref="FlaxEngine.GUI.Control" />
+    public class ToolStripButton : Control
+    {
+        /// <summary>
+        /// The default margin for button parts (icon, text, etc.).
+        /// </summary>
+        public const int DefaultMargin = 2;
+
+        private int _id;
+        private Sprite _icon;
+        private string _text;
+        private bool _mouseDown;
+
+        /// <summary>
+        /// Event fired when user clicks the button.
+        /// </summary>
+        public Action OnClicked;
+
+        /// <summary>
+        /// The checked state.
+        /// </summary>
+        public bool Checked;
+
+        /// <summary>
+        /// The automatic check mode.
+        /// </summary>
+        public bool AutoCheck;
+
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int ID => _id;
+
+        /// <summary>
+        /// The icon.
+        /// </summary>
+        public Sprite Icon
+        {
+            get { return _icon; }
+            set
+            {
+                _icon = value;
+                PerformLayout();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the text.
+        /// </summary>
+        /// <value>
+        /// The text.
+        /// </value>
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    PerformLayout();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolStripButton"/> class.
+        /// </summary>
+        /// <param name="height">The height.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="icon">The icon.</param>
+        public ToolStripButton(float height, int id, ref Sprite icon)
+            : base(true, 0, 0, height, height)
+        {
+            _id = id;
+            _icon = icon;
+            _text = string.Empty;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolStripButton"/> class.
+        /// </summary>
+        /// <param name="height">The height.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="icon">The icon.</param>
+        /// <param name="text">The text.</param>
+        public ToolStripButton(float height, int id, ref Sprite icon, ref string text)
+            : base(true, 0, 0, height, height)
+        {
+            _id = id;
+            _icon = icon;
+            _text = text;
+        }
+
+        /// <inheritdoc />
+        public override void Draw()
+        {
+            base.Draw();
+
+            // Cache data
+            var style = Style.Current;
+            float iconSize = Height - DefaultMargin;
+            var clientRect = new Rectangle(Vector2.Zero, Size);
+            var iconRect = new Rectangle(DefaultMargin, DefaultMargin, iconSize, iconSize);
+            var textRect = new Rectangle(DefaultMargin, 0, 0, Height);
+
+            // Draw background
+            if (IsMouseOver || Checked)
+                Render2D.FillRectangle(clientRect, Checked ? style.BackgroundSelected : _mouseDown ? style.BackgroundHighlighted : (style.LightBackground * 1.3f));
+
+            // Draw icon
+            if (_icon.IsValid)
+            {
+                Render2D.DrawSprite(_icon, iconRect, Enabled ? Color.White : style.ForegroundDisabled, true);
+                textRect.Location.X += iconSize + DefaultMargin;
+            }
+
+            // Draw text
+            if (_text.Length > 0)
+            {
+                textRect.Size.X = Width - DefaultMargin - textRect.Left;
+                Render2D.DrawText(
+                    style.FontMedium,
+                    _text,
+                    textRect,
+                    Enabled ? style.Foreground : style.ForegroundDisabled,
+                    TextAlignment.Near,
+                    TextAlignment.Center);
+            }
+        }
+
+        /// <inheritdoc />
+        public override void PerformLayout()
+        {
+            var style = Style.Current;
+            float iconSize = Height - DefaultMargin;
+            bool hasSprite = _icon.IsValid;
+            float width = DefaultMargin * 2;
+
+            if (hasSprite)
+                width += iconSize;
+            if (_text.Length > 0 && style.FontMedium)
+                width += style.FontMedium.MeasureText(_text).X + (hasSprite ? DefaultMargin : 0);
+
+            Width = width;
+        }
+
+        /// <inheritdoc />
+        public override bool OnMouseDown(Vector2 location, MouseButtons buttons)
+        {
+            if (buttons == MouseButtons.Left)
+            {
+                // Set flag
+                _mouseDown = true;
+
+                Focus();
+                return true;
+            }
+
+            return base.OnMouseDown(location, buttons);
+        }
+
+        /// <inheritdoc />
+        public override bool OnMouseUp(Vector2 location, MouseButtons buttons)
+        {
+            if (buttons == MouseButtons.Left && _mouseDown)
+            {
+                // Clear flag
+                _mouseDown = false;
+
+                // Fire events
+                if (AutoCheck)
+                    Checked = !Checked;
+                OnClicked?.Invoke();
+                (Parent as ToolStrip)?.OnButtonClickedInternal(_id);
+
+                return true;
+            }
+
+            return base.OnMouseUp(location, buttons);
+        }
+
+        /// <inheritdoc />
+        public override void OnMouseLeave()
+        {
+            // Clear flag
+            _mouseDown = false;
+
+            base.OnMouseLeave();
+        }
+
+        /// <inheritdoc />
+        public override void OnLostFocus()
+        {
+            // Clear flag
+            _mouseDown = false;
+
+            base.OnLostFocus();
+        }
+    }
+}
