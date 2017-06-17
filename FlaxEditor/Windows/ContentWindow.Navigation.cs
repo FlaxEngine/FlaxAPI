81f56a4a46387279b3e6ec41336a4926ae3d6147
mafiesto4 <wojtek@figat.pl>
2017-06-17 23:56:22 +0900

    [Editor] Added Content Window code

---
+++ FlaxEditor/Windows/ContentWindow.Navigation.cs
@@ -0,0 +1,239 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections.Generic;
+using FlaxEditor.Content;
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.Windows
+{
+    public partial class ContentWindow
+    {
+        private void treeOnSelectedChanged(List<TreeNode> from, List<TreeNode> to)
+        {
+            // Navigate
+            var source = from.Count > 0 ? from[0] as ContentTreeNode : null;
+            var target = to.Count > 0 ? to[0] as ContentTreeNode : null;
+            navigate(source, target);
+        }
+
+        private void navigate(ContentTreeNode target)
+        {
+            navigate(SelectedNode, target);
+        }
+
+        private void navigate(ContentTreeNode source, ContentTreeNode target)
+        {
+            // TODO: finish this
+            /*// Check if can do this action
+            if (target != null && _navigationUnlocked && source != target)
+            {
+                // Lock navigation
+                _navigationUnlocked = false;
+
+                // Check if already added to the Undo on the top
+                if (source != null && (_navigationUndo.Count == 0 || _navigationUndo[0] != source))
+                {
+                    // Add to Undo list
+                    _navigationUndo.Insert(0, source);
+                }
+
+                // Show folder contents and select tree node
+                if (target == _root)
+                {
+                    // Special case for root folder
+                    List<ContentItem> elements = new List<ContentItem>(8);
+                    for (int i = 0; i < _root.ChildrenCount; i++)
+                    {
+                        if (_root.GetChild(i) is ContentTreeNode node)
+                        {
+                            elements.Add(node.Folder);
+                        }
+                    }
+                    _view.ShowElements(elements);
+                }
+                else
+                {
+                    // Show folder contents
+                    _view.ShowElements(target.Folder.Children);
+                }
+                _tree.Select(target);
+                target.ExpandAllParents();
+
+                // Clear redo list
+                _navigationRedo.Clear();
+
+                // Set valid sizes for stacks
+                //RedoList.SetSize(32);
+                //UndoList.SetSize(32);
+
+                // Unlcok navigation
+                _navigationUnlocked = true;
+
+                // Update UI
+                updateToolstrip();
+            }
+
+            _view.Focus();*/
+        }
+
+        private void navigateBackward()
+        {
+            // TODO: finish this
+            /*// Check if navigation is unlocked
+            if (_navigationUnlocked && _navigationUndo.Count > 0)
+            {
+                // Pop node
+                ContentTreeNode node = _navigationUndo[0];
+                _navigationUndo.RemoveAtKeepOrder(0);
+
+                // Lock navigation
+                _navigationUnlocked = false;
+
+                // Add to Redo list
+                _navigationRedo.Insert(0, SelectedNode);
+
+                // Select node
+                _view.ShowElements(node.Folder.Children);
+                _tree.Select(node);
+                node.ExpandAllParents();
+
+                // Set valid sizes for stacks
+                //RedoList.SetSize(32);
+                //UndoList.SetSize(32);
+
+                // Clear search form and update view
+                //ClearSearch();
+
+                // Unlcok navigation
+                _navigationUnlocked = true;
+
+                // Update UI
+                updateToolstrip();
+
+                // TODO: select that folder to use arrows to navigate easly
+            }
+
+            _view.Focus();*/
+        }
+
+        private void navigateForward()
+        {
+            // TODO: finish this
+            /*// Check if navigation is unlocked
+            if (_navigationUnlocked && _navigationRedo.Count > 0)
+            {
+                // Pop node
+                ContentTreeNode node = _navigationRedo[0];
+                _navigationRedo.RemoveAtKeepOrder(0);
+
+                // Lock navigation
+                _navigationUnlocked = false;
+
+                // Add to Undo list
+                _navigationUndo.Insert(0, SelectedNode);
+
+                // Select node
+                _view.ShowElements(node.Folder.Children);
+                _tree.Select(node);
+                node.ExpandAllParents();
+
+                // Set valid sizes for stacks
+                //RedoList.SetSize(32);
+                //UndoList.SetSize(32);
+
+                // Clear search form and update view
+                //ClearSearch();
+
+                // Unlcok navigation
+                _navigationUnlocked = true;
+
+                // Update UI
+                updateToolstrip();
+            }
+
+            _view.Focus();*/
+        }
+
+        private void navigateUp()
+        {
+            ContentTreeNode target = _root;
+            ContentTreeNode current = SelectedNode;
+
+            if (current?.Folder.ParentFolder != null)
+            {
+                target = current.Folder.ParentFolder.Node;
+            }
+
+            navigate(target);
+        }
+
+        private  void navigationClearHistory()
+        {
+            _navigationUndo.Clear();
+            _navigationRedo.Clear();
+            updateToolstrip();
+        }
+
+        private void navigationBarUpdate()
+        {
+            // TODO: finish this
+            // Remove previous buttons
+            /*_navigationBar.DeleteChildren();
+
+            // Spawn buttons
+            Array<ContentTreeNode*> nodes(8);
+            ContentTreeNode* node = GetSelectedNode();
+            while (node != _root && node)
+            {
+                nodes.Add(node);
+                node = node.GetParentNode();
+            }
+            float x = 1;
+            float h = _toolStrip.GetItemsHeight();
+            for (int32 i = nodes.Count() - 1; i >= 0; i--)
+            {
+                var button = new CContentNavigationButton(h, nodes[i], x, CToolStrip_MarginV);
+                button.PerformLayout();
+                x += button.GetWidth() + 2;
+                _navigationBar.AddControl(button);
+            }
+
+            // Update
+            _navigationBar.PerformLayout();*/
+        }
+
+        /// <summary>
+        /// Gets the selected tree node.
+        /// </summary>
+        /// <value>
+        /// The selected node.
+        /// </value>
+        public ContentTreeNode SelectedNode => _tree.SelectedNode as ContentTreeNode;
+
+        /// <summary>
+        /// Gets the current view folder.
+        /// </summary>
+        /// <value>
+        /// The current view folder.
+        /// </value>
+        public ContentFolder CurrentViewFolder
+        {
+            get
+            {
+                var node = SelectedNode;
+                return node?.Folder;
+            }
+        }
+
+        /// <summary>
+        /// Shows the root folder.
+        /// </summary>
+        public void ShowRoot()
+        {
+            // Show root folder
+            _tree.Select(_root);
+        }
+    }
+}
--- FlaxEditor/Windows/ContentWindow.cs
+++ FlaxEditor/Windows/ContentWindow.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlaxEditor.Content;
+using FlaxEditor.Content.GUI;
+using FlaxEditor.GUI;
 using FlaxEngine.GUI;
 
 namespace FlaxEditor.Windows
@@ -16,11 +19,108 @@ namespace FlaxEditor.Windows
     /// Provides various functionalities for asset operations.
     /// </summary>
     /// <seealso cref="FlaxEditor.Windows.EditorWindow" />
-    public sealed class ContentWindow : EditorWindow
+    public sealed partial class ContentWindow : EditorWindow
     {
+        private bool _isReady;
+        private SplitPanel _split;
+        private ContentView _view;
+
+        private ToolStrip _toolStrip;
+
+        //private NavigationBar _navigationBar;
+        private Tree _tree;
+
+        private ContentTreeNode _root;
+
+        private bool _navigationUnlocked;
+        private readonly List<ContentTreeNode> _navigationUndo = new List<ContentTreeNode>(32);
+        private readonly List<ContentTreeNode> _navigationRedo = new List<ContentTreeNode>(32);
+
+        private NewItem _newElement;
+        //private AssetsPreviewManager _previewManager;
+
         internal ContentWindow(Editor editor)
             : base(editor, true, ScrollBars.None)
         {
+            // Tool strip
+            _toolStrip = new ToolStrip();
+            _toolStrip.AddButton(0, Editor.UI.GetIcon("Import32"));//.LinkTooltip(GetSharedTooltip(), "Import content");// Import
+            _toolStrip.AddSeparator();
+            _toolStrip.AddButton(1, Editor.UI.GetIcon("ArrowLeft32"));//.LinkTooltip(GetSharedTooltip(), "Navigate backward");// Backward
+            _toolStrip.AddButton(2, Editor.UI.GetIcon("ArrowRight32"));//.LinkTooltip(GetSharedTooltip(), "Navigate forward");// Forward
+            _toolStrip.AddButton(3, Editor.UI.GetIcon("ArrowUp32"));//.LinkTooltip(GetSharedTooltip(), "Navigate up");// Up
+            _toolStrip.OnButtonClicked += toolstripButtonClicked;
+            _toolStrip.Parent = this;
+
+            // Navigation bar
+            //_navigationBar = new CNavigationBar();
+            //_navigationBar.SetHeight(32);
+            //_navigationBar.Parent = this;
+
+            // Split panel
+            _split = new SplitPanel(Orientation.Horizontal, ScrollBars.Both, ScrollBars.Vertical);
+            _split.DockStyle = DockStyle.Fill;
+            _split.SplitterValue = 0.2f;
+            _split.Parent = this;
+
+            // Content structure tree
+            _tree = new Tree(false);
+            _tree.OnSelectedChanged += treeOnSelectedChanged;
+            _tree.Parent = _split.Panel1;
+
+            // Content View
+            _view = new ContentView();
+            //_view.OnOpen.Bind < ContentWindow, &ContentWindow::view_OnOpen > (this);
+            //_view.OnRename.Bind < ContentWindow, &ContentWindow::Rename > (this);
+            //_view.OnDelete.Bind < ContentWindow, &ContentWindow::view_OnDelete > (this);
+            //_view.OnDuplicate.Bind < ContentWindow, &ContentWindow::CloneSelection > (this);
+            //_view.OnNavigateBack.Bind < ContentWindow, &ContentWindow::navigateBackward > (this);
+            _view.Parent = _split.Panel2;
+        }
+
+        private void toolstripButtonClicked(int id)
+        {
+            /*switch (id)
+            {
+                // Import
+		        case 0: import(); break;
+
+                // Backward
+                case 1: navigateBackward(); break;
+
+                // Forward
+                case 2: navigateForward(); break;
+
+                // Up
+                case 3: navigateUp(); break;
+            }*/
+        }
+
+        private void viewOnOpen(ContentItem item)
+        {
+            //Open(item);
+        }
+
+        private void viewOnDelete()
+        {
+            // Check if has any selected items
+            //Delete(_view.Selection);
+        }
+
+        private void updateToolstrip()
+        {
+            if (_toolStrip == null)
+                return;
+
+            // Update buttons
+            var folder = CurrentViewFolder;
+            _toolStrip.GetButton(0).Enabled = folder != null && folder.CanHaveAssets;
+            _toolStrip.GetButton(1).Enabled = _navigationUndo.Count > 0;
+            _toolStrip.GetButton(2).Enabled = _navigationRedo.Count > 0;
+            _toolStrip.GetButton(3).Enabled = folder != null && _tree.SelectedNode != _root;
+
+            // Update navigation path
+            navigationBarUpdate();
         }
     }
 }
