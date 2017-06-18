6d59d53e5170241f3b955f2a9750b6bde61070a3
Tomasz Juszczak <99999deuter@gmail.com>
2017-06-18 21:53:56 +0200

    Added more PersistentData API

--- FlaxEngine/API/Objects/BasePersistentData.cs
+++ FlaxEngine/API/Objects/BasePersistentData.cs
@@ -12,6 +12,58 @@ namespace FlaxEngine
 {
 	public abstract partial class BasePersistentData
 	{
-		// TODO: This was auto-generated template made to keep file structure flow strict. Please add your custom implementations if needed.
-	}
+        /// <summary>
+        /// Gets persistant data from desired file.
+        /// </summary>
+        /// <param name="fileName">File to get persistant data from.</param>
+        /// <param name="createNew">If file was not found, should method create new file.</param>
+        /// <typeparam name="T">PersistentData expected type</typeparam>
+        /// <returns>null if file was not found.</returns>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        [UnmanagedCall]
+        public static T File<T>(string fileName, bool createNew = true) where T : BasePersistentData
+	    {
+#if UNIT_TEST_COMPILANT
+			throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+	        return (T)Internal_File(fileName, createNew);
+#endif
+	    }
+
+        /// <summary>
+        /// Gets persistant data from desired file by its config mapped name.
+        /// </summary>
+        /// <param name="fileName">File to get persistant data from by name from internal dictionary. Has to be mapped in config file.</param>
+        /// <param name="createNew">If file was not found, should method create new file.</param>
+        /// <typeparam name="T">PersistentData expected type</typeparam>
+        /// <returns>null if file was not found.</returns>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        [UnmanagedCall]
+	    public static T FileByName<T>(string fileName, bool createNew = true) where T : BasePersistentData
+	    {
+#if UNIT_TEST_COMPILANT
+			throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+	        return (T)Internal_FileByName(fileName, createNew);
+#endif
+	    }
+
+        /// <summary>
+        /// Internal counter for amount of changes insinde persistent data collection. 
+        /// </summary>
+        /// <remarks>
+        /// Increments number of changes made to current <see cref="BasePersistentData"/> and based on <see cref="PersistentDataConfig"/> 
+        /// if number is exceeded <see cref="BasePersistentData.Flush"/> is called. Multiple methods of automatic flush can be combined.
+        /// </remarks>
+        /// <param name="amount"></param>
+        /// <seealso cref="PersistentDataConfig"/>
+        public void Increment(int amount = 1)
+        {
+            
+        }
+    }
 }
---
+++ FlaxEngine/Attributes/PersisitentDataAttribute.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlaxEngine
+{
+    /// <summary>
+    ///     This attributes provides simple way of mapping data from fields and properties to persistant data structure.
+    /// </summary>
+    /// <remarks>
+    ///     Multiple ways of saving values are possible using this method.
+    ///     - Manual flush <see cref="BasePersistentData.Flush()" />
+    ///     - Time based flush <see cref="PersistentDataConfig" />
+    ///     - Incrementation manual counter <see cref="PersistentDataConfig" /> <see cref="BasePersistentData.Call()" />
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class PersisitentDataAttribute : Attribute
+    {
+        /// <summary>
+        ///     Override value for mapped "key" value
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        ///     Custom file name used to store this key-value pair
+        /// </summary>
+        public string File { get; set; }
+
+        /// <summary>
+        ///     Custom config name mapped to the File name for this key-value pair
+        /// </summary>
+        public string ConfigName { get; set; }
+
+        /// <summary>
+        ///     Maps marked field to the global persistant data file using default Field/Property name format
+        /// </summary>
+        public PersisitentDataAttribute()
+        {
+        }
+
+        /// <summary>
+        ///     Maps marked field to the global persistant data file with custom key format
+        /// </summary>
+        /// <param name="key">Override value for mapped "key" value</param>
+        public PersisitentDataAttribute(string key)
+        {
+            Key = key;
+        }
+
+        /// <summary>
+        ///     Maps marked field to the specified persistant data file with custom key format
+        /// </summary>
+        /// <param name="key">Override value for mapped "key" value</param>
+        /// <param name="file">Custom file name used to store this key-value pair</param>
+        /// <param name="isConfigName">If true, file name is instead configName</param>
+        public PersisitentDataAttribute(string key, string file, bool isConfigName = false)
+        {
+            Key = key;
+            if (isConfigName)
+            {
+                File = file;
+            }
+            else
+            {
+                ConfigName = file;
+            }
+        }
+    }
+}
