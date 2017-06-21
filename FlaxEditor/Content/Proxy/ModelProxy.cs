7f564c3cfeadfce5363a3c34d4a3b66dd3aabc66
mafiesto4 <wojtek@figat.pl>
2017-06-22 02:26:14 +0900

    [Editor] Added ModelProxy and TextureProxy

---
+++ FlaxEditor/Content/Proxy/ModelProxy.cs
@@ -0,0 +1,38 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using FlaxEditor.Windows;
+using FlaxEngine;
+
+namespace FlaxEditor.Content.Proxy
+{
+    /// <summary>
+    /// A <see cref="Model"/> asset proxy object.
+    /// </summary>
+    /// <seealso cref="FlaxEditor.Content.BinaryAssetProxy" />
+    public class ModelProxy : BinaryAssetProxy
+    {
+        /// <inheritdoc />
+        public override string Name => "Model";
+
+        /// <inheritdoc />
+        public override bool AcceptsTypeID(int typeID)
+        {
+            return typeID == Model.TypeID;
+        }
+
+        /// <inheritdoc />
+        public override EditorWindow Open(ContentItem item)
+        {
+            throw new NotImplementedException();// TODO: model window
+        }
+
+        /// <inheritdoc />
+        public override Color AccentColor => Color.FromRGB(0xe67e22);
+
+        /// <inheritdoc />
+        public override ContentDomain Domain => ContentDomain.Model;
+    }
+}
---
+++ FlaxEditor/Content/Proxy/TextureProxy.cs
@@ -0,0 +1,38 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using FlaxEditor.Windows;
+using FlaxEngine;
+
+namespace FlaxEditor.Content.Proxy
+{
+    /// <summary>
+    /// A <see cref="Texture"/> asset proxy object.
+    /// </summary>
+    /// <seealso cref="FlaxEditor.Content.BinaryAssetProxy" />
+    public class TextureProxy : BinaryAssetProxy
+    {
+        /// <inheritdoc />
+        public override string Name => "Texture";
+
+        /// <inheritdoc />
+        public override bool AcceptsTypeID(int typeID)
+        {
+            return typeID == Texture.TypeID;
+        }
+
+        /// <inheritdoc />
+        public override EditorWindow Open(ContentItem item)
+        {
+            throw new NotImplementedException();// TODO: texture window
+        }
+
+        /// <inheritdoc />
+        public override Color AccentColor => Color.FromRGB(0x25B84C);
+
+        /// <inheritdoc />
+        public override ContentDomain Domain => ContentDomain.Texture;
+    }
+}
