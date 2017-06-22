71928ee0ad61c6db0368cfc1443142b6124bd8e7
mafiesto4 <wojtek@figat.pl>
2017-06-23 00:24:35 +0900

    [Graphics] Expose RenderTask to C# API

---
+++ FlaxEngine/API/Objects/RenderTask.cs
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlaxEngine
+{
+	public partial class RenderTask
+	{
+		// TODO: This was auto-generated template made to keep file structure flow strict. Please add your custom implementations if needed.
+	}
+}
