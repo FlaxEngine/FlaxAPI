3e4f37b964918354f94b624f44c425ff280f0b2a
mafiesto4 <wojtek@figat.pl>
2017-06-24 14:29:01 +0900

    [Graphics] Added MSAALevel and TextureFlags enums

---
+++ FlaxEngine/Rendering/MSAALevel.cs
@@ -0,0 +1,32 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+namespace FlaxEngine.Rendering
+{
+    /// <summary>
+    /// Multisample count level.
+    /// </summary>
+    public enum MSAALevel
+    {
+        /// <summary>
+        /// Disabled multisampling.
+        /// </summary>
+        None = 1,
+
+        /// <summary>
+        /// Two samples per pixel.
+        /// </summary>
+        X2 = 2,
+
+        /// <summary>
+        /// Four samples per pixel.
+        /// </summary>
+        X4 = 4,
+
+        /// <summary>
+        /// Eight samples per pixel.
+        /// </summary>
+        X8 = 8
+    }
+}
