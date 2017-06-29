563c3572b873ae7c7a7057f10a80696e6f26db60
mafiesto4 <wojtek@figat.pl>
2017-06-30 02:15:03 +0900

    [Editor] More Transform Gizmo code

---
+++ FlaxEditor/Gizmo/TransformGizmo.Selection.cs
@@ -0,0 +1,165 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace FlaxEditor.Gizmo
+{
+    public partial class TransformGizmo
+    {
+        /// <summary>
+        /// Checks if given actor is selected.
+        /// </summary>
+        /// <param name="actor">Actor to check</param>
+        /// <returns>True if is selected, otherwise false.</returns>
+        public bool IsActorSelected(Actor actor)
+        {
+            return _selection.Contains(actor);
+        }
+
+        /// <summary>
+        /// Tries to select any actor.
+        /// </summary>
+        /// <param name="addRemove">True if use add/remove mode.</param>
+        public void PickActors(bool addRemove)
+        {
+            // Check if gismo is disaled
+            if (!IsActive)
+                return;
+
+            // Ensure player is not moving objects
+            if (_activeAxis != Axis.None)
+                return;
+            
+            // End current action
+            EndTransforming();
+
+            // Cache 'before' state
+            var before = new List<ITransformable>(_selection);
+
+            // Try to pick object
+            /*var mosueRay = _parent->GetMouseRay();
+            var hit = RaycastScene(mosueRay);
+
+            // Process test result
+            if (!addRemove && _selection.Count > 0)
+            {
+                _selection.Clear();
+
+            }
+            if (hit != null)
+            {
+                bool isSelected = _selection.Contains(hit);
+                if (addRemove)
+                {
+                    if (isSelected)
+                        _selection.Remove(hit);
+                    else
+                        _selection.Add(hit);
+                }
+                else if (!isSelected)
+                {
+                    _selection.Add(hit);
+                }
+            }*/
+
+            /*// Check if selection has been changed
+            var ur = _parent->GetUndoRedo();
+            if (before != _selection)
+            {
+                // Change selection
+                if (ur->IsDisabled())
+                {
+                    auto actors = _selection;
+                    select(actors, INVALID_INDEX);
+                }
+                else
+                {
+                    ur->AddAction(new SelectionChanged(this, before, _selection, _subObjectIndex, INVALID_INDEX))->Perform();
+                }
+            }
+            else
+            {
+                // Check if selected is only single object
+                if (_selection.Count == 1)
+                {
+                    // Try to get sub object
+                    int32 subObject = _selection[0]->IntersectsSubObjectEditor(mosueRay);
+
+                    // Check if has been changed
+                    if (_subObjectIndex != subObject)
+                    {
+                        // Change selection
+                        if (ur->IsDisabled())
+                        {
+                            auto actors = _selection;
+                            select(actors, subObject);
+                        }
+                        else
+                        {
+                            ur->AddAction(new SelectionChanged(this, before, _selection, _subObjectIndex, subObject))->Perform();
+                        }
+                    }
+                }
+            }*/
+
+            // Clear deltas
+            _tDelta = Vector3.Zero;
+            _lastIntersectionPosition = Vector3.Zero;
+        }
+
+        /// <summary>
+        /// Select all objects.
+        /// </summary>
+        public void SelectAll()
+        {
+            
+        }
+
+        /// <summary>
+        /// Selects actor.
+        /// </summary>
+        /// <param name="actor">Actor</param>
+        public void Select(Actor actor)
+        {
+            
+        }
+
+        /// <summary>
+        /// Selects actors.
+        /// </summary>
+        /// <param name="a">Actors</param>
+        public void Select(List<Actor> a)
+        {
+            
+        }
+
+        /// <summary>
+        /// Removes actor from the selection
+        /// </summary>
+        /// <param name="actor">Actor to deselect.</param>
+        public void Deselect(Actor actor)
+        {
+            
+        }
+
+        /// <summary>
+        /// Removes actors from the selection.
+        /// </summary>
+        /// <param name="actors">Actors to deselect.</param>
+        public void Deselect(IEnumerable<Actor> actors)
+        {
+            
+        }
+
+        /// <summary>
+        /// Deselect all actors.
+        /// </summary>
+        public void Deselect()
+        {
+            
+        }
+    }
+}
--- FlaxEditor/Gizmo/TransformGizmo.cs
+++ FlaxEditor/Gizmo/TransformGizmo.cs
@@ -17,9 +17,9 @@ namespace FlaxEditor.Gizmo
         private bool _isCloning;
         private bool _isActive;
         private readonly List<Transform> _startTransforms = new List<Transform>();
-        private readonly List<Actor> _selection = new List<Actor>();
-        private readonly List<Actor> _selectionParents = new List<Actor>();
-        
+        private readonly List<ITransformable> _selection = new List<ITransformable>();
+        private readonly List<ITransformable> _selectionParents = new List<ITransformable>();
+
         private Matrix _screenScaleMatrix;
         private float _screenScale;
 
@@ -27,17 +27,17 @@ namespace FlaxEditor.Gizmo
         private Vector3 _accMoveDelta;
         private Matrix _rotationMatrix;
 
-        private Vector3 _localForward;
-        private Vector3 _localUp;
-        private Vector3 _localRight;
+        private Vector3 _localForward = Vector3.ForwardLH;
+        private Vector3 _localUp = Vector3.Up;
+        private Vector3 _localRight = Vector3.Right;
 
-        private Matrix _objectOrientedWorld;
-        private Matrix _axisAlignedWorld;
+        private Matrix _objectOrientedWorld = Matrix.Identity;
+        private Matrix _axisAlignedWorld = Matrix.Identity;
 
-        private Matrix _gizmoWorld;
+        private Matrix _gizmoWorld = Matrix.Identity;
 
         private Vector3 _translationDelta;
-        private Matrix _rotationDelta;
+        private Matrix _rotationDelta = Matrix.Identity;
         private Vector3 _scaleDelta;
 
         private Vector3 _tDelta;
@@ -54,6 +54,49 @@ namespace FlaxEditor.Gizmo
         public TransformGizmo(IGizmoOwner owner)
             : base(owner)
         {
+        }
+
+        private void StartTransforming()
+        {
+            // Check if can start new action
+            int count = _selectionParents.Count;
+            if (count <= 0 || _isTransforming)
+            {
+                // Back
+                return;
+            }
+
+            // Start
+            _isTransforming = true;
+
+            // Cache 'before' state
+            _startTransforms.Clear();
+            if (_startTransforms.Capacity < count)
+                _startTransforms.Capacity = Mathf.NextPowerOfTwo(count);
+            for (int i = 0; i < count; i++)
+                _startTransforms.Add(_selectionParents[i].Transform);
+        }
+
+        private void EndTransforming()
+        {
+            // Check if wasn't working at all
+            if (!_isTransforming)
+            {
+                // Back
+                return;
+            }
+
+            /*// End action
+            auto ur = _parent->GetUndoRedo();
+            if (!ur->IsDisabled())
+            {
+                ur->AddAction(new TransformActors(this, _startTransforms));
+            }
+            _startTransforms.Clear();
+            _isTransforming = false;
+            _isCloning = false;
+            _parent->OnTransfomingEnd();
+            _parent->OnTransformObject();*/
         }
     }
 }
