2986c2b0a13fb44fe2fbe154e657d5199cdc057e
mafiesto4 <wojtek@figat.pl>
2017-07-08 20:37:07 +0900

    [Editor] Finished helper ModelPreview viewport

--- FlaxEditor/Viewport/Previews/ModelPreview.cs
+++ FlaxEditor/Viewport/Previews/ModelPreview.cs
@@ -5,6 +5,7 @@
 using FlaxEngine;
 using FlaxEngine.GUI;
 using FlaxEngine.Rendering;
+using Object = FlaxEngine.Object;
 
 namespace FlaxEditor.Viewport.Previews
 {
@@ -27,10 +28,8 @@ namespace FlaxEditor.Viewport.Previews
         /// </value>
         public Model Model
         {
-            set
-            {
-                // TODO: ....
-            }
+            get => _previewModel.Model;
+            set => _previewModel.Model = value;
         }
 
         /// <summary>
@@ -44,12 +43,44 @@ namespace FlaxEditor.Viewport.Previews
 
             Task.Enabled = false;
             Task.Flags = ViewFlags.DefaultModelPreview;
+            Task.OnBegin += TaskOnBegin;
 
             SetView(new Quaternion(0.424461186f, -0.0940724313f, 0.0443938486f, 0.899451137f));
 
-            //_previewModel = new ModelActor();
+            // Setup preview scene
+            _previewLight = DirectionalLight.New();
+            _previewLight.ShadowsMode = ShadowsCastingMode.None;
+            _previewLight.Orientation = Quaternion.Euler(new Vector3(52.1477f, -109.109f, -111.739f));
+            //
+            _envProbe = EnvironmentProbe.New();
+            _envProbe.AutoUpdate = false;
+            _envProbe.SetCustomProbe(FlaxEngine.Content.LoadAsyncInternal<CubeTexture>("Editor/SimplySky"));
+            //
+            _sky.SunLight = _previewLight;
+
+            // Link actors for rendering
+            Task.CustomActors.Add(_previewModel);
+            Task.CustomActors.Add(_previewLight);
+            Task.CustomActors.Add(_envProbe);
+            Task.CustomActors.Add(_sky);
+
+            // Update actors
+            for (int i = 0; i < Task.CustomActors.Count; i++)
+                Task.CustomActors[i].UpdateCache();
         }
 
+        private void TaskOnBegin(SceneRenderTask task)
+        {
+            // Update preview model scale to fit the preview
+            var model = Model;
+            if (model && model.IsLoaded)
+            {
+                float targetSize = 30.0f;
+                float maxSize = Mathf.Max(0.001f, model.Box.Size.MaxValue);
+                _previewModel.Scale = new Vector3(targetSize / maxSize);
+            }
+        }
+        
         /// <summary>
         /// Enables this preview rendering.
         /// </summary>
@@ -66,6 +97,16 @@ namespace FlaxEditor.Viewport.Previews
             Task.Enabled = false;
         }
 
+        /// <inheritdoc />
+        public override void OnDestroy()
+        {
+            // Ensure to cleanup created actor objects
+            Object.Destroy(ref _previewModel);
+            Object.Destroy(ref _previewLight);
+            Object.Destroy(ref _envProbe);
+            Object.Destroy(ref _sky);
 
+            base.OnDestroy();
+        }
     }
 }
