38eaa19fc453f925f9f81d6b799011a4d1343cf0
mafiesto4 <wojtek@figat.pl>
2017-07-12 19:23:04 +0900

    [Editor] Modify PreviewsCache constants

--- FlaxEditor/API/BinaryAssets/PreviewsCache.cs
+++ FlaxEditor/API/BinaryAssets/PreviewsCache.cs
@@ -7,36 +7,40 @@ using FlaxEngine.Rendering;
 
 namespace FlaxEditor
 {
-	public sealed partial class PreviewsCache
-	{
-	    /// <summary>
-	    /// The default asset previews icon size (both width and height since it's a square).
-	    /// </summary>
-	    public const int AssetIconSize = 64;
+    public sealed partial class PreviewsCache
+    {
+        /// <summary>
+        /// The default asset previews icon size (both width and height since it's a square).
+        /// </summary>
+        public const int AssetIconSize = 64;
 
-	    /// <summary>
-	    /// The default assets previews atlas size
-	    /// </summary>
-	    public const int AssetIconsAtlasSize = 1024;
+        /// <summary>
+        /// The default assets previews atlas size
+        /// </summary>
+        public const int AssetIconsAtlasSize = 1024;
 
-	    /// <summary>
-	    /// The default assets previews atlas margin (between icons)
-	    /// </summary>
-	    public const int AssetIconsAtlasMargin = 4;
+        /// <summary>
+        /// The default assets previews atlas margin (between icons)
+        /// </summary>
+        public const int AssetIconsAtlasMargin = 4;
 
-	    /// <summary>
-	    /// The default format of previews atlas.
-	    /// </summary>
-	    public const PixelFormat AssetIconsAtlasFormat = PixelFormat.R8G8B8A8_UNorm;
+        // Util constants
+        public const int AssetIconsPerRow = (int)((float)AssetIconsAtlasSize / (AssetIconSize + AssetIconsAtlasMargin));
+        public const int AssetIconsPerAtlas = AssetIconsPerRow * AssetIconsPerRow;
 
-	    /// <summary>
-	    /// The previews cache asset type unique ID.
-	    /// </summary>
-	    public const int TypeID = 11;
+        /// <summary>
+        /// The default format of previews atlas.
+        /// </summary>
+        public const PixelFormat AssetIconsAtlasFormat = PixelFormat.R8G8B8A8_UNorm;
 
-	    /// <summary>
-	    /// The asset type content domain.
-	    /// </summary>
-	    public const ContentDomain Domain = ContentDomain.Texture;
+        /// <summary>
+        /// The previews cache asset type unique ID.
+        /// </summary>
+        public const int TypeID = 11;
+
+        /// <summary>
+        /// The asset type content domain.
+        /// </summary>
+        public const ContentDomain Domain = ContentDomain.Texture;
     }
 }
