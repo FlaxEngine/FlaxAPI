6ea712ced7d82888f7d17825bcecd0c2ac3232ae
mafiesto4 <wojtek@figat.pl>
2017-07-13 18:07:38 +0900

    Fix onBuild script

--- CopyDotNetApi.bat
+++ CopyDotNetApi.bat
@@ -1,11 +1,14 @@
 @echo off
 
-set outputDir="..\Source\Bin\Editor\Assemblies"
+set outputDir=..\Source\Bin\Editor
+set outputAssembliesDir=%outputDir%\Assemblies
 
 if exist %outputDir% (
 	echo Copy Flax assemblies
-	start /MIN xcopy /s /y "FlaxEditor\bin" %outputDir%
-	start /MIN xcopy /s /y "FlaxEngine\bin" %outputDir%
+	if not exist "%outputAssembliesDir%" (
+		mkdir "%outputAssembliesDir%"
+	)
+	start /MIN xcopy /i /s /y "FlaxEngine\bin" "%outputAssembliesDir%" /exclude:excludedFileList.txt
+	start /MIN xcopy /i /s /y "FlaxEditor\bin" "%outputAssembliesDir%" /exclude:excludedFileList.txt
 	echo Done!
 )
-
