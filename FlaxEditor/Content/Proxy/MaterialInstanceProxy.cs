616ffb5ea7d4f42acb6ff3a2bcbead5649275a3c
mafiesto4 <wojtek@figat.pl>
2017-07-15 14:50:02 +0900

    [Thumbnails] Working thumbnails rendering for material and material instance

--- FlaxEditor/Content/Proxy/MaterialInstanceProxy.cs
+++ FlaxEditor/Content/Proxy/MaterialInstanceProxy.cs
@@ -3,8 +3,12 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 using System;
+using FlaxEditor.Content.Thumbnails;
+using FlaxEditor.Viewport.Previews;
 using FlaxEditor.Windows;
 using FlaxEngine;
+using FlaxEngine.GUI;
+using FlaxEngine.Rendering;
 
 namespace FlaxEditor.Content
 {
@@ -14,6 +18,8 @@ namespace FlaxEditor.Content
     /// <seealso cref="FlaxEditor.Content.BinaryAssetProxy" />
     public class MaterialInstanceProxy : BinaryAssetProxy
     {
+        private MaterialPreview _preview;
+
         /// <inheritdoc />
         public override string Name => "Material Instance";
 
@@ -34,5 +40,54 @@ namespace FlaxEditor.Content
 
         /// <inheritdoc />
         public override ContentDomain Domain => MaterialInstance.Domain;
+
+        /// <inheritdoc />
+        public override void OnThumbnailDrawPrepare(ThumbnailRequest request)
+        {
+            if (_preview == null)
+            {
+                _preview = new MaterialPreview(false);
+                _preview.RenderOnlyWithWindow = false;
+                _preview.Task.Enabled = false;
+                _preview.Size = new Vector2(PreviewsCache.AssetIconSize, PreviewsCache.AssetIconSize);
+                _preview.Resize();
+            }
+
+            // TODO: disable streaming for dependant assets during thumbnail rendering (and restore it after)
+        }
+
+        /// <inheritdoc />
+        public override bool CanDrawThumbnail(ThumbnailRequest request)
+        {
+            return _preview.HasLoadedAssets;
+        }
+
+        /// <inheritdoc />
+        public override void OnThumbnailDrawBegin(ThumbnailRequest request, ContainerControl guiRoot, GPUContext context)
+        {
+            _preview.Material = (MaterialInstance)request.Asset;
+            _preview.Parent = guiRoot;
+
+            _preview.Task.Internal_Render(context);
+        }
+
+        /// <inheritdoc />
+        public override void OnThumbnailDrawEnd(ThumbnailRequest request, ContainerControl guiRoot)
+        {
+            _preview.Material = null;
+            _preview.Parent = null;
+        }
+
+        /// <inheritdoc />
+        public override void Dispose()
+        {
+            if (_preview != null)
+            {
+                _preview.Dispose();
+                _preview = null;
+            }
+
+            base.Dispose();
+        }
     }
 }
