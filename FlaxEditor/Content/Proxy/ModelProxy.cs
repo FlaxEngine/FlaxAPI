b8be14e6aac259215f3e3e9a54a371e1523c7b7b
mafiesto4 <wojtek@figat.pl>
2017-07-15 15:01:25 +0900

    [Thumbnails] Generating model assets thumbnails works great

--- FlaxEditor/Content/Proxy/ModelProxy.cs
+++ FlaxEditor/Content/Proxy/ModelProxy.cs
@@ -2,9 +2,13 @@
 // Copyright (c) 2012-2017 Flax Engine. All rights reserved.
 ////////////////////////////////////////////////////////////////////////////////////
 
+using FlaxEditor.Content.Thumbnails;
+using FlaxEditor.Viewport.Previews;
 using FlaxEditor.Windows;
 using FlaxEditor.Windows.Assets;
 using FlaxEngine;
+using FlaxEngine.GUI;
+using FlaxEngine.Rendering;
 
 namespace FlaxEditor.Content
 {
@@ -14,6 +18,8 @@ namespace FlaxEditor.Content
     /// <seealso cref="FlaxEditor.Content.BinaryAssetProxy" />
     public class ModelProxy : BinaryAssetProxy
     {
+        private ModelPreview _preview;
+
         /// <inheritdoc />
         public override string Name => "Model";
 
@@ -34,5 +40,59 @@ namespace FlaxEditor.Content
 
         /// <inheritdoc />
         public override ContentDomain Domain => Model.Domain;
+
+        /// <inheritdoc />
+        public override void OnThumbnailDrawPrepare(ThumbnailRequest request)
+        {
+            if (_preview == null)
+            {
+                _preview = new ModelPreview(false);
+                _preview.RenderOnlyWithWindow = false;
+                _preview.Task.Enabled = false;
+                _preview.Size = new Vector2(PreviewsCache.AssetIconSize, PreviewsCache.AssetIconSize);
+                _preview.Resize();
+            }
+
+            // TODO: disable streaming for asset during thumbnail rendering (and restore it after)
+        }
+
+        /// <inheritdoc />
+        public override bool CanDrawThumbnail(ThumbnailRequest request)
+        {
+            if (!_preview.HasLoadedAssets)
+                return false;
+
+            // Check if asset is streamed enough
+            var asset = (Model)request.Asset;
+            return asset.LoadedLODs >= (int)(asset.LODs.Length * ThumbnailsModule.MinimumRequriedResourcesQuality);
+        }
+
+        /// <inheritdoc />
+        public override void OnThumbnailDrawBegin(ThumbnailRequest request, ContainerControl guiRoot, GPUContext context)
+        {
+            _preview.Model = (Model)request.Asset;
+            _preview.Parent = guiRoot;
+
+            _preview.Task.Internal_Render(context);
+        }
+
+        /// <inheritdoc />
+        public override void OnThumbnailDrawEnd(ThumbnailRequest request, ContainerControl guiRoot)
+        {
+            _preview.Model = null;
+            _preview.Parent = null;
+        }
+
+        /// <inheritdoc />
+        public override void Dispose()
+        {
+            if (_preview != null)
+            {
+                _preview.Dispose();
+                _preview = null;
+            }
+
+            base.Dispose();
+        }
     }
 }
