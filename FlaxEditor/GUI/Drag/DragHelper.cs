068d9eee2c7f1ebc92d0bd37ab875bc163f3b6da
mafiesto4 <wojtek@figat.pl>
2017-07-19 00:59:52 +0900

    [Editor] Added helper DragItems and DragHelper for drag and drops

---
+++ FlaxEditor/GUI/Drag/DragHelper.cs
@@ -0,0 +1,93 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.GUI.Drag
+{
+    /// <summary>
+    /// Base class for drag and drop operation helpers.
+    /// </summary>
+    /// <typeparam name="T">Type of the objects to collect from drag data.</typeparam>
+    public abstract class DragHelper<T>
+    {
+        /// <summary>
+        /// The objects gathered.
+        /// </summary>
+        public readonly List<T> Objects = new List<T>();
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has valid drag data.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has valid drag data; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasValidDrag => Objects.Count > 0;
+
+        /// <summary>
+        /// Invalids the drag data.
+        /// </summary>
+        public void InvalidDrag()
+        {
+            Objects.Clear();
+        }
+
+        /// <summary>
+        /// Called when drag enters.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="validateFunc">The validate function. Check if gathered object is valid to drop it.</param>
+        /// <returns>True if drag event is valid and can be performed, otherwise false.</returns>
+        public bool OnDragEnter(DragData data, Func<T, bool> validateFunc)
+        {
+            if(data == null || validateFunc == null)
+                throw new ArgumentNullException();
+
+            Objects.Clear();
+
+            if(data is DragDataText text)
+                GetherObjects(text, validateFunc);
+            else if(data is DragDataFiles files)
+                GetherObjects(files, validateFunc);
+
+            return HasValidDrag;
+        }
+
+        /// <summary>
+        /// Called when drag leaves.
+        /// </summary>
+        public void OnDragLeave()
+        {
+            Objects.Clear();
+        }
+
+        /// <summary>
+        /// Called when drag drops.
+        /// </summary>
+        public void OnDragDrop()
+        {
+            Objects.Clear();
+        }
+
+        /// <summary>
+        /// Gethers the objects from the drag data (text).
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="validateFunc">The validate function.</param>
+        protected virtual void GetherObjects(DragDataText data, Func<T, bool> validateFunc)
+        {
+        }
+
+        /// <summary>
+        /// Gethers the objects from the drag data (files).
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="validateFunc">The validate function.</param>
+        protected virtual void GetherObjects(DragDataFiles data, Func<T, bool> validateFunc)
+        {
+        }
+    }
+}
---
+++ FlaxEditor/GUI/Drag/DragItems.cs
@@ -0,0 +1,91 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using FlaxEditor.Content;
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.GUI.Drag
+{
+    /// <summary>
+    /// Helper class for handling <see cref="ContentItem"/> drag and drop.
+    /// </summary>
+    /// <seealso cref="ContentItem" />
+    public sealed class DragItems : DragHelper<ContentItem>
+    {
+        /// <summary>
+        /// The default prefix for drag data used for <see cref="ContentItem"/>.
+        /// </summary>
+        public const string DragPrefix = "ASSET!?";
+
+        /// <inheritdoc />
+        protected override void GetherObjects(DragDataText data, Func<ContentItem, bool> validateFunc)
+        {
+            var items = ParseData(data);
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (validateFunc(items[i]))
+                    Objects.Add(items[i]);
+            }
+        }
+
+        /// <summary>
+        /// Tries to parse the drag data to extract <see cref="ContentItem"/> collection.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns>Gathered objects or empty array if cannot get any valid.</returns>
+        public static ContentItem[] ParseData(DragDataText data)
+        {
+            if (data.Text.StartsWith(DragPrefix))
+            {
+                // Remove prefix and parse splited names
+                var paths = data.Text.Remove(0, DragPrefix.Length).Split('\n');
+                var results = new List<ContentItem>(paths.Length);
+                for (int i = 0; i < paths.Length; i++)
+                {
+                    // Find element
+                    var obj = Editor.Instance.ContentDatabase.Find(paths[i]);
+
+                    // Check it
+                    if (obj != null)
+                        results.Add(obj);
+                }
+
+                return results.ToArray();
+            }
+
+            return new ContentItem[0];
+        }
+
+        /// <summary>
+        /// Gets the drag data.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns>The data.</returns>
+        public static DragDataText GetDragData(ContentItem item)
+        {
+            if (item == null)
+                throw new ArgumentNullException();
+
+            return new DragDataText(DragPrefix + item.Path);
+        }
+
+        /// <summary>
+        /// Gets the drag data.
+        /// </summary>
+        /// <param name="items">The items.</param>
+        /// <returns>The data.</returns>
+        public static DragDataText GetDragData(IEnumerable<ContentItem> items)
+        {
+            if (items == null)
+                throw new ArgumentNullException();
+
+            string text = DragPrefix;
+            foreach (var item in items)
+                text += item.Path + '\n';
+            return new DragDataText(text);
+        }
+    }
+}
