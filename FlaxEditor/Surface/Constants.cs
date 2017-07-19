14c8900d654bf8fec19c9341240945291158234a
mafiesto4 <wojtek@figat.pl>
2017-07-19 21:21:04 +0900

    [Surface] Added Constants as surface settings

---
+++ FlaxEditor/Surface/Constants.cs
@@ -0,0 +1,22 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+namespace FlaxEditor.Surface
+{
+    /// <summary>
+    /// Set of static properties for the surface.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The node close button size.
+        /// </summary>
+        public const float NodeCloseButtonSize = 12.0f;
+
+        /// <summary>
+        /// The node close button margin from the edges.
+        /// </summary>
+        public const float NodeCloseButtonMargin = 2.0f;
+    }
+}
