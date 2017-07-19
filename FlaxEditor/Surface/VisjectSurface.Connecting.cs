69f52fe61974649baab1c5d0d288b1c4452ef1a8
mafiesto4 <wojtek@figat.pl>
2017-07-19 21:22:12 +0900

    [Surface] Added CanUseDirectCast check and some fields

--- FlaxEditor/Surface/VisjectSurface.Connecting.cs
+++ FlaxEditor/Surface/VisjectSurface.Connecting.cs
@@ -10,6 +10,49 @@ namespace FlaxEditor.Surface
     public partial class VisjectSurface
     {
         /// <summary>
+        /// Checks if can use direct conversion from one type to another.
+        /// </summary>
+        /// <param name="from">Source type.</param>
+        /// <param name="to">Target type.</param>
+        /// <returns>True if can use direct conversion, otherwise false.</returns>
+        public bool CanUseDirectCast(ConnectionType from, ConnectionType to)
+        {
+            bool result = (from & to) != 0;
+            if (!result)
+            {
+                if (Type == SurfaceType.Material)
+                {
+                    switch (from)
+                    {
+                        case ConnectionType.Bool:
+                        case ConnectionType.Integer:
+                        case ConnectionType.Float:
+                        case ConnectionType.Vector2:
+                        case ConnectionType.Vector3:
+                        case ConnectionType.Vector4:
+                            switch (to)
+                            {
+                                case ConnectionType.Bool:
+                                case ConnectionType.Integer:
+                                case ConnectionType.Float:
+                                case ConnectionType.Vector2:
+                                case ConnectionType.Vector3:
+                                case ConnectionType.Vector4:
+                                    result = true;
+                                    break;
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
         /// Begins connecting boxes action.
         /// </summary>
         /// <param name="box">The start box.</param>
--- FlaxEditor/Surface/VisjectSurface.cs
+++ FlaxEditor/Surface/VisjectSurface.cs
@@ -2,6 +2,11 @@
 // Copyright (c) 2012-2017 Flax Engine. All rights reserved.
 ////////////////////////////////////////////////////////////////////////////////////
 
+using System;
+using System.Collections.Generic;
+using FlaxEditor.GUI.Drag;
+using FlaxEditor.Surface.Elements;
+using FlaxEngine;
 using FlaxEngine.GUI;
 
 namespace FlaxEditor.Surface
@@ -12,8 +17,41 @@ namespace FlaxEditor.Surface
     /// <seealso cref="FlaxEngine.GUI.ScrollableControl" />
     public partial class VisjectSurface : ScrollableControl
     {
-        private IVisjectSurfaceOwner _owner;
-        private SurfaceType _type;
+        private bool _edited;
+        private float _targeScale = 1.0f;
+        private readonly List<SurfaceNode> _nodes = new List<SurfaceNode>(64);
+
+        private bool _leftMouseDown;
+        private bool _rightMouseDown;
+        private Vector2 _leftMouseDownPos;
+        private Vector2 _rightMouseDownPos;
+        private Vector2 _mousePos;
+        private float _mouseMoveAmount;
+
+        private bool _isMovingSelection;
+        private Box _startBox;
+        private Box _lastBoxUnderMouse;
+
+        //private VisjectCM _cmPrimaryMenu;
+        private ContextMenu _cmSecondaryMenu;
+        private Vector2 _cmStartPos;
+
+        private DragItems _dragOverItems = new DragItems();
+
+        /// <summary>
+        /// The owner.
+        /// </summary>
+        public readonly IVisjectSurfaceOwner Owner;
+
+        /// <summary>
+        /// The surface type.
+        /// </summary>
+        public readonly SurfaceType Type;
+
+        /// <summary>
+        /// The style used by the surface.
+        /// </summary>
+        public readonly SurfaceStyle Style;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="VisjectSurface"/> class.
@@ -23,8 +61,11 @@ namespace FlaxEditor.Surface
         public VisjectSurface(IVisjectSurfaceOwner owner, SurfaceType type)
             : base(true, 0, 0, 100, 100)
         {
-            _owner = owner;
-            _type = type;
+            Owner = owner;
+            Type = type;
+            Style = SurfaceStyle.CreateStyleHandler(Editor.Instance, Type);
+            if (Style == null)
+                throw new InvalidOperationException("Missing visject surface style.");
         }
     }
 }
