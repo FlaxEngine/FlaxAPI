d4d0d8a15d29a3a36e18461599d561b3976024a3
mafiesto4 <wojtek@figat.pl>
2017-07-20 23:57:08 +0900

    [Surface] Finish visject context menu

--- FlaxEditor/Surface/ContextMenu/VisjectCM.cs
+++ FlaxEditor/Surface/ContextMenu/VisjectCM.cs
@@ -2,6 +2,9 @@
 // Copyright (c) 2012-2017 Flax Engine. All rights reserved.
 ////////////////////////////////////////////////////////////////////////////////////
 
+using System;
+using System.Collections.Generic;
+using FlaxEngine;
 using FlaxEngine.GUI;
 
 namespace FlaxEditor.Surface.ContextMenu
@@ -12,6 +15,93 @@ namespace FlaxEditor.Surface.ContextMenu
     /// <seealso cref="FlaxEngine.GUI.ContextMenuBase" />
     public sealed class VisjectCM : ContextMenuBase
     {
+        private readonly List<VisjectCMGroup> _groups = new List<VisjectCMGroup>(16);
+        private TextBox _searchBox;
 
+        /// <summary>
+        /// The type of the surface.
+        /// </summary>
+        public readonly SurfaceType Type;
+
+        /// <summary>
+        /// Event fired when any item in this popup menu gets clicked.
+        /// </summary>
+        public event Action<VisjectCMItem> OnItemClicked;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VisjectCM"/> class.
+        /// </summary>
+        /// <param name="type">The surface type.</param>
+        public VisjectCM(SurfaceType type)
+        {
+            Type = type;
+
+            // Context menu dimensions
+            Size = new Vector2(320, 220);
+
+            // Search box
+            _searchBox = new TextBox(false, 0, 0);
+            _searchBox.DockStyle = DockStyle.Top;
+            _searchBox.TextChanged += OnSearchFilterChanged;
+            _searchBox.Parent = this;
+
+            // Create first panel (for scrollbar)
+            var panel1 = new Panel(ScrollBars.Vertical);
+            panel1.DockStyle = DockStyle.Fill;
+            panel1.Parent = this;
+
+            // Create second panel (for groups arrangement)
+            var panel2 = new VerticalPanel();
+            panel2.Width = panel1.Width;
+            panel2.Parent = panel1;
+        }
+
+        private void OnSearchFilterChanged()
+        {
+            // Skip events during setup or init stuff
+            if (IsLayoutLocked)
+                return;
+
+            // Update groups
+            for (int i = 0; i < _groups.Count; i++)
+                _groups[i].UpdateFilter(_searchBox.Text);
+            PerformLayout();
+        }
+
+        /// <summary>
+        /// Called when user clicks on an item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        public void OnClickItem(VisjectCMItem item)
+        {
+            Hide();
+            OnItemClicked?.Invoke(item);
+        }
+
+        /// <summary>
+        /// Resets the view.
+        /// </summary>
+        public void ResetView()
+        {
+            bool wasLayoutLocked = IsLayoutLocked;
+            IsLayoutLocked = true;
+
+            for (int i = 0; i < _groups.Count; i++)
+                _groups[i].ResetView();
+
+            _searchBox.Clear();
+
+            IsLayoutLocked = wasLayoutLocked;
+            PerformLayout();
+        }
+
+        /// <inheritdoc />
+        protected override void OnShow()
+        {
+            // Prepare
+            ResetView();
+
+            base.OnShow();
+        }
     }
 }
--- FlaxEditor/Surface/ContextMenu/VisjectCMGroup.cs
+++ FlaxEditor/Surface/ContextMenu/VisjectCMGroup.cs
@@ -13,18 +13,71 @@ namespace FlaxEditor.Surface.ContextMenu
     public sealed class VisjectCMGroup : DropPanel
     {
         /// <summary>
-        /// The archetype
+        /// The context menu.
+        /// </summary>
+        public readonly VisjectCM ContextMenu;
+
+        /// <summary>
+        /// The archetype.
         /// </summary>
         public readonly GroupArchetype Archetype;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="VisjectCMGroup"/> class.
         /// </summary>
+        /// <param name="cm">The context menu.</param>
         /// <param name="archetype">The group archetype.</param>
-        public VisjectCMGroup(GroupArchetype archetype) 
+        public VisjectCMGroup(VisjectCM cm, GroupArchetype archetype)
             : base(archetype.Name)
         {
+            ContextMenu = cm;
             Archetype = archetype;
+        }
+
+        /// <summary>
+        /// Resets the view.
+        /// </summary>
+        public void ResetView()
+        {
+            // Remove filter
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (_children[i] is VisjectCMItem item)
+                    item.UpdateFilter(null);
+            }
+
+            Close();
+            Visible = true;
+        }
+
+        /// <summary>
+        /// Updates the filter.
+        /// </summary>
+        /// <param name="filterText">The filter text.</param>
+        public void UpdateFilter(string filterText)
+        {
+            // Update items
+            bool isAnyVisible = false;
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (_children[i] is VisjectCMItem item)
+                {
+                    item.UpdateFilter(filterText);
+                    isAnyVisible |= item.Visible;
+                }
+            }
+
+            // Update itself
+            if (isAnyVisible)
+            {
+                Open();
+                Visible = true;
+            }
+            else
+            {
+                // Hide group if none of the items matched the filter
+                Visible = false;
+            }
         }
     }
 }
