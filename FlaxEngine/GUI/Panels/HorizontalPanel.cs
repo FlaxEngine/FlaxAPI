7ecd23b14d92a6dbf8ed4f9024a023e4d07ef020
mafiesto4 <wojtek@figat.pl>
2017-07-20 22:45:57 +0900

    [GUI] Added VerticalPanel and HorizontalPanel for easy controls arrange

---
+++ FlaxEngine/GUI/Panels/HorizontalPanel.cs
@@ -0,0 +1,39 @@
+﻿////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+namespace FlaxEngine.GUI
+{
+    /// <summary>
+    /// This panel arranges child controls horizontally.
+    /// </summary>
+    /// <seealso cref="FlaxEngine.GUI.PanelWithMargins" />
+    public class HorizontalPanel : PanelWithMargins
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HorizontalPanel"/> class.
+        /// </summary>
+        public HorizontalPanel()
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void PerformLayoutSelf()
+        {
+            // Sort controls from left to right
+            float x = _leftMargin;
+            float h = Height - _topMargin - _bottomMargin;
+            for (int i = 0; i < _children.Count; i++)
+            {
+                Control c = _children[i];
+                var w = c.Width;
+                c.Bounds = new Rectangle(x + _spacing, _topMargin, h, w);
+                x = c.Bottom;
+            }
+            x += _rightMargin;
+
+            // Update size
+            Width = x;
+        }
+    }
+}
