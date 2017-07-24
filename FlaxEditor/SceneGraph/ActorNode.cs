3c51733bfcc5e5de81e5454a38c15b3acbf97455
mafiesto4 <wojtek@figat.pl>
2017-07-25 00:24:46 +0900

    [Editor] Added code to sync actor tree nodes but commented for now

--- FlaxEditor/SceneGraph/ActorNode.cs
+++ FlaxEditor/SceneGraph/ActorNode.cs
@@ -3,9 +3,12 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using FlaxEditor.SceneGraph.GUI;
 using FlaxEditor.Windows;
 using FlaxEngine;
+using FlaxEngine.GUI;
 
 namespace FlaxEditor.SceneGraph
 {
@@ -150,7 +153,30 @@ namespace FlaxEditor.SceneGraph
         {
             return _actor.IntersectsItself(ref ray, ref distance);
         }
+        /*
+        /// <summary>
+        /// Synchronizes the children order (child actors) with teh actual state.
+        /// </summary>
+        public void SyncChildrenOrder()
+        {
+            var children = Actor.GetChildren();
 
+            // Validate data
+            if (children.Length != _treeNode.ChildrenCount)
+                throw new InvalidOperationException("Amount of child nodes is not matching actual state.");
+
+            // Sort child tree nodes (UI only)
+            // Note: ChildNodes may have diffrent order and have diffrent size
+            // because they may contain custom subnodes per actor type
+            Dictionary<Control, int> order = new Dictionary<Control, int>(children.Length * 2);
+            for (int i = 0; i < children.Length; i++)
+            {
+                var node = FindChild(children[i]);
+                order.Add(node.TreeNode, i);
+            }
+            _treeNode.Children.Sort((a, b) => order[a] - order[b]);
+        }
+        */
         /// <inheritdoc />
         protected override void OnParentChanged()
         {
@@ -161,7 +187,10 @@ namespace FlaxEditor.SceneGraph
             // (eg. we builded new node for spawned actor and link it to the game)
             if (_treeNode.Parent != null && !_treeNode.Parent.IsLayoutLocked)
             {
-                _treeNode.SortChildren();
+                // Sync order in parent to be the same as in c++
+                //((ActorNode)ParentNode).SyncChildrenOrder();
+
+                // Update UI
                 _treeNode.IsLayoutLocked = false;
                 _treeNode.PerformLayout();
             }
