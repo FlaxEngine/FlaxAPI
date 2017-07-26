45bfe704cc9e54ce88c7de6983ef8c8ccc6796f1
mafiesto4 <wojtek@figat.pl>
2017-07-26 17:12:00 +0900

    [Editor] Added VerticalPanelElement and ValueContainer for custom editors

---
+++ FlaxEditor/CustomEditors/Elements/VerticalPanelElement.cs
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using FlaxEngine.GUI;
+
+namespace FlaxEditor.CustomEditors.Elements
+{
+    /// <summary>
+    /// The vertical panel element.
+    /// </summary>
+    /// <seealso cref="FlaxEditor.CustomEditors.LayoutElement" />
+    public class VerticalPanelElement : LayoutElementsContainer
+    {
+        /// <summary>
+        /// The panel.
+        /// </summary>
+        public readonly VerticalPanel Panel = new VerticalPanel();
+
+        /// <summary>
+        /// Initializes the element.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        public void Init(string text)
+        {
+            Panel.Name = text;
+        }
+
+        /// <inheritdoc />
+        public override ContainerControl ContainerControl => Panel;
+    }
+}
