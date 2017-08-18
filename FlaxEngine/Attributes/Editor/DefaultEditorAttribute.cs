9e339de7d3b41b589fb07971c31e69fa95996c0f
mafiesto4 <wojtek@figat.pl>
2017-08-18 13:42:19 +0200

    [Editor] Added DefaultEditorAttribute

---
+++ FlaxEngine/Attributes/Editor/DefaultEditorAttribute.cs
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace FlaxEngine
+{
+    /// <summary>
+    /// Specifies default editor provided for the target object/class/field/property. Should be used along with <see cref="FlaxEngine.CustomEditorAttribute"/>.
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Delegate | AttributeTargets.Event | AttributeTargets.Method)]
+    public sealed class DefaultEditorAttribute : Attribute
+    {
+    }
+}
