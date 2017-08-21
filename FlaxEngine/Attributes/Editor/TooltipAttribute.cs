3af8735797092ec338c3d06920144697f07bfbce
mafiesto4 <wojtek@figat.pl>
2017-08-21 23:06:05 +0200

    [Engine] Added TooltipAttribute

---
+++ FlaxEngine/Attributes/Editor/TooltipAttribute.cs
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace FlaxEngine
+{
+    /// <summary>
+    /// Specifies a tooltip for a property/field in the editor.
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public sealed class TooltipAttribute : Attribute
+    {
+        /// <summary>
+        /// The tooltip text.
+        /// </summary>
+        public string Text;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TooltipAttribute"/> class.
+        /// </summary>
+        /// <param name="text">The tooltip text.</param>
+        public TooltipAttribute(string text)
+        {
+            Text = text;
+        }
+    }
+}
