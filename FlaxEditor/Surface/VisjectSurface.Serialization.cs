1a3ef2802c59e3030281e25111b570fa70c53ad1
mafiesto4 <wojtek@figat.pl>
2017-08-22 17:05:38 +0200

    [Editor] Visject surface saving  to bytes

--- FlaxEditor/Surface/VisjectSurface.Serialization.cs
+++ FlaxEditor/Surface/VisjectSurface.Serialization.cs
@@ -22,7 +22,7 @@ namespace FlaxEditor.Surface
             public SurfaceNode NodeB;
             public byte BoxB;
         }
-        
+
         [StructLayout(LayoutKind.Sequential)]
         struct VisjectSurfaceMeta10// TypeID: 10, for surface
         {
@@ -44,6 +44,52 @@ namespace FlaxEditor.Surface
             T stuff = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
             handle.Free();
             return stuff;
+        }
+
+        private static byte[] StructureToByteArray<T>(ref T stuff) where T : struct
+        {
+            // #stuid c#
+            int size = Marshal.SizeOf(typeof(T));
+            byte[] arr = new byte[size];
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(stuff, ptr, true);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+
+        private static unsafe string ReadStr(BinaryReader stream, int check)
+        {
+            int length = stream.ReadInt32();
+            if (length > 0 && length < 2000)
+            {
+                var str = stream.ReadBytes(length * 2);
+                fixed (byte* strPtr = str)
+                {
+                    var ptr = (char*)strPtr;
+                    for (int j = 0; j < length; j++)
+                        ptr[j] = (char)(ptr[j] ^ check);
+                }
+                return System.Text.Encoding.Unicode.GetString(str);
+            }
+
+            return string.Empty;
+        }
+
+        private static unsafe void WriteStr(BinaryWriter stream, string str, int check)
+        {
+            int length = str.Length;
+            stream.Write(length);
+            var bytes = System.Text.Encoding.Unicode.GetBytes(str);
+            if (bytes == null || bytes.Length * 2 != length)
+                throw new ArgumentException();
+            fixed (byte* bytesPtr = bytes)
+            {
+                var ptr = (char*)bytesPtr;
+                for (int j = 0; j < length; j++)
+                    ptr[j] = (char)(ptr[j] ^ check);
+            }
+            stream.Write(bytes);
         }
 
         private static void ReadCommonValue(BinaryReader stream, ref object value)
@@ -94,45 +140,102 @@ namespace FlaxEditor.Surface
                 {
                     String v;
                     ReadString(&v, 953);
-                    data->Set(v);
+                    data.Set(v);
                 }
                     break;
                 case 9:// CommonType::Box:
                 {
                     BoundingBox v;
                     ReadBox(&v);
-                    data->Set(v);
+                    data.Set(v);
                 }
                     break;
                 case 10:// CommonType::Rotation:
                 {
                     Quaternion v;
                     ReadQuaternion(&v);
-                    data->Set(v);
+                    data.Set(v);
                 }
                     break;
                 case 11:// CommonType::Transform:
                 {
                     Transform v;
                     ReadTransform(&v);
-                    data->Set(v);
+                    data.Set(v);
                 }
                     break;
                 case 12:// CommonType::Sphere:
                 {
                     BoundingSphere v;
                     ReadSphere(&v);
-                    data->Set(v);
+                    data.Set(v);
                 }
                     break;
                 case 13:// CommonType::Rect:
                 {
                     Rect v;
                     ReadRect(&v);
-                    data->Set(v);
+                    data.Set(v);
                 }
                     break;*/
                 default: throw new SystemException();
+            }
+        }
+
+        private static void WriteCommonValue(BinaryWriter stream, object value)
+        {
+            if (value is bool asBool)
+            {
+                stream.Write((byte)0);
+                stream.Write((byte)(asBool ? 1 : 0));
+            }
+            else if (value is int asInt)
+            {
+                stream.Write((byte)1);
+                stream.Write(asInt);
+            }
+            else if (value is float asFloat)
+            {
+                stream.Write((byte)2);
+                stream.Write(asFloat);
+            }
+            else if (value is Vector2 asVector2)
+            {
+                stream.Write((byte)3);
+                stream.Write(asVector2.X);
+                stream.Write(asVector2.Y);
+            }
+            else if (value is Vector3 asVector3)
+            {
+                stream.Write((byte)4);
+                stream.Write(asVector3.X);
+                stream.Write(asVector3.Y);
+                stream.Write(asVector3.Z);
+            }
+            else if (value is Vector4 asVector4)
+            {
+                stream.Write((byte)5);
+                stream.Write(asVector4.X);
+                stream.Write(asVector4.Y);
+                stream.Write(asVector4.Z);
+                stream.Write(asVector4.W);
+            }
+            else if (value is Color asColor)
+            {
+                stream.Write((byte)6);
+                stream.Write(asColor.R);
+                stream.Write(asColor.G);
+                stream.Write(asColor.B);
+                stream.Write(asColor.A);
+            }
+            else if (value is Guid asGuid)
+            {
+                stream.Write((byte)7);
+                stream.Write(asGuid.ToByteArray());
+            }
+            else
+            {
+                throw new SystemException();
             }
         }
 
@@ -200,25 +303,12 @@ namespace FlaxEditor.Surface
                     param.Type = (ParameterType)stream.ReadByte();
                     stream.Read(guidBytes, 0, 16);
                     param.ID = new Guid(guidBytes);
-                    {
-                        int length = stream.ReadInt32();
-                        if (length > 0 && length < 2000)
-                        {
-                            var str = stream.ReadBytes(length * 2);
-                            fixed (byte* strPtr = str)
-                            {
-                                var ptr = (char*)strPtr;
-                                for (int j = 0; j < length; j++)
-                                    ptr[j] = (char)(ptr[j] ^ 97);
-                            }
-                            param.Name = System.Text.Encoding.Unicode.GetString(str);
-                        }
-                    }
+                    param.Name = ReadStr(stream, 97);
                     param.IsPublic = stream.ReadByte() != 0;
                     param.IsStatic = stream.ReadByte() != 0;
                     param.IsUIVisible = stream.ReadByte() != 0;
                     param.IsUIEditable = stream.ReadByte() != 0;
-                    
+
                     // References
                     int refsCount = stream.ReadInt32();
                     param.ReferencedBy.Capacity = refsCount;
@@ -347,7 +437,6 @@ namespace FlaxEditor.Surface
         public bool Load(byte[] bytes)
         {
             Enabled = false;
-            Visible = false;
 
             // Clean data
             Parameters.Clear();
@@ -355,7 +444,7 @@ namespace FlaxEditor.Surface
             _startBox = null;
             _lastBoxUnderMouse = null;
             _surface.DisposeChildren();
-            
+
             bool result = true;
             try
             {
@@ -372,7 +461,6 @@ namespace FlaxEditor.Surface
                 Debug.LogException(ex);
             }
 
-            Visible = true;
             Enabled = true;
 
             if (result)
@@ -380,7 +468,7 @@ namespace FlaxEditor.Surface
                 // Error
                 return true;
             }
-            
+
             // Load surface meta
             var meta = Meta.GetEntry(10);
             if (meta.Data != null)
@@ -407,6 +495,183 @@ namespace FlaxEditor.Surface
             Owner.OnSurfaceEditedChanged();
 
             return false;
+        }
+
+        private bool saveGraph(BinaryWriter stream)
+        {
+            // IMPORTANT! This must match C++ Graph format
+            // Changes: don't write save time and keep engine build constant
+
+            // Magic Code
+            stream.Write(1963542358);
+
+            // Engine Build
+            stream.Write(6118);
+
+            // Time saved
+            stream.Write((long)0);
+
+            // Nodes count
+            stream.Write(_nodes.Count);
+
+            // Parameters count
+            stream.Write(Parameters.Count);
+
+            // For each node
+            for (int i = 0; i < _nodes.Count; i++)
+            {
+                var node = _nodes[i];
+
+                // ID
+                stream.Write(node.ID);
+
+                // Type
+                stream.Write(node.Type);
+            }
+
+            // For each param
+            for (int i = 0; i < Parameters.Count; i++)
+            {
+                var param = Parameters[i];
+
+                // Properties
+                stream.Write((byte)param.Type);
+                stream.Write(param.ID.ToByteArray());
+                WriteStr(stream, param.Name, 97);
+                stream.Write((byte)(param.IsPublic ? 1 : 0));
+                stream.Write((byte)(param.IsStatic ? 1 : 0));
+                stream.Write((byte)(param.IsUIVisible ? 1 : 0));
+                stream.Write((byte)(param.IsUIEditable ? 1 : 0));
+
+                // References
+                stream.Write(param.ReferencedBy.Count);
+                for (int j = 0; j < param.ReferencedBy.Count; j++)
+                    stream.Write(param.ReferencedBy[j].ID);
+
+                // Value
+                WriteCommonValue(stream, param.Value);
+
+                // Meta
+                param.Meta.Save(stream);
+            }
+
+            // For each node
+            for (int i = 0; i < _nodes.Count; i++)
+            {
+                var node = _nodes[i];
+
+                // Values
+                if (node.Values != null)
+                {
+                    stream.Write(node.Values.Length);
+                    for (int j = 0; j < node.Values.Length; j++)
+                        WriteCommonValue(stream, node.Values[j]);
+                }
+                else
+                {
+                    stream.Write(0);
+                }
+
+                // Boxes
+                var boxes = node.GetBoxes();
+                stream.Write((ushort)boxes.Count);
+                for (int j = 0; j < boxes.Count; j++)
+                {
+                    var box = boxes[j];
+
+                    stream.Write((byte)box.ID);
+                    stream.Write((uint)box.DefaultType);
+                    stream.Write((ushort)box.Connections.Count);
+                    for (int k = 0; k < box.Connections.Count; k++)
+                    {
+                        var targetBox = box.Connections[k];
+
+                        if (targetBox == null)
+                        {
+                            // Error
+                            return true;
+                        }
+
+                        stream.Write(targetBox.ParentNode.ID);
+                        stream.Write((byte)targetBox.ID);
+                    }
+                }
+
+                // Meta
+                node.Meta.Save(stream);
+            }
+
+            // Visject Meta
+            Meta.Save(stream);
+
+            // Ending char
+            stream.Write((byte)'\t');
+
+            return false;
+        }
+
+        /// <summary>
+        /// Saves the surface graph to bytes.
+        /// </summary>
+        /// <returns>The bytes with surface data or null if failed.</returns>
+        public byte[] Save()
+        {
+            Enabled = false;
+
+            // Save surface meta
+            VisjectSurfaceMeta10 meta10;
+            meta10.ViewPosition = ViewPosition;
+            meta10.Scale = ViewScale;
+            Meta.Release();
+            Meta.AddEntry(10, StructureToByteArray(ref meta10));
+
+            // Save all nodes meta
+            VisjectSurfaceMeta11 meta11;
+            for (int i = 0; i < _nodes.Count; i++)
+            {
+                var node = _nodes[i];
+                meta11.Position = node.Location;
+                meta11.Selected = node.IsSelected;
+                node.Meta.Release();
+                node.Meta.AddEntry(11, StructureToByteArray(ref meta11));
+            }
+
+            // Save graph
+            bool result = true;
+            byte[] bytes = null;
+            try
+            {
+                // Save graph
+                using (var stream = new MemoryStream())
+                using (var reader = new BinaryWriter(stream))
+                {
+                    result = saveGraph(reader);
+                    if (result == false)
+                        bytes = stream.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Error
+                Debug.LogException(ex);
+            }
+
+            Enabled = true;
+
+            if (result)
+            {
+                // Error
+                return null;
+            }
+
+            // Clear flag
+            if (_edited)
+            {
+                _edited = false;
+                Owner.OnSurfaceEditedChanged();
+            }
+
+            return bytes;
         }
     }
 }
