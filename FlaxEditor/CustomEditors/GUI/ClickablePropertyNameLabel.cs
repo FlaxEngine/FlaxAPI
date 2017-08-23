4a371d9a2bbd50dc664a962bffc494f1b96999f8
mafiesto4 <wojtek@figat.pl>
2017-08-23 20:46:42 +0200

    [CustomEditors] Added ClickablePropertyNameLabel with mouse events

---
+++ FlaxEditor/CustomEditors/GUI/ClickablePropertyNameLabel.cs
@@ -0,0 +1,92 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using FlaxEngine;
+
+namespace FlaxEditor.CustomEditors.GUI
+{
+    /// <summary>
+    /// Custom property name label that fires mouse events for label.
+    /// </summary>
+    /// <seealso cref="FlaxEditor.CustomEditors.GUI.PropertyNameLabel" />
+    public class ClickablePropertyNameLabel : PropertyNameLabel
+    {
+        /// <summary>
+        /// Mouse action delegate.
+        /// </summary>
+        /// <param name="label">The label.</param>
+        /// <param name="location">The mouse location.</param>
+        public delegate void MouseDelegate(ClickablePropertyNameLabel label, Vector2 location);
+
+        /// <summary>
+        /// The mouse left button clicks on the label.
+        /// </summary>
+        public MouseDelegate MouseLeftClick;
+
+        /// <summary>
+        /// The mouse right button clicks on the label.
+        /// </summary>
+        public MouseDelegate MouseRightClick;
+
+        /// <summary>
+        /// The mouse left button double clicks on the label.
+        /// </summary>
+        public MouseDelegate MouseLeftDoubleClick;
+
+        /// <summary>
+        /// The mouse right button double clicks on the label.
+        /// </summary>
+        public MouseDelegate MouseRightDoubleClick;
+
+        /// <inheritdoc />
+        public ClickablePropertyNameLabel(string name)
+            : base(name)
+        {
+        }
+
+        /// <inheritdoc />
+        public override bool OnMouseUp(Vector2 location, MouseButtons buttons)
+        {
+            // Fire events
+            if (buttons == MouseButtons.Left)
+            {
+                MouseLeftClick?.Invoke(this, location);
+            }
+            else if (buttons == MouseButtons.Right)
+            {
+                MouseRightClick?.Invoke(this, location);
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc />
+        public override bool OnMouseDoubleClick(Vector2 location, MouseButtons buttons)
+        {
+            // Fire events
+            if (buttons == MouseButtons.Left)
+            {
+                MouseLeftDoubleClick?.Invoke(this, location);
+            }
+            else if (buttons == MouseButtons.Right)
+            {
+                MouseRightDoubleClick?.Invoke(this, location);
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc />
+        public override void OnDestroy()
+        {
+            // Unlink events
+            MouseLeftClick = null;
+            MouseRightClick = null;
+            MouseLeftDoubleClick = null;
+            MouseRightDoubleClick = null;
+
+            base.OnDestroy();
+        }
+    }
+}
