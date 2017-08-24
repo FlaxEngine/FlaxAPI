73621e83bbef27c7380c3126a119afd7c4bcbd2a
mafiesto4 <wojtek@figat.pl>
2017-08-24 13:49:36 +0200

    [Actors] Added PostFxVolume actor type

--- FlaxEngine/API/Actors/Camera.cs
+++ FlaxEngine/API/Actors/Camera.cs
@@ -14,7 +14,6 @@ namespace FlaxEngine
 	public sealed partial class Camera
 	{
         // TODO: getMainCamera
-        // TODO: get/edit camera params
         // TODO: customAspectRatio, customViewport
         // TODO: ConvertMouseToRay
 
