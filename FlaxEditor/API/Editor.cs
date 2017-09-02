b2917e53d550973b121476f79e497d551a278910
mafiesto4 <wojtek@figat.pl>
2017-09-02 11:54:16 +0200

    [Editor] Building lightmaps progress reporting

--- FlaxEditor/API/Editor.cs
+++ FlaxEditor/API/Editor.cs
@@ -443,6 +443,97 @@ namespace FlaxEditor
 
         #endregion
 
+        #region Lightmaps Baking
+
+        /// <summary>
+        /// Lightmaps baking steps.
+        /// </summary>
+        public enum LightmapsBakeSteps
+        {
+            /// <summary>
+            /// The cache entries stage.
+            /// </summary>
+            CacheEntries,
+
+            /// <summary>
+            /// The generate lightmap charts stage.
+            /// </summary>
+            GenerateLightmapCharts,
+
+            /// <summary>
+            /// The pack lightmap charts stage.
+            /// </summary>
+            PackLightmapCharts,
+
+            /// <summary>
+            /// The update lightmaps collection stage.
+            /// </summary>
+            UpdateLightmapsCollection,
+
+            /// <summary>
+            /// The update entries stage.
+            /// </summary>
+            UpdateEntries,
+
+            /// <summary>
+            /// The generate hemispheres cache stage.
+            /// </summary>
+            GenerateHemispheresCache,
+
+            /// <summary>
+            /// The render hemispheres stage.
+            /// </summary>
+            RenderHemispheres,
+
+            /// <summary>
+            /// The cleanup stage.
+            /// </summary>
+            Cleanup
+        }
+
+        /// <summary>
+        /// Lightmaps baking progress event delegate.
+        /// </summary>
+        /// <param name="step">The current step.</param>
+        /// <param name="stepProgress">The current step progress (normalized to [0;1]).</param>
+        /// <param name="totalProgress">The total baking progress (normalized to [0;1]).</param>
+        public delegate void LightmapsBakeProgressDelegate(LightmapsBakeSteps step, float stepProgress, float totalProgress);
+
+        /// <summary>
+        /// Lighmaps baking nd event delegate.
+        /// </summary>
+        /// <param name="failed">True if baking failed or has been canceled, otherwise false.</param>
+        public delegate void LightmapsBakeEndDelegate(bool failed);
+
+        /// <summary>
+        /// Occurs when lightmaps baking starts.
+        /// </summary>
+        public static event Action LightmapsBakeStart;
+
+        /// <summary>
+        /// Occurs when lightmaps baking ends.
+        /// </summary>
+        public static event LightmapsBakeEndDelegate LightmapsBakeEnd;
+
+        /// <summary>
+        /// Occurs when lightmaps baking progress changes.
+        /// </summary>
+        public static event LightmapsBakeProgressDelegate LightmapsBakeProgress;
+        
+        internal static void Internal_LightmapsBake(LightmapsBakeSteps step, float stepProgress, float totalProgress, bool isProgressEvent)
+        {
+            if (isProgressEvent)
+                LightmapsBakeProgress?.Invoke(step, stepProgress, totalProgress);
+            else if (step == LightmapsBakeSteps.CacheEntries)
+                LightmapsBakeStart?.Invoke();
+            else if (step == LightmapsBakeSteps.GenerateLightmapCharts)
+                LightmapsBakeEnd?.Invoke(false);
+            else
+                LightmapsBakeEnd?.Invoke(true);
+        }
+
+        #endregion
+
         #region Internal Calls
 
 #if !UNIT_TEST_COMPILANT
