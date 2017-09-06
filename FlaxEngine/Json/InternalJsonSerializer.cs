9013d0eb056f44e063b1a09a49bc5a7b00ca517a
mafiesto4 <wojtek@figat.pl>
2017-09-06 16:14:05 +0200

    Use custom Newtonsoft.Json fork (binary included) to customize scripts serialization

--- FlaxEngine/Json/InternalJsonSerializer.cs
+++ FlaxEngine/Json/InternalJsonSerializer.cs
@@ -3,8 +3,13 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading;
 using FlaxEngine.Json.JsonCustomSerializers;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace FlaxEngine.Json
 {
@@ -35,6 +40,13 @@ namespace FlaxEngine.Json
         /// <inheritdoc />
         public override bool CanConvert(Type objectType)
         {
+            // Skip serialziation as reference id for the root object serialization (eg. Script)
+            var writer = InternalJsonSerializer.CurrentWriter.Value;
+            if (writer != null && writer.SerializeStackSize == 0)
+            {
+                return false;
+            }
+
             return typeof(Object).IsAssignableFrom(objectType);
         }
     }
@@ -42,6 +54,7 @@ namespace FlaxEngine.Json
     internal static class InternalJsonSerializer
     {
         public static JsonSerializerSettings Settings = CreateDefaultSettings();
+        public static ThreadLocal<JsonSerializerInternalWriter> CurrentWriter = new ThreadLocal<JsonSerializerInternalWriter>();
 
         public static JsonSerializerSettings CreateDefaultSettings()
         {
@@ -57,7 +70,35 @@ namespace FlaxEngine.Json
 
         internal static string Serialize(object obj)
         {
-            return JsonConvert.SerializeObject(obj, Formatting.Indented, Settings);
+            Type type = obj.GetType();
+
+            JsonSerializer jsonSerializer = JsonSerializer.CreateDefault(Settings);
+            jsonSerializer.Formatting = Formatting.Indented;
+
+            StringBuilder sb = new StringBuilder(256);
+            StringWriter sw = new StringWriter(sb, CultureInfo.InvariantCulture);
+            using (JsonTextWriter jsonWriter = new JsonTextWriter(sw))
+            {
+                // Prepare writer settings
+                jsonWriter.IndentChar = '\t';
+                jsonWriter.Indentation = 1;
+                jsonWriter.Formatting = jsonSerializer.Formatting;
+                jsonWriter.DateFormatHandling = jsonSerializer.DateFormatHandling;
+                jsonWriter.DateTimeZoneHandling = jsonSerializer.DateTimeZoneHandling;
+                jsonWriter.FloatFormatHandling = jsonSerializer.FloatFormatHandling;
+                jsonWriter.StringEscapeHandling = jsonSerializer.StringEscapeHandling;
+                jsonWriter.Culture = jsonSerializer.Culture;
+                jsonWriter.DateFormatString = jsonSerializer.DateFormatString;
+
+                JsonSerializerInternalWriter serializerWriter = new JsonSerializerInternalWriter(jsonSerializer);
+                CurrentWriter.Value = serializerWriter;
+
+                serializerWriter.Serialize(jsonWriter, obj, type);
+
+                CurrentWriter.Value = null;
+            }
+
+            return sw.ToString();
         }
 
         internal static void Deserialize(object input, string json)
