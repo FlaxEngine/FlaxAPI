17320872c63de3fdb88851eecb5b0bf82c80a72c
mafiesto4 <wojtek@figat.pl>
2017-10-16 11:58:38 +0200

    Added JsonAsset type

---
+++ FlaxEngine/API/Assets/JsonAsset.cs
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+namespace FlaxEngine
+{
+	public partial class JsonAsset
+	{
+        /// <summary>
+        /// The texture asset type unique ID.
+        /// </summary>
+        public const int TypeID = 15;
+        
+        /// <summary>
+        /// The asset type content domain.
+        /// </summary>
+        public const ContentDomain Domain = ContentDomain.Document;
+    }
+}
