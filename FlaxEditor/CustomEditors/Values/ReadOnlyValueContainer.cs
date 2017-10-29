0e0c2b17915fda9dbcc6645d0b8106203f6f1c5b
mafiesto4 <wojtek@figat.pl>
2017-10-29 17:56:22 +0100

    [CustomEditors] Added ReadOnlyValueContainer

---
+++ FlaxEditor/CustomEditors/Values/ReadOnlyValueContainer.cs
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+namespace FlaxEditor.CustomEditors
+{
+    /// <summary>
+    /// Custom <see cref="ValueContainer"/> for read-only values.
+    /// </summary>
+    /// <seealso cref="FlaxEditor.CustomEditors.ValueContainer" />
+    public sealed class ReadOnlyValueContainer : ValueContainer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadOnlyValueContainer"/> class.
+        /// </summary>
+        /// <param name="value">The initial value.</param>
+        public ReadOnlyValueContainer(object value)
+            : base(null, typeof(object))
+        {
+            Add(value);
+        }
+
+        /// <inheritdoc />
+        public override void Refresh(ValueContainer instanceValues)
+        {
+            // Not supported
+        }
+
+        /// <inheritdoc />
+        public override void Set(ValueContainer instanceValues, object value)
+        {
+            // Not supported
+        }
+
+        /// <inheritdoc />
+        public override void Set(ValueContainer instanceValues)
+        {
+            // Not supported
+        }
+    }
+}
