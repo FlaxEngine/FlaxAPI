b0eb773b0033956d66839e3a8e8fb4f8125482c9
mafiesto4 <wojtek@figat.pl>
2017-11-19 19:05:06 +0100

    [CustomEditors] Use helper Gizmo colors (red/gree/blue) for the actor transform axis (better usage)

--- FlaxEditor/CustomEditors/Dedicated/ActorEditor.cs
+++ FlaxEditor/CustomEditors/Dedicated/ActorEditor.cs
@@ -311,12 +311,31 @@ namespace FlaxEditor.CustomEditors.Dedicated
         /// <inheritdoc />
         protected override void SpawnProperty(LayoutElementsContainer itemLayout, ValueContainer itemValues, ItemInfo item)
         {
-            // Override layer editor
-            if(item.Order?.Order == -69)
-                item.CustomEditor = new CustomEditorAttribute(typeof(ActorLayerEditor));
-            // Override tag editor
-            else if (item.Order?.Order == -68)
-                item.CustomEditor = new CustomEditorAttribute(typeof(ActorTagEditor));
+            // Note: we cannot specify actor properties editor types directly because we want to keep editor classes in FlaxEditor assembly
+            int order = item.Order?.Order ?? int.MinValue;
+            switch (order)
+            {
+                // Override layer editor
+                case -69:
+                    item.CustomEditor = new CustomEditorAttribute(typeof(ActorLayerEditor));
+                    break;
+
+                // Override tag editor
+                case -68:
+                    item.CustomEditor = new CustomEditorAttribute(typeof(ActorTagEditor));
+                    break;
+
+                // Override position/scale editor
+                case -30:
+                case -10:
+                    item.CustomEditor = new CustomEditorAttribute(typeof(ActorTransformEditor.PositionScaleEditor));
+                    break;
+
+                // Override orientation editor
+                case -20:
+                    item.CustomEditor = new CustomEditorAttribute(typeof(ActorTransformEditor.OrientationEditor));
+                    break;
+            }
 
             base.SpawnProperty(itemLayout, itemValues, item);
         }
