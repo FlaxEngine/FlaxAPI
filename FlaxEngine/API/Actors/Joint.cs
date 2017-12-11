af7c198432488cb0fa3c2b99ee357eaafe3fd1ed
mafiesto4 <wojtek@figat.pl>
2017-12-11 18:24:13 +0100

    Added base Joint class

---
+++ FlaxEngine/API/Actors/Joint.cs
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlaxEngine
+{
+	public abstract partial class Joint
+	{
+	}
+}
