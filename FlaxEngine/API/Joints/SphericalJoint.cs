f9e8b2d36768a2091e8298bd6c8a94afb1bc882d
mafiesto4 <wojtek@figat.pl>
2017-12-11 21:45:47 +0100

    Added SphericalJoint

---
+++ FlaxEngine/API/Joints/SphericalJoint.cs
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace FlaxEngine
+{
+    /// <summary>
+    /// Flags that control spherical joint options.
+    /// </summary>
+    [Flags]
+    public enum SphericalJointFlag
+    {
+        /// <summary>
+        /// The none.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The joint cone range limit is enabled.
+        /// </summary>
+        Limit = 0x1
+    }
+
+    public sealed partial class SphericalJoint
+    {
+    }
+}
