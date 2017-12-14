4265db276d3fe76ef54327b7ca0c36373c2e1bf5
mafiesto4 <wojtek@figat.pl>
2017-12-14 19:57:46 +0100

    Added more GraphicsSettings and GraphicsQuality to edit game quality at runtime

--- FlaxEditor/Content/Settings/GraphicsSettings.cs
+++ FlaxEditor/Content/Settings/GraphicsSettings.cs
@@ -3,12 +3,14 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 using FlaxEngine;
+using FlaxEngine.Rendering;
 
 namespace FlaxEditor.Content.Settings
 {
     /// <summary>
-    /// The graphics rendering settings container. Allows to edit asset via editor.
+    /// The graphics rendering settings container. Allows to edit asset via editor. To modify those settings at runtime use <see cref="GraphicsQuality"/>.
     /// </summary>
+    /// <seealso cref="GraphicsQuality"/>
     public sealed class GraphicsSettings : SettingsBase
     {
         /// <summary>
@@ -16,5 +18,29 @@ namespace FlaxEditor.Content.Settings
         /// </summary>
         [EditorOrder(20), EditorDisplay("General"), Tooltip("Enables rendering synchronization with the refresh rate of the display device to avoid \"tearing\" artifacts.")]
         public bool UseVSync = false;
+
+        /// <summary>
+        /// Screen Space Reflections quality.
+        /// </summary>
+        [EditorOrder(1000), EditorDisplay("Quality", "SSR Quality"), Tooltip("Screen Space Reflections quality.")]
+        public Quality SSRQuality;
+
+        /// <summary>
+        /// Screen Space Ambient Occlusion quality setting.
+        /// </summary>
+        [EditorOrder(1010), EditorDisplay("Quality", "SSAO Quality"), Tooltip("Screen Space Ambient Occlusion quality setting.")]
+        public Quality SSAOQuality;
+
+        /// <summary>
+        /// The shadows quality.
+        /// </summary>
+        [EditorOrder(1020), EditorDisplay("Quality", "Shadows Quality"), Tooltip("The shadows quality.")]
+        public Quality ShadowsQuality;
+
+        /// <summary>
+        /// The shadow maps quality (textures resolution).
+        /// </summary>
+        [EditorOrder(1030), EditorDisplay("Quality", "Shadow Maps Quality"), Tooltip("The shadow maps quality (textures resolution).")]
+        public Quality ShadowMapsQuality;
     }
 }
---
+++ FlaxEngine/API/Static/GraphicsQuality.cs
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using System.Runtime.CompilerServices;
+
+namespace FlaxEngine.Rendering
+{
+    public static partial class GraphicsQuality
+    {
+        /// <summary>
+        /// Enables rendering synchronization with the refresh rate of the display device to avoid "tearing" artifacts.
+        /// </summary>
+        public static bool UseVSync
+        {
+            get { return Internal_GetValue(0) != 0; }
+            set { Internal_SetValue(0, value ? 1 : 0); }
+        }
+
+        /// <summary>
+        /// Screen Space Reflections quality.
+        /// </summary>
+        public static Quality SSRQuality
+        {
+            get { return (Quality)Internal_GetValue(1); }
+            set { Internal_SetValue(1, (int)value); }
+        }
+
+        /// <summary>
+        /// Screen Space Ambient Occlusion quality setting.
+        /// </summary>
+        public static Quality SSAOQuality
+        {
+            get { return (Quality)Internal_GetValue(2); }
+            set { Internal_SetValue(2, (int)value); }
+        }
+
+        /// <summary>
+        /// The shadows quality.
+        /// </summary>
+        public static Quality ShadowsQuality
+        {
+            get { return (Quality)Internal_GetValue(3); }
+            set { Internal_SetValue(3, (int)value); }
+        }
+
+        /// <summary>
+        /// The shadow maps quality (textures resolution).
+        /// </summary>
+        public static Quality ShadowMapsQuality
+        {
+            get { return (Quality)Internal_GetValue(4); }
+            set { Internal_SetValue(4, (int)value); }
+        }
+
+        #region Internal Calls
+
+#if !UNIT_TEST_COMPILANT
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        internal static extern int Internal_GetValue(int index);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        internal static extern void Internal_SetValue(int index, int value);
+#endif
+
+        #endregion
+    }
+}
