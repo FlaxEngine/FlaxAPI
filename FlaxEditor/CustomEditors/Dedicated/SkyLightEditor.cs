0b701ba6c2cfe632e46fd1a6278f0e9646c90a06
mafiesto4 <wojtek@figat.pl>
2017-12-17 12:26:51 +0100

    Added SkyLightEditor to inject Bake probe button

---
+++ FlaxEditor/CustomEditors/Dedicated/SkyLightEditor.cs
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2012-2017 Flax Engine. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+using FlaxEngine;
+
+namespace FlaxEditor.CustomEditors.Dedicated
+{
+    /// <summary>
+    /// Custom editor for <see cref="SkyLight"/>.
+    /// </summary>
+    /// <seealso cref="ActorEditor" />
+    [CustomEditor(typeof(SkyLight)), DefaultEditor]
+    public class SkyLightEditor : ActorEditor
+    {
+        /// <inheritdoc />
+        public override void Initialize(LayoutElementsContainer layout)
+        {
+            base.Initialize(layout);
+
+            if (Values.HasDiffrentTypes == false)
+            {
+                // Add 'Bake' button
+                layout.Space(10);
+                var button = layout.Button("Bake");
+                button.Button.Clicked += BakeButtonClicked;
+            }
+        }
+
+        private void BakeButtonClicked()
+        {
+            for (int i = 0; i < Values.Count; i++)
+            {
+                if (Values[i] is SkyLight skyLight)
+                {
+                    skyLight.Bake();
+                    Editor.Instance.Scene.MarkSceneEdited(skyLight.Scene);
+                }
+            }
+        }
+    }
+}
