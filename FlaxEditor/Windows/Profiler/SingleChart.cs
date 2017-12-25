1b5cbe9375d2e08991061fbb168369065059eca6
mafiesto4 <wojtek@figat.pl>
2017-12-25 15:30:45 +0100

    Drawing SingleChart

--- FlaxEditor/Windows/Profiler/SingleChart.cs
+++ FlaxEditor/Windows/Profiler/SingleChart.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FlaxEngine;
 using FlaxEngine.GUI;
 
@@ -18,7 +19,6 @@ namespace FlaxEditor.Windows.Profiler
         private const float TitleHeight = 20;
         private readonly List<float> _samples;
         private string _sample;
-        private Vector2 _samplesRange;
         private int _maxSamples;
 
         /// <summary>
@@ -59,7 +59,10 @@ namespace FlaxEditor.Windows.Profiler
         public void AddSample(float value)
         {
             if (_samples.Count == _maxSamples)
+            {
                 _samples.RemoveAt(0);
+            }
+
             _samples.Add(value);
             _sample = FormatSample(value);
         }
@@ -71,15 +74,45 @@ namespace FlaxEditor.Windows.Profiler
 
             var style = Style.Current;
             float chartHeight = Height - TitleHeight;
+            float pointsOffset = 4.0f;
 
             // Draw chart
-            // TODO: ..
+            if (_samples.Count > 0)
+            {
+                int samplesInViewCount = Math.Min((int)(Width / pointsOffset), _samples.Count);
+                float maxValue = _samples[0];
+                for (int i = 1; i < samplesInViewCount; i++)
+                {
+                    maxValue = Mathf.Max(maxValue, _samples[i]);
+                }
+
+                var chartRect = new Rectangle(0, 0, Width, chartHeight);
+                Render2D.PushClip(ref chartRect);
+                Color chartColor = style.BackgroundSelected;
+                Vector2 chartRoot = chartRect.BottomRight;
+                float samplesRange = maxValue * 1.1f;
+                float samplesCoeff = -chartHeight / samplesRange;
+                Vector2 posPrev = chartRoot + new Vector2(0, _samples.Last() * samplesCoeff);
+                float posX = 0;
+
+                for (int i = _samples.Count - 1; i >= 0; i--)
+                {
+                    float sample = _samples[i];
+                    Vector2 pos = chartRoot + new Vector2(posX, sample * samplesCoeff);
+                    Render2D.DrawLine(posPrev, pos, chartColor);
+                    posPrev = pos;
+                    posX -= pointsOffset;
+                }
+
+                Render2D.PopClip();
+            }
 
             // Draw title
-            var headerRect = new Rectangle(2, chartHeight, Width - 4, TitleHeight);
+            var headerRect = new Rectangle(0, chartHeight, Width, TitleHeight);
+            var headerTextRect = new Rectangle(2, chartHeight, Width - 4, TitleHeight);
             Render2D.FillRectangle(headerRect, style.BackgroundNormal);
-            Render2D.DrawText(style.FontMedium, Title, headerRect, Color.White * 0.8f, TextAlignment.Near, TextAlignment.Center);
-            Render2D.DrawText(style.FontMedium, _sample, headerRect, Color.White, TextAlignment.Far, TextAlignment.Center);
+            Render2D.DrawText(style.FontMedium, Title, headerTextRect, Color.White * 0.8f, TextAlignment.Near, TextAlignment.Center);
+            Render2D.DrawText(style.FontMedium, _sample, headerTextRect, Color.White, TextAlignment.Far, TextAlignment.Center);
         }
     }
 }
