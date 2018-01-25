ffab3d4bc0b10b7f2384bee883cc945b6ae09ed2
mafiesto4 <wojtek@figat.pl>
2018-01-25 17:21:36 +0100

    Tweak PlayingState restoring engine settings, reduce physics simulation fps in edit mode to 30fps

--- FlaxEditor/States/PlayingState.cs
+++ FlaxEditor/States/PlayingState.cs
@@ -8,110 +8,118 @@ using FlaxEngine.Utilities;
 
 namespace FlaxEditor.States
 {
-    /// <summary>
-    /// In this state editor is simulating game.
-    /// </summary>
-    /// <seealso cref="FlaxEditor.States.EditorState" />
-    public sealed class PlayingState : EditorState
-    {
-        private readonly DuplicateScenes _duplicateScenes = new DuplicateScenes();
+	/// <summary>
+	/// In this state editor is simulating game.
+	/// </summary>
+	/// <seealso cref="FlaxEditor.States.EditorState" />
+	public sealed class PlayingState : EditorState
+	{
+		private readonly DuplicateScenes _duplicateScenes = new DuplicateScenes();
 
-        /// <summary>
-        /// Gets a value indicating whether any scene was dirty before entering the play mode.
-        /// </summary>
-        public bool WasDirty => _duplicateScenes.WasDirty;
+		/// <summary>
+		/// Gets a value indicating whether any scene was dirty before entering the play mode.
+		/// </summary>
+		public bool WasDirty => _duplicateScenes.WasDirty;
 
-        /// <inheritdoc />
-        public override bool CanEditScene => true;
+		/// <inheritdoc />
+		public override bool CanEditScene => true;
 
-        /// <inheritdoc />
-        public override bool CanEnterPlayMode => true;
+		/// <inheritdoc />
+		public override bool CanEnterPlayMode => true;
 
-        /// <summary>
-        /// Occurs when play mode is starting (before scene duplicating).
-        /// </summary>
-        public event Action SceneDuplicating;
+		/// <summary>
+		/// Occurs when play mode is starting (before scene duplicating).
+		/// </summary>
+		public event Action SceneDuplicating;
 
-        /// <summary>
-        /// Occurs when play mode is starting (after scene duplicating).
-        /// </summary>
-        public event Action SceneDuplicated;
+		/// <summary>
+		/// Occurs when play mode is starting (after scene duplicating).
+		/// </summary>
+		public event Action SceneDuplicated;
 
-        /// <summary>
-        /// Occurs when play mode is ending (before scene restoring).
-        /// </summary>
-        public event Action SceneRestoring;
+		/// <summary>
+		/// Occurs when play mode is ending (before scene restoring).
+		/// </summary>
+		public event Action SceneRestoring;
 
-        /// <summary>
-        /// Occurs when play mode is ending (after scene restoring).
-        /// </summary>
-        public event Action SceneRestored;
+		/// <summary>
+		/// Occurs when play mode is ending (after scene restoring).
+		/// </summary>
+		public event Action SceneRestored;
 
-        /// <summary>
-        /// Gets or sets a value indicating whether game logic is paused.
-        /// </summary>
-        public bool IsPaused
-        {
-            get => !SceneManager.IsGameLogicRunning;
-            set
-            {
-                if (!IsActive)
-                    throw new InvalidOperationException();
+		/// <summary>
+		/// Gets or sets a value indicating whether game logic is paused.
+		/// </summary>
+		public bool IsPaused
+		{
+			get => !SceneManager.IsGameLogicRunning;
+			set
+			{
+				if (!IsActive)
+					throw new InvalidOperationException();
 
-                SceneManager.IsGameLogicRunning = !value;
-            }
-        }
+				SceneManager.IsGameLogicRunning = !value;
+			}
+		}
 
-        internal PlayingState(Editor editor)
-            : base(editor)
-        {
-        }
+		internal PlayingState(Editor editor)
+			: base(editor)
+		{
+			SetupEditorEnvOptions();
+		}
 
-        /// <inheritdoc />
-        public override void OnEnter()
-        {
-            Input.ScanGamepads();
+		/// <inheritdoc />
+		public override void OnEnter()
+		{
+			Input.ScanGamepads();
 
-            // Remove references to the scene objects
-            Editor.Scene.ClearRefsToSceneObjects();
+			// Remove references to the scene objects
+			Editor.Scene.ClearRefsToSceneObjects();
 
-            // Apply game settings (user may modify them before the gameplay)
-            GameSettings.Apply();
-            
-            // Duplicate editor scene for simulation
-            SceneDuplicating?.Invoke();
-            _duplicateScenes.GatherSceneData();
-            SceneDuplicated?.Invoke();
+			// Apply game settings (user may modify them before the gameplay)
+			GameSettings.Apply();
 
-            // Fire events
-            Editor.OnPlayBegin();
-            IsPaused = false;
-        }
+			// Duplicate editor scene for simulation
+			SceneDuplicating?.Invoke();
+			_duplicateScenes.GatherSceneData();
+			SceneDuplicated?.Invoke();
 
-        /// <inheritdoc />
-        public override void OnExit(State nextState)
-        {
-            IsPaused = true;
+			// Fire events
+			Editor.OnPlayBegin();
+			IsPaused = false;
+		}
 
-            // Remove references to the scene objects
-            Editor.Scene.ClearRefsToSceneObjects();
+		private void SetupEditorEnvOptions()
+		{
+			Time.TimeScale = 1.0f;
+			Time.PhysicsFPS = 30;
+			Physics.AutoSimulation = true;
+			Screen.CursorVisible = true;
+			Screen.CursorLock = CursorLockMode.None;
+		}
 
-            // Restore editor scene
-            SceneRestoring?.Invoke();
-            _duplicateScenes.RestoreSceneData();
-            SceneRestored?.Invoke();
+		/// <inheritdoc />
+		public override void OnExit(State nextState)
+		{
+			IsPaused = true;
 
-            // Restore game settings and state for editor environment
-            Time.TimeScale = 1.0f;
-            Physics.AutoSimulation = true;
-            Screen.CursorVisible = true;
-            var win = Editor.Windows.GameWin?.ParentWindow;
-            if (win != null)
-                win.Cursor = CursorType.Default;
+			// Remove references to the scene objects
+			Editor.Scene.ClearRefsToSceneObjects();
 
-            // Fire events
-            Editor.OnPlayEnd();
-            IsPaused = true;
-        }
-    }
+			// Restore editor scene
+			SceneRestoring?.Invoke();
+			_duplicateScenes.RestoreSceneData();
+			SceneRestored?.Invoke();
+
+			// Restore game settings and state for editor environment
+			SetupEditorEnvOptions();
+			var win = Editor.Windows.GameWin?.ParentWindow;
+			if (win != null)
+				win.Cursor = CursorType.Default;
+
+			// Fire events
+			Editor.OnPlayEnd();
+			IsPaused = true;
+		}
+	}
 }
