e51064c8236e426a13603fb3ee10356a435d2be6
mafiesto4 <wojtek@figat.pl>
2018-02-26 23:39:15 +0100

    Added AudioClip C# API

--- FlaxEngine/API/BinaryAssets/AudioClip.Gen.cs
+++ FlaxEngine/API/BinaryAssets/AudioClip.Gen.cs
@@ -26,8 +26,42 @@ namespace FlaxEngine
 		{
 		}
 
+		/// <summary>
+		/// Gets the audio data format.
+		/// </summary>
+		[UnmanagedCall]
+		public AudioFormat Format
+		{
+#if UNIT_TEST_COMPILANT
+			get; set;
+#else
+			get { return Internal_GetFormat(unmanagedPtr); }
+#endif
+		}
+
+		/// <summary>
+		/// Gets the audio data info metadata.
+		/// </summary>
+		/// <param name="info">The output audio data header from the asset. May be invalid if asset is not loaded.</param>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+		[UnmanagedCall]
+		public void GetInfo(out AudioDataInfo info) 
+		{
+#if UNIT_TEST_COMPILANT
+			throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+			Internal_GetInfo(unmanagedPtr, out info);
+#endif
+		}
+
 #region Internal Calls
 #if !UNIT_TEST_COMPILANT
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal static extern AudioFormat Internal_GetFormat(IntPtr obj);
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		internal static extern void Internal_GetInfo(IntPtr obj, out AudioDataInfo info);
 #endif
 #endregion
 	}
--- FlaxEngine/API/BinaryAssets/AudioClip.cs
+++ FlaxEngine/API/BinaryAssets/AudioClip.cs
@@ -17,5 +17,30 @@ namespace FlaxEngine
 
 	public sealed partial class AudioClip
 	{
+		/// <summary>
+		/// Meta-data describing a chunk of audio.
+		/// </summary>
+		public struct AudioDataInfo
+		{
+			/// <summary>
+			/// The total number of audio samples in the audio data (includes all channels).
+			/// </summary>
+			public uint NumSamples;
+
+			/// <summary>
+			/// The number of audio samples per second, per channel.
+			/// </summary>
+			public uint SampleRate;
+
+			/// <summary>
+			/// The number of channels. Each channel has its own set of samples.
+			/// </summary>
+			public uint NumChannels;
+
+			/// <summary>
+			/// The number of bits per sample.
+			/// </summary>
+			public uint BitDepth;
+		}
 	}
 }
