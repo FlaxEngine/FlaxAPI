7282d9eada4174d3497110b78d0efb4fc25c0a83
mafiesto4 <wojtek@figat.pl>
2018-03-01 22:17:55 +0100

    Added AudioSettings.MuteOnFocusLoss

--- FlaxEditor/Content/Settings/AudioSettings.cs
+++ FlaxEditor/Content/Settings/AudioSettings.cs
@@ -16,5 +16,11 @@ namespace FlaxEditor.Content.Settings
 		/// </summary>
 		[EditorOrder(100), EditorDisplay("General"), Limit(0, 10.0f, 0.01f), Tooltip("The doppler doppler effect factor. Scale for source and listener velocities. Default is 1.")]
 		public float DopplerFactor = 1.0f;
+
+		/// <summary>
+		/// True if mute all audio playback when game has no use focus.
+		/// </summary>
+		[EditorOrder(200), EditorDisplay("General", "Mute On Focus Loss"), Tooltip("If checked, engine will mute all audio playback when game has no use focus.")]
+		public bool MuteOnFocusLoss = true;
 	}
 }
