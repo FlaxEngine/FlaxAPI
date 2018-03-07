cef8e3f90a750f4df41cdcf504e63a0edb9a5393
mafiesto4 <wojtek@figat.pl>
2018-03-07 22:01:42 +0100

    Fixes to json serializer (also format code)

--- FlaxEngine/Json/JsonCustomSerializers/ExtendedDefaultContractResolver.cs
+++ FlaxEngine/Json/JsonCustomSerializers/ExtendedDefaultContractResolver.cs
@@ -11,80 +11,112 @@ using Newtonsoft.Json.Serialization;
 
 namespace FlaxEngine.Json.JsonCustomSerializers
 {
-    internal class ExtendedDefaultContractResolver : DefaultContractResolver
-    {
-        private Type[] _attributesIgnoreList =
-        {
-            typeof(UnmanagedCallAttribute),
-            typeof(NonSerializedAttribute),
-            typeof(NoSerializeAttribute)
-        };
+	internal class ExtendedDefaultContractResolver : DefaultContractResolver
+	{
+		private readonly Type _flaxType = typeof(Object);
 
-        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
-        {
-            var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+		private readonly Type[] _attributesIgnoreList =
+		{
+			typeof(UnmanagedCallAttribute),
+			typeof(NonSerializedAttribute),
+			typeof(NoSerializeAttribute)
+		};
 
-            var result = new List<JsonProperty>(fields.Length + properties.Length);
+		/// <inheritdoc />
+		protected override JsonContract CreateContract(Type objectType)
+		{
+			var contract = base.CreateContract(objectType);
 
-            for (int i = 0; i < fields.Length; i++)
-            {
-                var f = fields[i];
-                var attributes = f.GetCustomAttributes();
+			// Override contract for Flax objects
+			if (_flaxType.IsAssignableFrom(objectType))
+			{
+				((JsonObjectContract)contract).ItemReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+			}
 
-                // Serialize non-public fields only with a proper attribute
-                if (!f.IsPublic && !attributes.Any(x => x is SerializeAttribute))
-                    continue;
+			return contract;
+		}
 
-                // Check if has attribute to skip serialization
-                bool noSerialize = false;
-                foreach (var attribute in attributes)
-                {
-                    if (_attributesIgnoreList.Contains(attribute.GetType()))
-                    {
-                        noSerialize = true;
-                        break;
-                    }
-                }
-                if (noSerialize)
-                    continue;
+		protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+		{
+			var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+			var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
-                var jsonProperty = CreateProperty(f, memberSerialization);
-                jsonProperty.Writable = true;
-                jsonProperty.Readable = true;
-                result.Add(jsonProperty);
-            }
+			var result = new List<JsonProperty>(fields.Length + properties.Length);
 
-            for (int i = 0; i < properties.Length; i++)
-            {
-                var p = properties[i];
+			for (int i = 0; i < fields.Length; i++)
+			{
+				var f = fields[i];
+				var attributes = f.GetCustomAttributes();
 
-                // Serialize only properties with read/write
-                if (!(p.CanRead && p.CanWrite && p.GetIndexParameters().GetLength(0) == 0))
-                    continue;
+				// Serialize non-public fields only with a proper attribute
+				if (!f.IsPublic && !attributes.Any(x => x is SerializeAttribute))
+					continue;
 
-                var attributes = p.GetCustomAttributes();
+				// Check if has attribute to skip serialization
+				bool noSerialize = false;
+				foreach (var attribute in attributes)
+				{
+					if (_attributesIgnoreList.Contains(attribute.GetType()))
+					{
+						noSerialize = true;
+						break;
+					}
+				}
 
-                // Check if has attribute to skip serialization
-                bool noSerialize = false;
-                foreach (var attribute in attributes)
-                {
-                    if (_attributesIgnoreList.Contains(attribute.GetType()))
-                    {
-                        noSerialize = true;
-                        break;
-                    }
-                }
-                if (noSerialize)
-                    continue;
+				if (noSerialize)
+					continue;
 
-                var jsonProperty = CreateProperty(p, memberSerialization);
-                jsonProperty.Writable = true;
-                jsonProperty.Readable = true;
-                result.Add(jsonProperty);
-            }
+				var jsonProperty = CreateProperty(f, memberSerialization);
+				jsonProperty.Writable = true;
+				jsonProperty.Readable = true;
 
-            return result;
-        }
-    }
+				if (_flaxType.IsAssignableFrom(f.FieldType))
+				{
+					jsonProperty.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+					jsonProperty.Converter = JsonSerializer.ObjectConverter;
+				}
+
+				result.Add(jsonProperty);
+			}
+
+			for (int i = 0; i < properties.Length; i++)
+			{
+				var p = properties[i];
+
+				// Serialize only properties with read/write
+				if (!(p.CanRead && p.CanWrite && p.GetIndexParameters().GetLength(0) == 0))
+					continue;
+
+				var attributes = p.GetCustomAttributes();
+
+				// Check if has attribute to skip serialization
+				bool noSerialize = false;
+				foreach (var attribute in attributes)
+				{
+					if (_attributesIgnoreList.Contains(attribute.GetType()))
+					{
+						noSerialize = true;
+						break;
+					}
+				}
+
+				if (noSerialize)
+					continue;
+
+				var jsonProperty = CreateProperty(p, memberSerialization);
+				jsonProperty.Writable = true;
+				jsonProperty.Readable = true;
+
+				if (_flaxType.IsAssignableFrom(p.PropertyType))
+				{
+					jsonProperty.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+					jsonProperty.Converter = JsonSerializer.ObjectConverter;
+				}
+
+				result.Add(jsonProperty);
+			}
+
+			return result;
+		}
+	}
 }
--- FlaxEngine/Json/JsonSerializer.cs
+++ FlaxEngine/Json/JsonSerializer.cs
@@ -13,257 +13,259 @@ using Newtonsoft.Json.Serialization;
 
 namespace FlaxEngine.Json
 {
-    /// <summary>
-    /// Serialize references to the FlaxEngine.Object as Guid (format N).
-    /// </summary>
-    /// <seealso cref="Newtonsoft.Json.JsonConverter" />
-    internal class FlaxObjectConverter : JsonConverter
-    {
-        internal struct GuidInterop
-        {
-            public uint A;
-            public uint B;
-            public uint C;
-            public uint D;
-        }
+	/// <summary>
+	/// Serialize references to the FlaxEngine.Object as Guid (format N).
+	/// </summary>
+	/// <seealso cref="Newtonsoft.Json.JsonConverter" />
+	internal class FlaxObjectConverter : JsonConverter
+	{
+		internal struct GuidInterop
+		{
+			public uint A;
+			public uint B;
+			public uint C;
+			public uint D;
+		}
 
-        internal static unsafe void ParseHex(char* str, int length, out uint result)
-        {
-            uint sum = 0;
-            char* p = str;
-            char* end = str + length;
+		internal static unsafe void ParseHex(char* str, int length, out uint result)
+		{
+			uint sum = 0;
+			char* p = str;
+			char* end = str + length;
 
-            if (*p == '0' && *(p + 1) == 'x')
-                p += 2;
+			if (*p == '0' && *(p + 1) == 'x')
+				p += 2;
 
-            while (p < end  && *p != 0)
-            {
-                int c = *p - '0';
+			while (p < end && *p != 0)
+			{
+				int c = *p - '0';
 
-                if (c < 0 || c > 9)
-                {
-                    c = char.ToLower(*p) - 'a' + 10;
-                    if (c < 10 || c > 15)
-                    {
-                        result = 0;
-                        return;
-                    }
-                }
+				if (c < 0 || c > 9)
+				{
+					c = char.ToLower(*p) - 'a' + 10;
+					if (c < 10 || c > 15)
+					{
+						result = 0;
+						return;
+					}
+				}
 
-                sum = 16 * sum + (uint)c;
+				sum = 16 * sum + (uint)c;
 
-                p++;
-            }
+				p++;
+			}
 
-            result = sum;
-        }
+			result = sum;
+		}
 
-        internal static void ParseHex(string str, int start, int length, out uint result)
-        {
-            uint sum = 0;
-            int p = start;
-            int end = start + length;
+		internal static void ParseHex(string str, int start, int length, out uint result)
+		{
+			uint sum = 0;
+			int p = start;
+			int end = start + length;
 
-            if (str.Length < end)
-            {
-                result = 0;
-                return;
-            }
+			if (str.Length < end)
+			{
+				result = 0;
+				return;
+			}
 
-            if (str[p] == '0' && str[p + 1] == 'x')
-                p += 2;
+			if (str[p] == '0' && str[p + 1] == 'x')
+				p += 2;
 
-            while (p < end && str[p] != 0)
-            {
-                int c = str[p] - '0';
+			while (p < end && str[p] != 0)
+			{
+				int c = str[p] - '0';
 
-                if (c < 0 || c > 9)
-                {
-                    c = char.ToLower(str[p]) - 'a' + 10;
-                    if (c < 10 || c > 15)
-                    {
-                        result = 0;
-                        return;
-                    }
-                }
+				if (c < 0 || c > 9)
+				{
+					c = char.ToLower(str[p]) - 'a' + 10;
+					if (c < 10 || c > 15)
+					{
+						result = 0;
+						return;
+					}
+				}
 
-                sum = 16 * sum + (uint)c;
+				sum = 16 * sum + (uint)c;
 
-                p++;
-            }
+				p++;
+			}
 
-            result = sum;
-        }
+			result = sum;
+		}
 
-        internal static unsafe string GetStringID(Guid id)
-        {
-            GuidInterop* g = (GuidInterop*)&id;
-            return string.Format("{0:x8}{1:x8}{2:x8}{3:x8}", g->A, g->B, g->C, g->D);
-        }
+		internal static unsafe string GetStringID(Guid id)
+		{
+			GuidInterop* g = (GuidInterop*)&id;
+			return string.Format("{0:x8}{1:x8}{2:x8}{3:x8}", g->A, g->B, g->C, g->D);
+		}
 
-        internal static unsafe void ParseID(string str, out Guid id)
-        {
-            GuidInterop g;
+		internal static unsafe void ParseID(string str, out Guid id)
+		{
+			GuidInterop g;
 
-            // Broken after VS 15.5
-            /*fixed (char* a = str)
-            {
-                char* b = a + 8;
-                char* c = b + 8;
-                char* d = c + 8;
+			// Broken after VS 15.5
+			/*fixed (char* a = str)
+			{
+			    char* b = a + 8;
+			    char* c = b + 8;
+			    char* d = c + 8;
 
-                ParseHex(a, 8, out g.A);
-                ParseHex(b, 8, out g.B);
-                ParseHex(c, 8, out g.C);
-                ParseHex(d, 8, out g.D);
-            }*/
+			    ParseHex(a, 8, out g.A);
+			    ParseHex(b, 8, out g.B);
+			    ParseHex(c, 8, out g.C);
+			    ParseHex(d, 8, out g.D);
+			}*/
 
-            // Temporary fix (not using raw char* pointer)
-            ParseHex(str, 0, 8, out g.A);
-            ParseHex(str, 8, 8, out g.B);
-            ParseHex(str, 16, 8, out g.C);
-            ParseHex(str, 24, 8, out g.D);
+			// Temporary fix (not using raw char* pointer)
+			ParseHex(str, 0, 8, out g.A);
+			ParseHex(str, 8, 8, out g.B);
+			ParseHex(str, 16, 8, out g.C);
+			ParseHex(str, 24, 8, out g.D);
 
-            id = *(Guid*)&g;
-        }
+			id = *(Guid*)&g;
+		}
 
-        /// <inheritdoc />
-        public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
-        {
-            Guid id = Guid.Empty;
-            if (value is Object obj)
-                id = obj.ID;
+		/// <inheritdoc />
+		public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+		{
+			Guid id = Guid.Empty;
+			if (value is Object obj)
+				id = obj.ID;
 
-            writer.WriteValue(GetStringID(id));
-        }
+			writer.WriteValue(GetStringID(id));
+		}
 
-        /// <inheritdoc />
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.String)
-            {
-                Guid id;
-                ParseID((string)reader.Value, out id);
-                return Object.Find<Object>(ref id);
-            }
+		/// <inheritdoc />
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+		{
+			if (reader.TokenType == JsonToken.String)
+			{
+				Guid id;
+				ParseID((string)reader.Value, out id);
+				return Object.Find<Object>(ref id);
+			}
 
-            return null;
-        }
+			return null;
+		}
 
-        /// <inheritdoc />
-        public override bool CanConvert(Type objectType)
-        {
-            // Skip serialziation as reference id for the root object serialization (eg. Script)
-            var writer = JsonSerializer.CurrentWriter.Value;
-            if (writer != null && writer.SerializeStackSize == 0)
-            {
-                return false;
-            }
+		/// <inheritdoc />
+		public override bool CanConvert(Type objectType)
+		{
+			// Skip serialziation as reference id for the root object serialization (eg. Script)
+			var writer = JsonSerializer.CurrentWriter.Value;
+			if (writer != null && writer.SerializeStackSize == 0)
+			{
+				return false;
+			}
 
-            return typeof(Object).IsAssignableFrom(objectType);
-        }
-    }
-    /*
-    /// <summary>
-    /// Serialize Guid values using `N` format
-    /// </summary>
-    /// <seealso cref="Newtonsoft.Json.JsonConverter" />
-    internal class GuidConverter : JsonConverter
-    {
-        /// <inheritdoc />
-        public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
-        {
-            Guid id = (Guid)value;
-            writer.WriteValue(id.ToString("N"));
-        }
+			return typeof(Object).IsAssignableFrom(objectType);
+		}
+	}
 
-        /// <inheritdoc />
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.String)
-            {
-                var id = Guid.Parse((string)reader.Value);
-                return id;
-            }
+	/*
+	/// <summary>
+	/// Serialize Guid values using `N` format
+	/// </summary>
+	/// <seealso cref="Newtonsoft.Json.JsonConverter" />
+	internal class GuidConverter : JsonConverter
+	{
+	    /// <inheritdoc />
+	    public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+	    {
+	        Guid id = (Guid)value;
+	        writer.WriteValue(id.ToString("N"));
+	    }
 
-            return Guid.Empty;
-        }
+	    /// <inheritdoc />
+	    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+	    {
+	        if (reader.TokenType == JsonToken.String)
+	        {
+	            var id = Guid.Parse((string)reader.Value);
+	            return id;
+	        }
 
-        /// <inheritdoc />
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof(Guid);
-        }
-    }
-    */
-    /// <summary>
-    /// Objects serialization tool (json format).
-    /// </summary>
-    public static class JsonSerializer
-    {
-        internal static JsonSerializerSettings Settings = CreateDefaultSettings();
-        internal static ThreadLocal<JsonSerializerInternalWriter> CurrentWriter = new ThreadLocal<JsonSerializerInternalWriter>();
+	        return Guid.Empty;
+	    }
 
-        internal static JsonSerializerSettings CreateDefaultSettings()
-        {
-            var settings = new JsonSerializerSettings
-            {
-                ContractResolver = new ExtendedDefaultContractResolver(),
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                TypeNameHandling = TypeNameHandling.Auto,
-                NullValueHandling = NullValueHandling.Ignore,
-            };
-            settings.Converters.Add(new FlaxObjectConverter());
-            //settings.Converters.Add(new GuidConverter());
-            return settings;
-        }
+	    /// <inheritdoc />
+	    public override bool CanConvert(Type objectType)
+	    {
+	        return objectType == typeof(Guid);
+	    }
+	}
+	*/
+	/// <summary>
+	/// Objects serialization tool (json format).
+	/// </summary>
+	public static class JsonSerializer
+	{
+		internal static JsonSerializerSettings Settings = CreateDefaultSettings();
+		internal static FlaxObjectConverter ObjectConverter;
+		internal static ThreadLocal<JsonSerializerInternalWriter> CurrentWriter = new ThreadLocal<JsonSerializerInternalWriter>();
 
-        /// <summary>
-        /// Serializes the specified object.
-        /// </summary>
-        /// <param name="obj">The object.</param>
-        /// <returns>The output json string.</returns>
-        public static string Serialize(object obj)
-        {
-            Type type = obj.GetType();
+		internal static JsonSerializerSettings CreateDefaultSettings()
+		{
+			var settings = new JsonSerializerSettings
+			{
+				ContractResolver = new ExtendedDefaultContractResolver(),
+				ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+				TypeNameHandling = TypeNameHandling.Auto,
+				NullValueHandling = NullValueHandling.Ignore,
+			};
+			settings.Converters.Add(ObjectConverter = new FlaxObjectConverter());
+			//settings.Converters.Add(new GuidConverter());
+			return settings;
+		}
 
-            Newtonsoft.Json.JsonSerializer jsonSerializer = Newtonsoft.Json.JsonSerializer.CreateDefault(Settings);
-            jsonSerializer.Formatting = Formatting.Indented;
+		/// <summary>
+		/// Serializes the specified object.
+		/// </summary>
+		/// <param name="obj">The object.</param>
+		/// <returns>The output json string.</returns>
+		public static string Serialize(object obj)
+		{
+			Type type = obj.GetType();
 
-            StringBuilder sb = new StringBuilder(256);
-            StringWriter sw = new StringWriter(sb, CultureInfo.InvariantCulture);
-            using (JsonTextWriter jsonWriter = new JsonTextWriter(sw))
-            {
-                // Prepare writer settings
-                jsonWriter.IndentChar = '\t';
-                jsonWriter.Indentation = 1;
-                jsonWriter.Formatting = jsonSerializer.Formatting;
-                jsonWriter.DateFormatHandling = jsonSerializer.DateFormatHandling;
-                jsonWriter.DateTimeZoneHandling = jsonSerializer.DateTimeZoneHandling;
-                jsonWriter.FloatFormatHandling = jsonSerializer.FloatFormatHandling;
-                jsonWriter.StringEscapeHandling = jsonSerializer.StringEscapeHandling;
-                jsonWriter.Culture = jsonSerializer.Culture;
-                jsonWriter.DateFormatString = jsonSerializer.DateFormatString;
+			Newtonsoft.Json.JsonSerializer jsonSerializer = Newtonsoft.Json.JsonSerializer.CreateDefault(Settings);
+			jsonSerializer.Formatting = Formatting.Indented;
 
-                JsonSerializerInternalWriter serializerWriter = new JsonSerializerInternalWriter(jsonSerializer);
-                CurrentWriter.Value = serializerWriter;
+			StringBuilder sb = new StringBuilder(256);
+			StringWriter sw = new StringWriter(sb, CultureInfo.InvariantCulture);
+			using (JsonTextWriter jsonWriter = new JsonTextWriter(sw))
+			{
+				// Prepare writer settings
+				jsonWriter.IndentChar = '\t';
+				jsonWriter.Indentation = 1;
+				jsonWriter.Formatting = jsonSerializer.Formatting;
+				jsonWriter.DateFormatHandling = jsonSerializer.DateFormatHandling;
+				jsonWriter.DateTimeZoneHandling = jsonSerializer.DateTimeZoneHandling;
+				jsonWriter.FloatFormatHandling = jsonSerializer.FloatFormatHandling;
+				jsonWriter.StringEscapeHandling = jsonSerializer.StringEscapeHandling;
+				jsonWriter.Culture = jsonSerializer.Culture;
+				jsonWriter.DateFormatString = jsonSerializer.DateFormatString;
 
-                serializerWriter.Serialize(jsonWriter, obj, type);
+				JsonSerializerInternalWriter serializerWriter = new JsonSerializerInternalWriter(jsonSerializer);
+				CurrentWriter.Value = serializerWriter;
 
-                CurrentWriter.Value = null;
-            }
+				serializerWriter.Serialize(jsonWriter, obj, type);
 
-            return sw.ToString();
-        }
+				CurrentWriter.Value = null;
+			}
 
-        /// <summary>
-        /// Deserializes the specified object (from the input json data).
-        /// </summary>
-        /// <param name="input">The objeect.</param>
-        /// <param name="json">The input json data.</param>
-        public static void Deserialize(object input, string json)
-        {
-            JsonConvert.PopulateObject(json, input, Settings);
-        }
-    }
+			return sw.ToString();
+		}
+
+		/// <summary>
+		/// Deserializes the specified object (from the input json data).
+		/// </summary>
+		/// <param name="input">The objeect.</param>
+		/// <param name="json">The input json data.</param>
+		public static void Deserialize(object input, string json)
+		{
+			JsonConvert.PopulateObject(json, input, Settings);
+		}
+	}
 }
