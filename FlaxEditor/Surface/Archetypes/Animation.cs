d5a4643c3970dec7d66f7dee2b17d723eb86678e
mafiesto4 <wojtek@figat.pl>
2018-03-27 19:25:16 +0200

    Added Bone Transform node (in local space)

--- FlaxEditor/Surface/Archetypes/Animation.cs
+++ FlaxEditor/Surface/Archetypes/Animation.cs
@@ -61,7 +61,28 @@ namespace FlaxEditor.Surface.Archetypes
 				Resize(Mathf.Max(230, style.FontLarge.MeasureText(Name).X + 20), 160);
 			}
 		}
-		
+
+		/// <summary>
+		/// The bone transformation modes.
+		/// </summary>
+		public enum BoneTransformMode
+		{
+			/// <summary>
+			/// No transformation.
+			/// </summary>
+			None = 0,
+
+			/// <summary>
+			/// Applies the transformation.
+			/// </summary>
+			Add = 1,
+
+			/// <summary>
+			/// Replaces the transformation.
+			/// </summary>
+			Replace = 2,
+		}
+
 		/// <summary>
 		/// The nodes for that group.
 		/// </summary>
@@ -106,6 +127,29 @@ namespace FlaxEditor.Surface.Archetypes
 					NodeElementArchetype.Factory.Input(1, "Loop", true, ConnectionType.Bool, 6, 2),
 					NodeElementArchetype.Factory.Input(2, "Start Position", true, ConnectionType.Float, 7, 3),
 					NodeElementArchetype.Factory.Asset(0, Surface.Constants.LayoutOffsetY * 3, 0, ContentDomain.Animation),
+				}
+			},
+			new NodeArchetype
+			{
+				TypeID = 3,
+				Title = "Transform Bone (local space)",
+				Description = "Trnsforms the skeleton root bone",
+				Flags = NodeFlags.AnimGraphOnly,
+				Size = new Vector2(260, 130),
+				DefaultValues = new object[]
+				{
+					0,
+					(int)BoneTransformMode.Add,
+				},
+				Elements = new[]
+				{
+					NodeElementArchetype.Factory.Output(0, string.Empty, ConnectionType.Impulse, 0),
+					NodeElementArchetype.Factory.Input(0, string.Empty, true, ConnectionType.Impulse, 1),
+					NodeElementArchetype.Factory.Input(1, "Translation", true, ConnectionType.Vector3, 2),
+					NodeElementArchetype.Factory.Input(2, "Rotation", true, ConnectionType.Rotation, 3),
+					NodeElementArchetype.Factory.Input(3, "Scale", true, ConnectionType.Vector3, 4),
+					NodeElementArchetype.Factory.SkeletonNodeSelect(0, Surface.Constants.LayoutOffsetY * 4, 120, 0),
+					NodeElementArchetype.Factory.ComboBox(0, Surface.Constants.LayoutOffsetY * 5, 120, 1, typeof(BoneTransformMode)),
 				}
 			},
 		};
