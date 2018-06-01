4f26d5da0ecca0731bb136e54d799966524b03fe
mafiesto4 <wojtek@figat.pl>
2018-06-01 16:34:17 +0200

    Added option for selected camera preview

--- FlaxEditor/Options/InterfaceOptions.cs
+++ FlaxEditor/Options/InterfaceOptions.cs
@@ -11,6 +11,12 @@ namespace FlaxEditor.Options
     public class InterfaceOptions
     {
         /// <summary>
+        /// Gets or sets a value indicating whether show selected camera preview in the editor window.
+        /// </summary>
+        [EditorDisplay("Interface"), EditorOrder(80), Tooltip("Determines whether show selected camera preview in the edit window.")]
+        public bool ShowSelectedCameraPreview { get; set; } = true;
+
+        /// <summary>
         /// Gets or sets a value indicating whether center mouse position on window focus in play mode. Helps when working with games that lock mouse cursor.
         /// </summary>
         [EditorDisplay("Interface", "Center Mouse On Game Window Focus"), EditorOrder(100), Tooltip("Determines whether center mouse position on window focus in play mode. Helps when working with games that lock mouse cursor.")]
