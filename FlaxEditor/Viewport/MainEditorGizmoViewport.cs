534667f5d754da84d42fba9dd4cc247a7ee84bd0
mafiesto4 <wojtek@figat.pl>
2018-06-04 21:39:47 +0200

    Skip colliders when drag and drop into editor window

--- FlaxEditor/Viewport/MainEditorGizmoViewport.cs
+++ FlaxEditor/Viewport/MainEditorGizmoViewport.cs
@@ -644,7 +644,7 @@ namespace FlaxEditor.Viewport
                 var ray = ConvertMouseToRay(ref location);
                 float closest = float.MaxValue;
                 var gridPlane = new Plane(Vector3.Zero, Vector3.Up);
-                hit = Editor.Instance.Scene.Root.RayCast(ref ray, ref closest);
+                hit = Editor.Instance.Scene.Root.RayCast(ref ray, ref closest, SceneGraphNode.RayCastData.FlagTypes.SkipColliders);
                 if (hit != null)
                 {
                     // Use hit location
