745572c08aeff22ff7731af6b51152f8dffd9fc7
mafiesto4 <wojtek@figat.pl>
2018-06-08 20:26:06 +0200

    Added UICanvas.RenderMode and serialization

--- FlaxEngine/API/Actors/UICanvas.cs
+++ FlaxEngine/API/Actors/UICanvas.cs
@@ -1,8 +1,78 @@
 // Copyright (c) 2012-2018 Wojciech Figat. All rights reserved.
 
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
 namespace FlaxEngine
 {
+    /// <summary>
+    /// The canvas rendering modes.
+    /// </summary>
+    public enum CanvasRenderMode
+    {
+        /// <summary>
+        /// The screen space rendering mode that places UI elements on the screen rendered on top of the scene. If the screen is resized or changes resolution, the Canvas will automatically change size to match this.
+        /// </summary>
+        ScreenSpace = 0,
+
+        /// <summary>
+        /// The camera space rendering mode that places Canvas in a given distance in front of a specified Camera. The UI elements are rendered by this camera, which means that the Camera settings affect the appearance of the UI. If the Camera is set to Perspective, the UI elements will be rendered with perspective, and the amount of perspective distortion can be controlled by the Camera Field of View. If the screen is resized, changes resolution, or the camera frustum changes, the Canvas will automatically change size to match as well.
+        /// </summary>
+        CameraSpace = 1,
+
+        /// <summary>
+        /// The world space rendering mode that places Canvas as any other object in the scene. The size of the Canvas can be set manually using its Rect Transform, and UI elements will render in front of or behind other objects in the scene based on 3D placement. This is useful for UIs that are meant to be a part of the world. This is also known as a "diegetic interface".
+        /// </summary>
+        WorldSpace = 2,
+    }
+
     public sealed partial class UICanvas
     {
+        private CanvasRenderMode _renderMode;
+
+        /// <summary>
+        /// Gets or sets the canvas rendering mode.
+        /// </summary>
+        [EditorOrder(10), EditorDisplay("Canvas"), Tooltip("Canvas rendering mode.")]
+        public CanvasRenderMode RenderMode
+        {
+            get => _renderMode;
+            set
+            {
+                if (_renderMode != value)
+                {
+                    _renderMode = value;
+
+                    // TODO: handle change
+                }
+            }
+        }
+
+        internal string Serialize()
+        {
+            StringBuilder sb = new StringBuilder(256);
+            StringWriter sw = new StringWriter(sb, CultureInfo.InvariantCulture);
+            using (JsonTextWriter jsonWriter = new JsonTextWriter(sw))
+            {
+                jsonWriter.IndentChar = '\t';
+                jsonWriter.Indentation = 1;
+
+                jsonWriter.WriteStartObject();
+
+                jsonWriter.WritePropertyName("RenderMode");
+                jsonWriter.WriteValue(_renderMode);
+
+                jsonWriter.WriteEndObject();
+            }
+
+            return sw.ToString();
+        }
+
+        internal void Deserialize(string json)
+        {
+            Json.JsonSerializer.Deserialize(this, json);
+        }
     }
 }
