49b75d1d2db60c6dee97d663730cd154f4792983
mafiesto4 <wojtek@figat.pl>
2018-07-15 23:09:24 +0200

    Added PrefabManager

---
+++ FlaxEngine/API/Static/PrefabManager.cs
@@ -0,0 +1,20 @@
+// Copyright (c) 2012-2018 Wojciech Figat. All rights reserved.
+
+namespace FlaxEngine
+{
+    public static partial class PrefabManager
+    {
+        /// <summary>
+        /// Spawns the instance of the prefab objects. Prefab wil be spawned to the first scene.
+        /// </summary>
+        /// <param name="prefab">The prefab asset.</param>
+        /// <returns>The created actor (root) or null if failed</returns>
+        public static Actor SpawnPrefab(Prefab prefab)
+        {
+            var scene = SceneManager.GetScene(0);
+            if (scene == null)
+                return null;
+            return SpawnPrefab(prefab, scene);
+        }
+    }
+}
