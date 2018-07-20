f129acf9c4348c5924d61a825014d50fed01d1c5
mafiesto4 <wojtek@figat.pl>
2018-07-20 12:19:54 +0200

    Implementing prefab instance buttons in ActorEditor and showing full diff

--- FlaxEditor/CustomEditors/Dedicated/ActorEditor.cs
+++ FlaxEditor/CustomEditors/Dedicated/ActorEditor.cs
@@ -3,7 +3,9 @@
 using System;
 using System.Collections.Generic;
 using FlaxEditor.CustomEditors.Editors;
+using FlaxEditor.GUI;
 using FlaxEngine;
+using FlaxEngine.GUI;
 
 namespace FlaxEditor.CustomEditors.Dedicated
 {
@@ -66,6 +68,156 @@ namespace FlaxEditor.CustomEditors.Dedicated
             }
 
             return items;
+        }
+
+        /// <inheritdoc />
+        public override void Initialize(LayoutElementsContainer layout)
+        {
+            // Check for prefab link
+            if (Values.IsSingleObject && Values[0] is Actor actor && actor.HasPrefabLink)
+            {
+                // TODO: consider editing more than one instance of the same prefab asset at once
+
+                var prefab = FlaxEngine.Content.LoadAsync<Prefab>(actor.PrefabID);
+                // TODO: don't stall here?
+                if (prefab && !prefab.WaitForLoaded())
+                {
+                    var prefabObjectId = actor.PrefabObjectID;
+                    var prefabInstance = Prefab.Internal_GetDefaultInstance(prefab.unmanagedPtr, ref prefabObjectId);
+                    if (prefabInstance != null)
+                    {
+                        // Use default prefab instance as a reference for the editor
+                        Values.SetReferenceValue(prefabInstance);
+
+                        // Add some UI
+                        var panel = layout.CustomContainer<UniformGridPanel>();
+                        panel.CustomControl.Height = 20.0f;
+                        panel.CustomControl.SlotsVertically = 1;
+                        panel.CustomControl.SlotsHorizontally = 2;
+
+                        // Selecting actor prefab asset
+                        var selectPrefab = panel.Button("Select Prefab");
+                        selectPrefab.Button.Clicked += () => Editor.Instance.Windows.ContentWin.Select(prefab);
+
+                        // Viewwing changes applied to this actor
+                        var viewChanges = panel.Button("View Changes");
+                        viewChanges.Button.Clicked += () => ViewChanges(viewChanges.Button, new Vector2(0.0f, 20.0f));
+                    }
+                }
+            }
+
+            base.Initialize(layout);
+        }
+
+        private TreeNode CreateDiffNode(CustomEditor editor)
+        {
+            var node = new TreeNode(false);
+
+            node.Tag = editor;
+
+            if (editor.ParentEditor?.Values?.Type?.IsArray ?? false)
+            {
+                node.Text = "Element " + editor.ParentEditor.ChildrenEditors.IndexOf(editor);
+            }
+            else if (editor.Values.Info != null)
+            {
+                node.Text = CustomEditorsUtil.GetPropertyNameUI(editor.Values.Info.Name);
+            }
+            else if (editor.Values[0] != null)
+            {
+                node.Text = editor.Values[0].ToString();
+            }
+
+            if (editor.Values[0] is Actor || editor.Values[0] is Script)
+            {
+                node.TextColor = FlaxEngine.GUI.Style.Current.ProgressNormal;
+            }
+
+            node.Expand(true);
+
+            return node;
+        }
+
+        private TreeNode ProcessDiff(CustomEditor editor)
+        {
+            // Skip if no change detected
+            if (!editor.Values.IsReferenceValueModified)
+                return null;
+
+            bool isArray = editor.Values.IsArray;
+
+            TreeNode result = null;
+
+            if (!isArray)
+                result = CreateDiffNode(editor);
+
+            for (int i = 0; i < editor.ChildrenEditors.Count; i++)
+            {
+                var child = ProcessDiff(editor.ChildrenEditors[i]);
+                if (child != null)
+                {
+                    if (result == null)
+                        result = CreateDiffNode(editor);
+
+                    result.AddChild(child);
+                }
+            }
+
+            return result;
+        }
+
+        private void ViewChanges(Control target, Vector2 targetLocation)
+        {
+            // Build a tree out of modified properties
+            var rootNode = ProcessDiff(this);
+
+            // Skip if no changes detected
+            if (rootNode == null || rootNode.ChildrenCount == 0)
+            {
+                var cm1 = new ContextMenu();
+                cm1.AddButton("No changes detected");
+                cm1.Show(target, targetLocation);
+                return;
+            }
+
+            // Create context menu
+            var cm = new PrefabDiffContextMenu();
+            cm.Tree.AddChild(rootNode);
+            cm.Tree.RightClick += OnDiffNodeRightClick;
+            cm.Tree.Tag = cm;
+            cm.RevertAll += OnDiffRevertAll;
+            cm.ApplyAll += OnDiffApplyAll;
+            cm.Show(target, targetLocation);
+        }
+
+        private void OnDiffNodeRightClick(TreeNode node, Vector2 location)
+        {
+            var diffMenu = (PrefabDiffContextMenu)node.ParentTree.Tag;
+
+            var menu = new ContextMenu();
+            menu.AddButton("Revert", () => OnDiffRevert((CustomEditor)node.Tag));
+            menu.AddSeparator();
+            menu.AddButton("Revert All", OnDiffRevertAll);
+            menu.AddButton("Apply All", OnDiffApplyAll);
+
+            diffMenu.ShowChild(menu, node.PointToParent(diffMenu, new Vector2(location.X, node.HeaderHeight)));
+        }
+
+        private void OnDiffRevertAll()
+        {
+            OnDiffRevert(this);
+        }
+
+        private void OnDiffApplyAll()
+        {
+            MessageBox.Show("Apply all");
+        }
+
+        private void OnDiffRevert(CustomEditor editor)
+        {
+            Editor.Log("Reverting object changes to prefab");
+
+            MessageBox.Show("Revert " + editor.GetType());
         }
     }
 }
