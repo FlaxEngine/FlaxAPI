83d0b5f1cab19236f05a718b23de1b24b7092cef
mafiesto4 <wojtek@figat.pl>
2018-07-26 22:30:56 +0200

    Add TextRender.UpdateLayout

--- FlaxEngine/API/Actors/TextRender.Gen.cs
+++ FlaxEngine/API/Actors/TextRender.Gen.cs
@@ -205,6 +205,22 @@ namespace FlaxEngine
 #endif
         }
 
+        /// <summary>
+        /// Updates the text vertex buffer layout and cached data if its dirty.
+        /// </summary>
+#if UNIT_TEST_COMPILANT
+        [Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        [UnmanagedCall]
+        public void UpdateLayout()
+        {
+#if UNIT_TEST_COMPILANT
+            throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+            Internal_UpdateLayout(unmanagedPtr);
+#endif
+        }
+
         #region Internal Calls
 
 #if !UNIT_TEST_COMPILANT
@@ -273,6 +289,9 @@ namespace FlaxEngine
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         internal static extern void Internal_SetBaseLinesGapScale(IntPtr obj, float val);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        internal static extern void Internal_UpdateLayout(IntPtr obj);
 #endif
 
         #endregion
