1c0f24cddaa6c90b8d52aab500c2926948934926
mafiesto4 <wojtek@figat.pl>
2018-08-03 00:49:31 +0200

    Add methods to Render2D to draw texture/sprite with point sampler(except of linear)

--- FlaxEngine/API/Static/Render2D.Gen.cs
+++ FlaxEngine/API/Static/Render2D.Gen.cs
@@ -251,6 +251,46 @@ namespace FlaxEngine
         }
 
         /// <summary>
+        /// Draw texture (uses point sampler)
+        /// </summary>
+        /// <param name="t">Texture to draw</param>
+        /// <param name="rect">Rectangle to draw</param>
+        /// <param name="color">Color to use</param>
+        /// <param name="withAlpha">True if use alpha blending, otherwise it will be disabled.</param>
+#if UNIT_TEST_COMPILANT
+        [Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        [UnmanagedCall]
+        public static void DrawTexturePoint(Texture t, Rectangle rect, Color color, bool withAlpha = false)
+        {
+#if UNIT_TEST_COMPILANT
+            throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+            Internal_DrawTexture1(Object.GetUnmanagedPtr(t), ref rect, ref color, withAlpha);
+#endif
+        }
+
+        /// <summary>
+        /// Draw texture (uses point sampler)
+        /// </summary>
+        /// <param name="t">Texture to draw</param>
+        /// <param name="rect">Rectangle to draw</param>
+        /// <param name="color">Color to use</param>
+        /// <param name="withAlpha">True if use alpha blending, otherwise it will be disabled.</param>
+#if UNIT_TEST_COMPILANT
+        [Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        [UnmanagedCall]
+        public static void DrawTexturePoint(SpriteAtlas t, Rectangle rect, Color color, bool withAlpha = false)
+        {
+#if UNIT_TEST_COMPILANT
+            throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+            Internal_DrawTexture2(Object.GetUnmanagedPtr(t), ref rect, ref color, withAlpha);
+#endif
+        }
+
+        /// <summary>
         /// Draw line
         /// </summary>
         /// <param name="p1">Start point</param>
--- FlaxEngine/API/Static/Render2D.cs
+++ FlaxEngine/API/Static/Render2D.cs
@@ -142,6 +142,45 @@ namespace FlaxEngine
 #endif
         }
 
+        /// <summary>
+        /// Draws sprite (uses point sampler).
+        /// </summary>
+        /// <param name="sprite">Sprite to draw.</param>
+        /// <param name="rect">Rectangle to draw.</param>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        [UnmanagedCall]
+        public static void DrawSpritePoint(Sprite sprite, Rectangle rect)
+        {
+#if UNIT_TEST_COMPILANT
+			throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+            Color color = Color.White;
+            Internal_DrawSpritePoint(Object.GetUnmanagedPtr(sprite.Atlas), sprite.Index, ref rect, ref color, true);
+#endif
+        }
+
+        /// <summary>
+        /// Draws sprite (uses point sampler).
+        /// </summary>
+        /// <param name="sprite">Sprite to draw.</param>
+        /// <param name="rect">Rectangle to draw.</param>
+        /// <param name="color">Color to multiply all texture pixels.</param>
+        /// <param name="withAlpha">True if use alpha blending, otherwise it will be disabled.</param>
+#if UNIT_TEST_COMPILANT
+		[Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        [UnmanagedCall]
+        public static void DrawSpritePoint(Sprite sprite, Rectangle rect, Color color, bool withAlpha = true)
+        {
+#if UNIT_TEST_COMPILANT
+			throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+            Internal_DrawSpritePoint(Object.GetUnmanagedPtr(sprite.Atlas), sprite.Index, ref rect, ref color, withAlpha);
+#endif
+        }
+
         #region Internal Calls
 
 #if !UNIT_TEST_COMPILANT
@@ -156,6 +195,9 @@ namespace FlaxEngine
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         internal static extern void Internal_DrawSprite(IntPtr atlas, int index, ref Rectangle rect, ref Color color, bool withAlpha);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        internal static extern void Internal_DrawSpritePoint(IntPtr atlas, int index, ref Rectangle rect, ref Color color, bool withAlpha);
 #endif
 
         #endregion
