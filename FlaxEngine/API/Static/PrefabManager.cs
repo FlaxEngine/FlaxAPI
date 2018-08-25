127d39d7e88c885ef40720ab1e68c8bc98f37df5
mafiesto4 <wojtek@figat.pl>
2018-08-25 21:33:51 +0200

    Add more helper methods for prefabs spawning

--- FlaxEngine/API/Static/PrefabManager.cs
+++ FlaxEngine/API/Static/PrefabManager.cs
@@ -11,13 +11,87 @@ namespace FlaxEngine
         /// Spawns the instance of the prefab objects. Prefab will be spawned to the first loaded scene.
         /// </summary>
         /// <param name="prefab">The prefab asset.</param>
-        /// <returns>The created actor (root) or null if failed</returns>
+        /// <returns>The created actor (root) or null if failed.</returns>
         public static Actor SpawnPrefab(Prefab prefab)
         {
             var scene = SceneManager.GetScene(0);
             if (scene == null)
                 return null;
             return SpawnPrefab(prefab, scene);
+        }
+
+        /// <summary>
+        /// Spawns the instance of the prefab objects. Prefab will be spawned to the first loaded scene.
+        /// </summary>
+        /// <param name="prefab">The prefab asset.</param>
+        /// <param name="position">The spawn position in the world space.</param>
+        /// <returns>The created actor (root) or null if failed.</returns>
+        public static Actor SpawnPrefab(Prefab prefab, Vector3 position)
+        {
+            var instance = SpawnPrefab(prefab);
+            if (instance)
+            {
+                instance.Position = position;
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Spawns the instance of the prefab objects. Prefab will be spawned to the first loaded scene.
+        /// </summary>
+        /// <param name="prefab">The prefab asset.</param>
+        /// <param name="position">The spawn position in the world space.</param>
+        /// <param name="rotation">The spawn rotation (in world space).</param>
+        /// <returns>The created actor (root) or null if failed.</returns>
+        public static Actor SpawnPrefab(Prefab prefab, Vector3 position, Quaternion rotation)
+        {
+            var instance = SpawnPrefab(prefab);
+            if (instance)
+            {
+                var transform = instance.Transform;
+                transform.Translation = position;
+                transform.Orientation = rotation;
+                instance.Transform = transform;
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Spawns the instance of the prefab objects. Prefab will be spawned to the first loaded scene.
+        /// </summary>
+        /// <param name="prefab">The prefab asset.</param>
+        /// <param name="position">The spawn position in the world space.</param>
+        /// <param name="rotation">The spawn rotation (in world space).</param>
+        /// <param name="scale">The spawn scale.</param>
+        /// <returns>The created actor (root) or null if failed.</returns>
+        public static Actor SpawnPrefab(Prefab prefab, Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            var instance = SpawnPrefab(prefab);
+            if (instance)
+            {
+                Transform transform;
+                transform.Translation = position;
+                transform.Orientation = rotation;
+                transform.Scale = scale;
+                instance.Transform = transform;
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Spawns the instance of the prefab objects. Prefab will be spawned to the first loaded scene.
+        /// </summary>
+        /// <param name="prefab">The prefab asset.</param>
+        /// <param name="transform">The spawn transformation in the world space.</param>
+        /// <returns>The created actor (root) or null if failed.</returns>
+        public static Actor SpawnPrefab(Prefab prefab, Transform transform)
+        {
+            var instance = SpawnPrefab(prefab);
+            if (instance)
+            {
+                instance.Transform = transform;
+            }
+            return instance;
         }
 
         #region Internal Calls
