05e421f34712a375077fdac709efa37825c7f05d
mafiesto4 <wojtek@figat.pl>
2018-10-14 22:49:28 +0200

    Add editor terrain actor part

---
+++ FlaxEditor/SceneGraph/Actors/TerrainNode.cs
@@ -0,0 +1,18 @@
+// Copyright (c) 2012-2018 Wojciech Figat. All rights reserved.
+
+using FlaxEngine;
+
+namespace FlaxEditor.SceneGraph.Actors
+{
+    /// <summary>
+    /// Scene tree node for <see cref="Terrain"/> actor type.
+    /// </summary>
+    public sealed class TerrainNode : ActorNode
+    {
+        /// <inheritdoc />
+        public TerrainNode(Actor actor)
+        : base(actor)
+        {
+        }
+    }
+}
