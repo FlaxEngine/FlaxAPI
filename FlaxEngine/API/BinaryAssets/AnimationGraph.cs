450f6d733862f796655c7517a52b4802758691e1
mafiesto4 <wojtek@figat.pl>
2018-12-23 14:32:27 +0100

    Add AnimationGraph custom nodes evaluation logic

--- FlaxEngine/API/BinaryAssets/AnimationGraph.cs
+++ FlaxEngine/API/BinaryAssets/AnimationGraph.cs
@@ -1,6 +1,8 @@
 // Copyright (c) 2012-2018 Wojciech Figat. All rights reserved.
 
 using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace FlaxEngine
 {
@@ -22,6 +24,28 @@ namespace FlaxEngine
         public abstract class CustomNode
         {
             /// <summary>
+            /// The node evaluation context structure.
+            /// </summary>
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Context
+            {
+                /// <summary>
+                /// The reserved data field.
+                /// </summary>
+                public IntPtr Reserved;
+
+                /// <summary>
+                /// The graph node identifier (unique per graph).
+                /// </summary>
+                public uint NodeId;
+
+                /// <summary>
+                /// The requested box identifier to evaluate its value.
+                /// </summary>
+                public int BoxId;
+            }
+
+            /// <summary>
             /// Loads the node data from the serialized values and prepares the node to run. In most cases this method is called from the content loading thread (not the main game thread).
             /// </summary>
             /// <param name="values">The node values array. The first item is always the typename of the custom node type, second one is node group name, others are customizable by editor node archetype.</param>
@@ -30,7 +54,27 @@ namespace FlaxEngine
             /// <summary>
             /// Evaluates the node based on inputs and node data.
             /// </summary>
-            public abstract void Evaluate();
+            /// <param name="context">The evaluation context.</param>
+            /// <returns>The node value for the given context (node values, output box id, etc.).</returns>
+            public abstract object Evaluate(ref Context context);
+
+            /// <summary>
+            /// Check if th box of the given ID has valid connection to get its value.
+            /// </summary>
+            /// <param name="context">The context.</param>
+            /// <param name="boxId">The input box identifier.</param>
+            /// <returns>True if has connection, otherwise false..</returns>
+            [MethodImpl(MethodImplOptions.InternalCall)]
+            public static extern bool HasConnection(ref Context context, int boxId);
+
+            /// <summary>
+            /// Gets the value of the input box of the given ID. Throws the exception if box has no valid connection.
+            /// </summary>
+            /// <param name="context">The context.</param>
+            /// <param name="boxId">The input box identifier.</param>
+            /// <returns>The value.</returns>
+            [MethodImpl(MethodImplOptions.InternalCall)]
+            public static extern object GetInputValue(ref Context context, int boxId);
         }
     }
 }
