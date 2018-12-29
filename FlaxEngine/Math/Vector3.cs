8022ce13f439dbc4f9913d37e3f953b16bdda4ed
mafiesto4 <wojtek@figat.pl>
2018-12-29 17:27:38 +0100

    Add helper Vector3 distance method with ref input but return result

--- FlaxEngine/Math/Vector3.cs
+++ FlaxEngine/Math/Vector3.cs
@@ -768,6 +768,26 @@ namespace FlaxEngine
         /// <param name="value2">The second vector.</param>
         /// <returns>The distance between the two vectors.</returns>
         /// <remarks>
+        /// <see cref="Vector3.DistanceSquared(ref Vector3, ref Vector3, out float)" /> may be preferred when only the relative
+        /// distance is needed
+        /// and speed is of the essence.
+        /// </remarks>
+        public static float Distance(ref Vector3 value1, ref Vector3 value2)
+        {
+            float x = value1.X - value2.X;
+            float y = value1.Y - value2.Y;
+            float z = value1.Z - value2.Z;
+
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        /// <summary>
+        /// Calculates the distance between two vectors.
+        /// </summary>
+        /// <param name="value1">The first vector.</param>
+        /// <param name="value2">The second vector.</param>
+        /// <returns>The distance between the two vectors.</returns>
+        /// <remarks>
         /// <see cref="Vector3.DistanceSquared(Vector3, Vector3)" /> may be preferred when only the relative distance is needed
         /// and speed is of the essence.
         /// </remarks>
@@ -801,6 +821,29 @@ namespace FlaxEngine
             float z = value1.Z - value2.Z;
 
             result = x * x + y * y + z * z;
+        }
+
+        /// <summary>
+        /// Calculates the squared distance between two vectors.
+        /// </summary>
+        /// <param name="value1">The first vector.</param>
+        /// <param name="value2">The second vector.</param>
+        /// <returns>The squared distance between the two vectors.</returns>
+        /// <remarks>
+        /// Distance squared is the value before taking the square root.
+        /// Distance squared can often be used in place of distance if relative comparisons are being made.
+        /// For example, consider three points A, B, and C. To determine whether B or C is further from A,
+        /// compare the distance between A and B to the distance between A and C. Calculating the two distances
+        /// involves two square roots, which are computationally expensive. However, using distance squared
+        /// provides the same information and avoids calculating two square roots.
+        /// </remarks>
+        public static float DistanceSquared(ref Vector3 value1, ref Vector3 value2)
+        {
+            float x = value1.X - value2.X;
+            float y = value1.Y - value2.Y;
+            float z = value1.Z - value2.Z;
+
+            return x * x + y * y + z * z;
         }
 
         /// <summary>
