9ad7cf44d0ca3da7c812dcbf7e9326be074b745d
mafiesto4 <wojtek@figat.pl>
2019-02-15 21:37:22 +0100

    Add StaticFlags.Navigation

--- FlaxEngine/Enums/StaticFlags.cs
+++ FlaxEngine/Enums/StaticFlags.cs
@@ -5,34 +5,39 @@ using System;
 namespace FlaxEngine
 {
     /// <summary>
-    /// Static flags for the actor
+    /// Static flags for the actor objects.
     /// </summary>
     [Flags]
     public enum StaticFlags
     {
         /// <summary>
-        /// Non-static object
+        /// Non-static object.
         /// </summary>
         None = 0,
 
         /// <summary>
-        /// Object is considered to be static for reflection probes offline caching
+        /// Object is considered to be static for reflection probes offline caching.
         /// </summary>
         ReflectionProbe = 1 << 0,
 
         /// <summary>
-        /// Object is considered to be static for static lightmaps
+        /// Object is considered to be static for static lightmaps.
         /// </summary>
         Lightmap = 1 << 1,
 
         /// <summary>
-        /// Object is considered to have static transformation in space (no dynamic physics and any movement at runtime)
+        /// Object is considered to have static transformation in space (no dynamic physics and any movement at runtime).
         /// </summary>
         Transform = 1 << 2,
 
         /// <summary>
-        /// Objects is fully static on the scene
+        /// Object is considered to affect navigation (static occluder or walkable surface).
         /// </summary>
-        FullyStatic = Transform | ReflectionProbe | Lightmap,
+        Navigation = 1 << 3,
+
+        /// <summary>
+        /// Objects is fully static on the scene.
+        /// </summary>
+        FullyStatic = Transform | ReflectionProbe | Lightmap | Navigation,
     };
 }
