eeec5fab05cd53d8693830b88aa5ef3a3f0c6f82
mafiesto4 <wojtek@figat.pl>
2019-03-04 19:38:19 +0100

    Add Particles nodes group type

---
+++ FlaxEditor/Surface/Archetypes/Particles.cs
@@ -0,0 +1,45 @@
+// Copyright (c) 2012-2019 Wojciech Figat. All rights reserved.
+
+using FlaxEngine;
+
+namespace FlaxEditor.Surface.Archetypes
+{
+    /// <summary>
+    /// Contains archetypes for nodes from the Particles group.
+    /// </summary>
+    public static class Particles
+    {
+        /// <summary>
+        /// Customized <see cref="SurfaceNode"/> for main particle emitter node.
+        /// </summary>
+        /// <seealso cref="FlaxEditor.Surface.SurfaceNode" />
+        public class ParticleEmitterNode : SurfaceNode
+        {
+            /// <inheritdoc />
+            public ParticleEmitterNode(uint id, VisjectSurfaceContext context, NodeArchetype nodeArch, GroupArchetype groupArch)
+            : base(id, context, nodeArch, groupArch)
+            {
+            }
+        }
+
+        /// <summary>
+        /// The nodes for that group.
+        /// </summary>
+        public static NodeArchetype[] Nodes =
+        {
+            new NodeArchetype
+            {
+                TypeID = 1,
+                Create = (id, context, arch, groupArch) => new ParticleEmitterNode(id, context, arch, groupArch),
+                Title = "Particle Emitter",
+                Description = "Main particle emitter node",
+                Flags = NodeFlags.ParticleEmitterGraph | NodeFlags.NoRemove | NodeFlags.NoSpawnViaGUI | NodeFlags.NoCloseButton,
+                Size = new Vector2(150, 300),
+                /*Elements = new[]
+                {
+                    NodeElementArchetype.Factory.Input(0, "", true, ConnectionType.Impulse, 0),
+                }*/
+            },
+        };
+    }
+}
