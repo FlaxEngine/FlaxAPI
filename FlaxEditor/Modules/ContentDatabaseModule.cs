c94d3c54c7a9fbc232801eda63b444749c251578
mafiesto4 <wojtek@figat.pl>
2019-03-15 10:06:07 +0100

    Register ParticleSystemProxy

--- FlaxEditor/Modules/ContentDatabaseModule.cs
+++ FlaxEditor/Modules/ContentDatabaseModule.cs
@@ -859,6 +859,7 @@ namespace FlaxEditor.Modules
             Proxy.Add(new PreviewsCacheProxy());
             Proxy.Add(new FontProxy());
             Proxy.Add(new ParticleEmitterProxy());
+            Proxy.Add(new ParticleSystemProxy());
             Proxy.Add(new ScriptProxy());
             Proxy.Add(new SceneProxy());
             Proxy.Add(new PrefabProxy());
