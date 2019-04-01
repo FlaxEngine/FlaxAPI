da848996cd82706c012d3910bc9c49fda057c0af
mafiesto4 <wojtek@figat.pl>
2019-04-02 00:08:51 +0200

    Doc twekas

--- FlaxEditor/States/EditorState.cs
+++ FlaxEditor/States/EditorState.cs
@@ -13,9 +13,6 @@ namespace FlaxEditor.States
         /// <summary>
         /// Gets the editor state machine.
         /// </summary>
-        /// <value>
-        /// The editor state machine.
-        /// </value>
         public new EditorStateMachine StateMachine => owner as EditorStateMachine;
 
         /// <summary>
@@ -24,51 +21,43 @@ namespace FlaxEditor.States
         public readonly Editor Editor;
 
         /// <summary>
-        /// Check if can edit assets in this state
+        /// Checks if can edit assets in this state.
         /// </summary>
-        /// <returns>True if can edit content, otherwise false</returns>
         public virtual bool CanEditContent => true;
 
         /// <summary>
-        /// Check if can edit scene in this state
+        /// Checks if can edit scene in this state
         /// </summary>
-        /// <returns>True if can edit scene, otherwise false</returns>
         public virtual bool CanEditScene => false;
 
         /// <summary>
-        /// Check if can use toolbox in this state
+        /// Checks if can use toolbox in this state.
         /// </summary>
-        /// <returns>True if can use toolbox, otherwise false</returns>
         public virtual bool CanUseToolbox => false;
 
         /// <summary>
-        /// Check if can use undo/redo in this state
+        /// Checks if can use undo/redo in this state.
         /// </summary>
-        /// <returns>True if can use undo/redo, otherwise false</returns>
         public virtual bool CanUseUndoRedo => false;
 
         /// <summary>
-        /// Check if can change scene in this state
+        /// Checks if can change scene in this state.
         /// </summary>
-        /// <returns>True if can change scene, otherwise false</returns>
         public virtual bool CanChangeScene => false;
 
         /// <summary>
-        /// Check if can enter play mode in this state
+        /// Checks if can enter play mode in this state.
         /// </summary>
-        /// <returns>True if can enter play mode, otherwise false</returns>
         public virtual bool CanEnterPlayMode => false;
 
         /// <summary>
-        /// Check if can enter recompile scripts in this state
+        /// Checks if can enter recompile scripts in this state.
         /// </summary>
-        /// <returns>True if can recompile scripts, otherwise false</returns>
         public virtual bool CanReloadScripts => false;
 
         /// <summary>
-        /// Check if static is valid for Editor UI calls and other stuff
+        /// Checks if static is valid for Editor UI calls and other stuff.
         /// </summary>
-        /// <returns>True if state is valid for Editor UI calls and other stuff, otherwise false</returns>
         public virtual bool IsEditorReady => true;
 
         /// <summary>
