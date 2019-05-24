ed87070592c16f14b6c18390c874823cad176c30
Jean-Baptiste Perrier <jimi.vacarians@gmail.com>
2019-05-24 13:13:59 +0100

    Type constraint support

--- FlaxEditor/Modules/ContentFindingModule.cs
+++ FlaxEditor/Modules/ContentFindingModule.cs
@@ -14,15 +14,15 @@ namespace FlaxEditor.Modules
         public string Name;
         public Action Action;
     }
-    
+
     public struct SearchResult
     {
         public string Name;
         public string Type;
         public object Item;
     }
-    
-    
+
+
     /// <summary>
     /// The content finding module.
     /// </summary>
@@ -32,15 +32,11 @@ namespace FlaxEditor.Modules
         /// The content finding context menu.
         /// </summary>
         public ContentFinder Finder { get; private set; }
-        
+
         private List<QuickAction> _quickActions = new List<QuickAction>();
-        
+
         public ContentFindingModule(Editor editor) : base(editor)
         {
-            AddQuickAction("Print Hello", () =>
-            {
-                Debug.Log("Hello");
-            });
         }
 
         /// <inheritdoc />
@@ -57,7 +53,7 @@ namespace FlaxEditor.Modules
         /// <param name="action"></param>
         public void AddQuickAction(string name, Action action)
         {
-            _quickActions.Add(new QuickAction(){Name = name, Action = action});
+            _quickActions.Add(new QuickAction() { Name = name, Action = action });
         }
 
         /// <summary>
@@ -81,42 +77,57 @@ namespace FlaxEditor.Modules
 
         public List<SearchResult> Search(string charsToFind)
         {
-            var regex = new Regex(charsToFind.Trim(), RegexOptions.IgnoreCase);
+            string type = ".*";
+            string name = charsToFind.Trim();
+
+            if (charsToFind.Contains(':'))
+            {
+                var args = charsToFind.Split(':');
+                type = ".*"+args[1].Trim()+".*";
+                name = ".*"+args[0].Trim()+".*";
+            }
             
+            if (name.Equals(""))
+                name = ".*";
+
+            Debug.Log("Search : type = " + type + "  name = " + name);
+
+            Regex typeRegex = new Regex(type, RegexOptions.IgnoreCase);
+            Regex nameRegex = new Regex(name, RegexOptions.IgnoreCase);
+
             BlockingCollection<SearchResult> matches = new BlockingCollection<SearchResult>();
-            
+
             List<ContentItem> assetsItems = Editor.Instance.ContentDatabase.ProjectContent.Folder.Children;
             List<ContentItem> scriptsItems = Editor.Instance.ContentDatabase.ProjectSource.Folder.Children;
 
+            ProcessItems(nameRegex, typeRegex, assetsItems, matches);
+            ProcessItems(nameRegex, typeRegex, scriptsItems, matches);
 
-            if (!charsToFind.Trim().Equals(""))
+            _quickActions.ForEach((action) =>
             {
-                ProcessItems(regex, assetsItems, matches);
-                ProcessItems(regex, scriptsItems, matches);
-                
-                _quickActions.ForEach((action) =>
-                {
-                    if(regex.Match(action.Name).Success)
-                        matches.Add(new SearchResult(){Name = action.Name, Type = "Quick Action", Item = action});
-                });
-            }
+                if (nameRegex.Match(action.Name).Success && typeRegex.Match("Quick Action").Success)
+                    matches.Add(new SearchResult() { Name = action.Name, Type = "Quick Action", Item = action });
+            });
 
             return matches.ToList();
         }
-        
-        private void ProcessItems(Regex regex, List<ContentItem> items, BlockingCollection<SearchResult> matches)
+
+        private void ProcessItems(Regex nameRegex, Regex typeRegex, List<ContentItem> items, BlockingCollection<SearchResult> matches)
         {
-            Match _match;
             foreach (var contentItem in items)
             {
+                //Debug.Log(contentItem.GetType().Name);
                 if (contentItem.IsAsset)
                 {
-                    _match = regex.Match(contentItem.ShortName);
-                    if (_match.Success)
+                    if (nameRegex.Match(contentItem.ShortName).Success)
                     {
                         var asset = contentItem as AssetItem;
-                        string finalName;
                         
+                        if (!typeRegex.Match(asset.TypeName).Success)
+                            continue;
+                        
+                        string finalName;
+
                         if (_aliases.ContainsKey(asset.TypeName))
                         {
                             finalName = _aliases[asset.TypeName];
@@ -126,44 +137,35 @@ namespace FlaxEditor.Modules
                             var splits = asset.TypeName.Split('.');
                             finalName = splits[splits.Length - 1];
                         }
-                        matches.Add(new SearchResult() { Name = asset.ShortName, Type = finalName, Item = asset});
+                        matches.Add(new SearchResult() { Name = asset.ShortName, Type = finalName, Item = asset });
                     }
                 }
-                else if(contentItem.IsFolder)
+                else if (contentItem.IsFolder)
                 {
                     //ThreadPool.QueueUserWorkItem((state) => ProcessItems(regex, ((ContentFolder) contentItem).Children, matches));
-                    ProcessItems(regex, ((ContentFolder) contentItem).Children, matches);
+                    ProcessItems(nameRegex, typeRegex, ((ContentFolder)contentItem).Children, matches);
                 }
                 else if (contentItem.GetType().Name.Equals("FileItem"))
                 {
-                    
                 }
                 else
                 {
-                    _match = regex.Match(contentItem.ShortName);
-                    if (_match.Success)
+                    if (nameRegex.Match(contentItem.ShortName).Success && typeRegex.Match(contentItem.GetType().Name).Success)
                     {
-                        string finalName;
-                        if (_aliases.ContainsKey(contentItem.GetType().Name))
-                        {
-                            finalName = _aliases[contentItem.GetType().Name];
-                        }
-                        else
-                        {
-                            finalName = contentItem.GetType().Name.Replace("Item", "");
-                        }
-                        matches.Add(new SearchResult() { Name = contentItem.ShortName, Type = finalName, Item = contentItem});
+                        string finalName = contentItem.GetType().Name.Replace("Item", "");
+                        
+                        matches.Add(new SearchResult() { Name = contentItem.ShortName, Type = finalName, Item = contentItem });
                     }
                 }
             }
         }
-        
+
         private Dictionary<string, string> _aliases = new Dictionary<string, string>()
         {
-            {"FlaxEditor.Content.Settings.GameSettings", "Settings"},
-            {"FlaxEditor.Content.Settings.GraphicsSettings", "Settings"},
-            {"FlaxEditor.Content.Settings.InputSettings", "Settings"},
-            {"SceneItem", "Scene"}
+            { "FlaxEditor.Content.Settings.GameSettings", "Settings" },
+            { "FlaxEditor.Content.Settings.GraphicsSettings", "Settings" },
+            { "FlaxEditor.Content.Settings.InputSettings", "Settings" },
+            { "SceneItem", "Scene" }
         };
     }
 }
