b490e5b11094e418d63f2e8de51568255c336263
mafiesto4 <wojtek@figat.pl>
2019-06-28 14:30:46 +0200

    Move ShuntingYard to editor assembly

--- FlaxEditor/Utilities/ShuntingYardParser.cs
+++ FlaxEditor/Utilities/ShuntingYardParser.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace FlaxEngine
+namespace FlaxEditor.Utilities
 {
     /// <summary>
     /// The Shunting-Yard algorithm parsing utilities.
