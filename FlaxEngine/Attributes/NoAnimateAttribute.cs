e1374a3def4cbde4f643187b9eebe74d22217bc9
mafiesto4 <wojtek@figat.pl>
2019-09-02 17:31:17 +0200

    Allow to use NoAnimateAttribute for fields and methods

--- FlaxEngine/Attributes/NoAnimateAttribute.cs
+++ FlaxEngine/Attributes/NoAnimateAttribute.cs
@@ -5,9 +5,9 @@ using System;
 namespace FlaxEngine
 {
     /// <summary>
-    /// Indicates that a property of a class cannot be animated by the scene animations system. This class cannot be inherited.
+    /// Indicates that a member of a class cannot be animated by the scene animations system. This class cannot be inherited.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Method)]
     public sealed class NoAnimateAttribute : Attribute
     {
     }
