fe05098a69782c9e4c992a8af0861e444da56677
mafiesto4 <wojtek@figat.pl>
2019-09-28 13:39:02 +0200

    Add Font.ProcessText

--- FlaxEngine/API/Objects/Font.cs
+++ FlaxEngine/API/Objects/Font.cs
@@ -1,20 +1,46 @@
 // Copyright (c) 2012-2019 Wojciech Figat. All rights reserved.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace FlaxEngine
 {
+    /// <summary>
+    /// The font line info generated during text processing.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct FontLineCache
+    {
+        /// <summary>
+        /// The root position of the line (upper left corner).
+        /// </summary>
+        public Vector2 RootPos;
+
+        /// <summary>
+        /// The maximum line extent on the Y axis (down from RootPos).
+        /// </summary>
+        public Vector2 MaxYExtent;
+
+        /// <summary>
+        /// The line width.
+        /// </summary>
+        public float Width;
+
+        /// <summary>
+        /// The first character index (from the input text).
+        /// </summary>
+        public int FirstCharIndex;
+
+        /// <summary>
+        /// The last character index (from the input text).
+        /// </summary>
+        public int LastCharIndex;
+    };
+
     /// <summary>
     /// Represents font object that can be using during text rendering (it uses Font Asset but with precached data for chosen font properties).
     /// </summary>
     public partial class Font
     {
-        // TODO: provide ProcessText APIs
         // TODO: provide GetCharacter APIs
         // TODO: expose CharacterEntry and ability to get per font character info
     }
