524ce6cf1c4808c4b544a481d08b4c41c3a8bba4
mafiesto4 <wojtek@figat.pl>
2019-10-16 09:43:23 +0200

    Bump up build number

--- FlaxEngine.Tests/Properties/AssemblyInfo.cs
+++ FlaxEngine.Tests/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4aaed6a2-38b1-4a31-ab04-9264a94a8eca")]
-[assembly: AssemblyVersion("0.5.6187")]
-[assembly: AssemblyFileVersion("0.5.6187")]
+[assembly: AssemblyVersion("0.5.6188")]
+[assembly: AssemblyFileVersion("0.5.6188")]
