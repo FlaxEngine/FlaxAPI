9b7cb65e0574710b3793d9ce0e37b0de2e5f1247
Wojtek Figat <wojtek@figat.pl>
2019-11-22 15:48:57 +0100

    Expose GPUDevice.CreateTexture to C# API

--- FlaxEngine/API/Static/GPUDevice.Gen.cs
+++ FlaxEngine/API/Static/GPUDevice.Gen.cs
@@ -138,6 +138,24 @@ namespace FlaxEngine
 #endif
         }
 
+        /// <summary>
+        /// Creates the texture.
+        /// </summary>
+        /// <param name="name">The resource name.</param>
+        /// <returns>The texture.</returns>
+#if UNIT_TEST_COMPILANT
+        [Obsolete("Unit tests, don't support methods calls.")]
+#endif
+        [UnmanagedCall]
+        public static GPUTexture CreateTexture(string name = "")
+        {
+#if UNIT_TEST_COMPILANT
+            throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
+#else
+            return Internal_CreateTexture(name);
+#endif
+        }
+
         #region Internal Calls
 
 #if !UNIT_TEST_COMPILANT
@@ -167,6 +185,9 @@ namespace FlaxEngine
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         internal static extern void Internal_DumpResourcesToLog();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        internal static extern GPUTexture Internal_CreateTexture(string name);
 #endif
 
         #endregion
