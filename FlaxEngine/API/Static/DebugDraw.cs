524f6a93b5f04e64d80e670217a44343fc78395f
Wojtek Figat <wojtek@figat.pl>
2019-12-13 01:34:45 +0100

    Update API

--- FlaxEngine/API/Static/DebugDraw.cs
+++ FlaxEngine/API/Static/DebugDraw.cs
@@ -7,26 +7,6 @@ namespace FlaxEngine
     public static partial class DebugDraw
     {
         /// <summary>
-        /// Draws the wireframe sphere.
-        /// </summary>
-        /// <param name="sphere">The sphere.</param>
-        /// <param name="color">The color.</param>
-        /// <param name="duration">The duration (in seconds). Use 0 to draw it only once.</param>
-        /// <param name="depthTest">If set to <c>true</c> depth test will be performed, otherwise depth will be ignored.</param>
-#if UNIT_TEST_COMPILANT
-		[Obsolete("Unit tests, don't support methods calls.")]
-#endif
-        [UnmanagedCall]
-        public static void DrawWireSphere(BoundingSphere sphere, Color color, float duration = 0.0f, bool depthTest = true)
-        {
-#if UNIT_TEST_COMPILANT
-			throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
-#else
-            Internal_DrawWireSphere(ref sphere.Center, sphere.Radius, ref color, duration, depthTest);
-#endif
-        }
-
-        /// <summary>
         /// Draws the debug data.
         /// </summary>
         /// <param name="task">The calling rendering task.</param>
@@ -35,7 +15,7 @@ namespace FlaxEngine
 		[Obsolete("Unit tests, don't support methods calls.")]
 #endif
         [UnmanagedCall]
-        public static void Draw(FlaxEngine.RenderTask task, Actor[] selectedActors)
+        public static void Draw(RenderTask task, Actor[] selectedActors)
         {
 #if UNIT_TEST_COMPILANT
 			throw new NotImplementedException("Unit tests, don't support methods calls. Only properties can be get or set.");
