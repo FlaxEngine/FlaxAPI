09587b9a9b7fb4af8355450f765a185610b42d16
Wojtek Figat <wojtek@figat.pl>
2019-12-18 12:27:57 +0100

    Fix ShaderProfile entries naming for Directx

--- FlaxEngine/Rendering/Enums.cs
+++ FlaxEngine/Rendering/Enums.cs
@@ -76,14 +76,14 @@ namespace FlaxEngine
         Unknown = 0,
 
         /// <summary>
-        /// Shader Model 4.0
+        /// DirectX (Shader Model 4 compatible)
         /// </summary>
-        ShaderModel4 = 1,
+        DirectX_SM4 = 1,
 
         /// <summary>
-        /// Shader Model 5.0
+        /// DirectX (Shader Model 5 compatible)
         /// </summary>
-        ShaderModel5 = 2,
+        DirectX_SM5 = 2,
 
         /// <summary>
         /// GLSL 410
