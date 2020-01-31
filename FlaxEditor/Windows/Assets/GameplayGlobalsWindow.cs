79f57ad587eeed89c8e4028cb745fb4354cf835c
Wojtek Figat <wojtek@figat.pl>
2020-01-31 12:16:13 +0100

    Add gameplay globals adding/removing with undo

--- FlaxEditor/Windows/Assets/GameplayGlobalsWindow.cs
+++ FlaxEditor/Windows/Assets/GameplayGlobalsWindow.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using FlaxEditor.Content;
 using FlaxEditor.CustomEditors;
+using FlaxEditor.CustomEditors.GUI;
 using FlaxEditor.GUI;
+using FlaxEditor.GUI.ContextMenu;
 using FlaxEngine;
 using FlaxEngine.GUI;
 
@@ -18,18 +20,71 @@ namespace FlaxEditor.Windows.Assets
     /// <seealso cref="FlaxEditor.Windows.Assets.AssetEditorWindow" />
     public sealed class GameplayGlobalsWindow : AssetEditorWindowBase<GameplayGlobals>
     {
+        private class AddRemoveParamAction : IUndoAction
+        {
+            public PropertiesProxy Proxy;
+            public bool IsAdd;
+            public string Name;
+            public object DefaultValue;
+
+            /// <inheritdoc />
+            public string ActionString => IsAdd ? "Add parameter" : "Remove parameter";
+
+            /// <inheritdoc />
+            public void Do()
+            {
+                if (IsAdd)
+                    Add();
+                else
+                    Remove();
+            }
+
+            /// <inheritdoc />
+            public void Undo()
+            {
+                if (IsAdd)
+                    Remove();
+                else
+                    Add();
+            }
+
+            private void Add()
+            {
+                Proxy.DefaultValues[Name] = DefaultValue;
+                Proxy.Values[Name] = DefaultValue;
+                Proxy.Window._propertiesEditor.BuildLayoutOnUpdate();
+            }
+
+            private void Remove()
+            {
+                DefaultValue = Proxy.DefaultValues[Name];
+                Proxy.Values.Remove(Name);
+                Proxy.DefaultValues.Remove(Name);
+                Proxy.Window._propertiesEditor.BuildLayoutOnUpdate();
+            }
+
+            /// <inheritdoc />
+            public void Dispose()
+            {
+                DefaultValue = null;
+                Proxy = null;
+            }
+        }
+
         [CustomEditor(typeof(PropertiesProxyEditor))]
         private sealed class PropertiesProxy
         {
+            public GameplayGlobalsWindow Window;
             public GameplayGlobals Asset;
             public Dictionary<string, object> DefaultValues;
             public Dictionary<string, object> Values;
 
-            public void Set(GameplayGlobals asset)
+            public void Init(GameplayGlobalsWindow window)
             {
-                Asset = asset;
-                DefaultValues = asset.DefaultValues;
-                Values = asset.Values;
+                Window = window;
+                Asset = window.Asset;
+                DefaultValues = Asset.DefaultValues;
+                Values = Asset.Values;
             }
         }
 
@@ -159,10 +214,18 @@ namespace FlaxEditor.Windows.Assets
                     // TODO: editing default value
                     // TODO: editing value
                     // TODO: renaming variables
+                    // TODO: removing variable
                     // TODO: copy/paste values
 
-                    var valueContainer = new VariableValueContainer(_proxy, e.Key, e.Value);
-                    layout.Property(e.Key, valueContainer);
+                    var name = e.Key;
+                    var valueContainer = new VariableValueContainer(_proxy, name, e.Value);
+                    var propertyLabel = new ClickablePropertyNameLabel(name)
+                    {
+                        Tag = name,
+                    };
+                    propertyLabel.MouseRightClick += (label, location) => ShowParameterMenu(name, label, ref location);
+                    var property = layout.AddPropertyItem(propertyLabel);
+                    property.Object(valueContainer);
                 }
 
                 // TODO: improve the UI
@@ -177,8 +240,7 @@ namespace FlaxEditor.Windows.Assets
 
             private void OnAddParamButtonClicked()
             {
-                // TODO: impl this
-                MessageBox.Show("Add " + AllowedTypes[_addParamType.SelectedIndex]);
+                AddParameter(AllowedTypes[_addParamType.SelectedIndex]);
             }
 
             /// <inheritdoc />
@@ -186,32 +248,106 @@ namespace FlaxEditor.Windows.Assets
             {
                 if (_proxy?.Asset != null)
                 {
-                    _proxy.DefaultValues = _proxy.Asset.DefaultValues;
+                    // Get the current values
                     _proxy.Values = _proxy.Asset.Values;
                 }
 
                 base.Refresh();
+            }
+
+            /// <summary>
+            /// Shows the parameter context menu.
+            /// </summary>
+            /// <param name="name">The name.</param>
+            /// <param name="label">The label control.</param>
+            /// <param name="targetLocation">The target location.</param>
+            private void ShowParameterMenu(string name, Control label, ref Vector2 targetLocation)
+            {
+                var contextMenu = new ContextMenu();
+                contextMenu.AddButton("Delete", () => DeleteParameter(name));
+                contextMenu.Show(label, targetLocation);
+            }
+
+            /// <summary>
+            /// Adds the parameter.
+            /// </summary>
+            /// <param name="type">The type.</param>
+            private void AddParameter(Type type)
+            {
+                var material = _proxy?.Asset;
+                if (material == null || !material.IsLoaded)
+                    return;
+                var action = new AddRemoveParamAction
+                {
+                    Proxy = _proxy,
+                    IsAdd = true,
+                    Name = "New parameter",
+                    DefaultValue = Utilities.Utils.GetDefaultValue(type),
+                };
+                _proxy.Window.Undo.AddAction(action);
+                action.Do();
+            }
+            
+            /// <summary>
+            /// Removes the parameter.
+            /// </summary>
+            /// <param name="name">The name.</param>
+            private void DeleteParameter(string name)
+            {
+                var action = new AddRemoveParamAction
+                {
+                    Proxy = _proxy,
+                    IsAdd = false,
+                    Name = name,
+                };
+                _proxy.Window.Undo.AddAction(action);
+                action.Do();
             }
         }
 
         private CustomEditorPresenter _propertiesEditor;
         private PropertiesProxy _proxy;
         private ToolStripButton _saveButton;
+        private ToolStripButton _undoButton;
+        private ToolStripButton _redoButton;
         private ToolStripButton _resetButton;
         private Undo _undo;
+
+        /// <summary>
+        /// Gets the undo for asset editing actions.
+        /// </summary>
+        public Undo Undo => _undo;
 
         /// <inheritdoc />
         public GameplayGlobalsWindow(Editor editor, AssetItem item)
         : base(editor, item)
         {
             _undo = new Undo();
+            _undo.ActionDone += OnUndo;
+            _undo.UndoDone += OnUndo;
+            _undo.RedoDone += OnUndo;
             _propertiesEditor = new CustomEditorPresenter(_undo);
             _propertiesEditor.Panel.Parent = this;
             _propertiesEditor.Modified += MarkAsEdited;
             _proxy = new PropertiesProxy();
             _propertiesEditor.Select(_proxy);
+
             _saveButton = (ToolStripButton)_toolstrip.AddButton(editor.Icons.Save32, Save).LinkTooltip("Save asset");
+            _toolstrip.AddSeparator();
+            _undoButton = (ToolStripButton)_toolstrip.AddButton(Editor.Icons.Undo32, _undo.PerformUndo).LinkTooltip("Undo (Ctrl+Z)");
+            _redoButton = (ToolStripButton)_toolstrip.AddButton(Editor.Icons.Redo32, _undo.PerformRedo).LinkTooltip("Redo (Ctrl+Y)");
+            _toolstrip.AddSeparator();
             _resetButton = (ToolStripButton)_toolstrip.AddButton(editor.Icons.Rotate32, Reset).LinkTooltip("Resets the variables values to the default values");
+
+            InputActions.Add(options => options.Save, Save);
+            InputActions.Add(options => options.Undo, _undo.PerformUndo);
+            InputActions.Add(options => options.Redo, _undo.PerformRedo);
+        }
+
+        private void OnUndo(IUndoAction action)
+        {
+            UpdateToolstrip();
+            MarkAsEdited();
         }
 
         private void Reset()
@@ -223,7 +359,7 @@ namespace FlaxEditor.Windows.Assets
         protected override void OnAssetLoaded()
         {
             _undo.Clear();
-            _proxy.Set(Asset);
+            _proxy.Init(this);
             _propertiesEditor.BuildLayoutOnUpdate();
             UpdateToolstrip();
 
@@ -242,6 +378,8 @@ namespace FlaxEditor.Windows.Assets
         protected override void UpdateToolstrip()
         {
             _saveButton.Enabled = IsEdited;
+            _undoButton.Enabled = _undo.CanUndo;
+            _redoButton.Enabled = _undo.CanRedo;
             _resetButton.Enabled = _asset != null;
 
             base.UpdateToolstrip();
@@ -271,6 +409,8 @@ namespace FlaxEditor.Windows.Assets
             _propertiesEditor = null;
             _proxy = null;
             _saveButton = null;
+            _undoButton = null;
+            _redoButton = null;
             _resetButton = null;
         }
     }
